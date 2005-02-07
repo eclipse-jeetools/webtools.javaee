5d3e712bc1f5d547a38b320faf40962d06c4d795
ndai <ndai>
2005-02-07 12:25:39 +0000

    Refactored AntProjectBuilder

--- plugins/org.eclipse.jst.j2ee.ejb.annotations.xdoclet/src/org/eclipse/jst/j2ee/ejb/annotations/internal/xdoclet/XDocletBuilder.java
+++ plugins/org.eclipse.jst.j2ee.ejb.annotations.xdoclet/src/org/eclipse/jst/j2ee/ejb/annotations/internal/xdoclet/XDocletBuilder.java
@@ -8,18 +8,10 @@
  **************************************************************************************************/
 package org.eclipse.jst.j2ee.ejb.annotations.internal.xdoclet;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import org.eclipse.core.resources.ICommand;
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
@@ -35,7 +27,6 @@ import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -43,17 +34,9 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.content.IContentDescription;
 import org.eclipse.core.runtime.content.IContentType;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IPackageFragment;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.IParent;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jst.common.launcher.ant.AntLauncher;
 import org.eclipse.jst.j2ee.ejb.annotations.xdoclet.Logger;
-import org.eclipse.jst.j2ee.ejb.annotations.xdoclet.XDocletAnnotationPlugin;
 import org.eclipse.jst.j2ee.ejb.annotations.xdoclet.XDocletPreferenceStore;
 
 public class XDocletBuilder extends IncrementalProjectBuilder implements
@@ -116,7 +99,7 @@ public class XDocletBuilder extends IncrementalProjectBuilder implements
 	}
 
 	/**
-	 * Add the StructuredBuilder to the build spec of a single IProject
+	 * Add the XDocletBuilder to the build spec of a single IProject
 	 * 
 	 * @param project -
 	 *            the IProject to add to, when needed
@@ -305,177 +288,12 @@ public class XDocletBuilder extends IncrementalProjectBuilder implements
 	void build(int kind, Map args, IResource resource, IContentType[] types,
 			IProgressMonitor monitor) {
 		if (!monitor.isCanceled() && resource.getType() == IResource.FILE) {
-			buildUsingAnt(resource, monitor);
+			XDocletAntProjectBuilder antProjectBuilder= new XDocletAntProjectBuilder();
+			antProjectBuilder.buildUsingAnt(resource, monitor);
 
 		}
 	}
 
-	/**
-	 * @param resource
-	 * @param monitor
-	 */
-	private void buildUsingAnt(IResource resource, IProgressMonitor monitor) {
-		URL url = Platform.getBundle(XDocletAnnotationPlugin.PLUGINID).getEntry(
-				"/templates/builder/xdoclet.xml");
-		IJavaProject javaProject = JavaCore.create(resource.getProject());
-		ICompilationUnit compilationUnit = JavaCore
-				.createCompilationUnitFrom((IFile) resource);
-		try {
-			IPackageFragmentRoot packageFragmentRoot = this
-					.getPackageFragmentRoot(compilationUnit);
-			String beanPath = constructBeanPath(packageFragmentRoot, resource);
-
-			Properties properties = createAntBuildProperties(resource,
-					javaProject, packageFragmentRoot, beanPath);
-
-			 HashMap templates = new HashMap();
-			 templates.put("@beans@", beanPath);
-			 templates.put("@jboss@", addJbossTask());
-			 templates.put("@jonas@", addJonasTask());
-			 templates.put("@weblogic@", addWeblogicTask());
-			 templates.put("@websphere@", addWebSphereTask());
-
-			AntLauncher antLauncher = new AntLauncher(url, resource.getParent()
-					.getLocation(), properties, templates);
-			antLauncher.launch("ejbdoclet", monitor);
-		} catch (Exception e) {
-			Logger.logException(e);
-		}
-	}
-
-	/**
-	 * @param resource
-	 * @param javaProject
-	 * @param packageFragmentRoot
-	 * @param beanPath
-	 * @return
-	 * @throws JavaModelException
-	 */
-	private Properties createAntBuildProperties(IResource resource,
-			IJavaProject javaProject, IPackageFragmentRoot packageFragmentRoot,
-			String beanPath) throws JavaModelException {
-		Properties properties = new Properties();
-		properties.put("ejb", resource.getProject().getName());
-		properties.put("ejb.project.dir", resource.getProject().getLocation()
-				.toString());
-		properties.put("ejb.module.src", packageFragmentRoot.getResource()
-				.getProjectRelativePath().toString());
-		properties.put("ejb.module.gen", packageFragmentRoot.getResource()
-				.getProjectRelativePath().toString());
-		properties.put("ejb.bin.dir", this.getJavaProjectOutputContainer(
-				javaProject).toString());
-		properties.put("xdoclet.home", XDocletPreferenceStore.getProperty(XDocletPreferenceStore.XDOCLETHOME));
-		URL url = Platform.getBundle("org.apache.ant").getEntry("/");
-		try {
-			url = Platform.asLocalURL(url);
-			
-		} catch (IOException e) {
-		}
-		File file = new File(url.getFile());
-		properties.put("ant.home",file.getAbsolutePath());
-		properties.put("ejb.spec.version", "2.0");
-		properties.put("java.class.path", "");
-		properties.put("project.class.path", "");
-		properties.put("project.path", "");
-		return properties;
-	}
-
-	/**
-	 * 
-	 * XDoclet ejb task requires the names of the files in the fileset to be
-	 * relative to the source folder (i.e. root of the classpath) Se wee need to
-	 * find porvide them relative to the package fragment root
-	 * 
-	 * @param changedBean
-	 * @return
-	 */
-	private String constructBeanPath(IPackageFragmentRoot root, IResource changedBean) {
-
-		List ejbs = new ArrayList();
-		getAllAnnotatedEjbs(root, ejbs);
-		String beans = "";
-		try {
-			Iterator iterator = ejbs.iterator();
-			while (iterator.hasNext()) {
-				ICompilationUnit cu = (ICompilationUnit) iterator.next();
-				IResource bean;
-				bean = cu.getCorrespondingResource();
-				IPath path = bean.getProjectRelativePath();
-				path = makeRelativeTo(path, root);
-				beans +="\t<include name=\""+ path.toString()+"\" />\n";
-
-			}
-		} catch (JavaModelException e) {
-			Logger.logException(e);
-		}
-		return beans;
-	}
-
-	/**
-	 * @param path2
-	 * @param root
-	 * @return
-	 */
-	private IPath makeRelativeTo(IPath path, IPackageFragmentRoot root) {
-		try {
-			IPath rpath = root.getCorrespondingResource()
-					.getProjectRelativePath();
-			if (rpath.isPrefixOf(path))
-				return path.removeFirstSegments(rpath
-						.matchingFirstSegments(path));
-		} catch (JavaModelException e) {
-			Logger.logException(e);
-		}
-
-		return path;
-	}
-
-	/**
-	 * @param root
-	 * @return
-	 */
-	private void getAllAnnotatedEjbs(IParent root, List list) {
-		IJavaElement[] elements = null;
-		try {
-			elements = root.getChildren();
-			if (elements == null)
-				return;
-			for (int i = 0; i < elements.length; i++) {
-				IJavaElement element = elements[i];
-				if (element.getElementType() == IJavaElement.COMPILATION_UNIT) {
-					String contents = ((ICompilationUnit) element).getSource();
-					// System.out.println(contents);
-					if (contents.indexOf(XDOCLET_EJB_BEAN_TAG) >= 0)
-						list.add(element);
-				} else if (element.getElementType() == IJavaElement.PACKAGE_FRAGMENT) {
-					getAllAnnotatedEjbs((IPackageFragment) element, list);
-				}
-			}
-		} catch (JavaModelException e) {
-			Logger.logException(e);
-		}
-	}
-
-	private IPackageFragmentRoot getPackageFragmentRoot(ICompilationUnit res) {
-		IJavaElement current = res;
-		do {
-			if (current instanceof IPackageFragmentRoot)
-				return (IPackageFragmentRoot) current;
-			current = current.getParent();
-		} while (current != null);
-		return null;
-	}
-
-	private IPath getJavaProjectOutputContainer(IJavaProject proj)
-			throws JavaModelException {
-		IPath path = proj.getOutputLocation();
-		if (path == null)
-			return null;
-		if (path.segmentCount() == 1)
-			return path;
-		return ((IContainer) proj.getProject()).getFolder(
-				path.removeFirstSegments(1)).getProjectRelativePath();
-	}
 
 	/*
 	 * (non-Javadoc)
@@ -619,63 +437,7 @@ public class XDocletBuilder extends IncrementalProjectBuilder implements
 		}
 	}
 
-	private String addWeblogicTask()
-	{
-	    if(!XDocletPreferenceStore.isPropertyActive(XDocletPreferenceStore.EJB_WEBLOGIC))
-	    	return "";
-	    else
-	    	return  "<weblogic version=\""+XDocletPreferenceStore.getProperty(XDocletPreferenceStore.EJB_WEBLOGIC+"_VERSION")+"\""+
-	      "   xmlencoding=\"UTF-8\""+
-	      "   destdir=\"\\${ejb.dd.dir}\""+
-	      "   validatexml=\"false\""+
-	      "   datasource=\"\\${data.source.name}\""+
-	      "   mergedir=\"\\${ejb.dd.dir}\""+
-	      "   persistence=\"weblogic\" />";
-	      
 
-	}
-	
-	private String addJbossTask()
-	{
-	    if(!XDocletPreferenceStore.isPropertyActive(XDocletPreferenceStore.EJB_JBOSS))
-	    	return "";
-	    else
-	    	return  
-	    	"<jboss version=\""+XDocletPreferenceStore.getProperty(XDocletPreferenceStore.EJB_JBOSS+"_VERSION")+"\""+
-	        "    unauthenticatedPrincipal=\"nobody\""+
-	        "    xmlencoding=\"UTF-8\""+
-	        "    destdir=\"\\${ejb.dd.dir}\""+
-	        "    validatexml=\"false\""+
-		    "    datasource=\"\\${data.source.name}\""+
-	        "    datasourcemapping=\"PLEASE_MODIFY_THIS\""+
-	        "    preferredrelationmapping=\"PLEASE_MODIFY_THIS\" />";
-	      
-
-	}
-	
-	private String addJonasTask()
-	{
-	    if(!XDocletPreferenceStore.isPropertyActive(XDocletPreferenceStore.EJB_JONAS))
-	    	return "";
-	    else
-	    	return  
-	    	"<jonas version=\""+XDocletPreferenceStore.getProperty(XDocletPreferenceStore.EJB_JONAS+"_VERSION")+"\""+
-	        "    xmlencoding=\"UTF-8\""+
-	        "    destdir=\"\\${ejb.dd.dir}\""+
-	        "    validatexml=\"false\""+
-		    "    mergedir=\"\\${ejb.dd.dir}\" />";
-	      
-
-	}
-
-	private String addWebSphereTask()
-	{
-	    if(!XDocletPreferenceStore.isPropertyActive(XDocletPreferenceStore.EJB_WEBSPHERE))
-	    	return "";
-	    else
-	    	return  "<webSphere destdir=\"\\${ejb.dd.dir}\"/>";
-	}
-	
 	public static void shutdown() {
 	}
 
