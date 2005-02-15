39846ee35e35e665346e7057dc216879e6c42408
sgunturi <sgunturi>
2005-02-15 23:01:44 +0000

    85343 : EPL copyrights replace CPL copyrights

--- tests/org.eclipse.jem.tests/javatests/InitParserTest46376.java
+++ tests/org.eclipse.jem.tests/javatests/InitParserTest46376.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
  * Copyright (c)  2003 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 /*
  *  $RCSfile: InitParserTest46376.java,v $
- *  $Revision: 1.1 $  $Date: 2003/11/11 16:27:40 $ 
+ *  $Revision: 1.2 $  $Date: 2005/02/15 23:00:16 $ 
  */
  
 /**
--- tests/org.eclipse.jem.tests/javatests/org/eclipse/jem/tests/proxy/initParser/tree/ASTOperationsTest.java
+++ tests/org.eclipse.jem.tests/javatests/org/eclipse/jem/tests/proxy/initParser/tree/ASTOperationsTest.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
  * Copyright (c) 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 /*
  *  $RCSfile: ASTOperationsTest.java,v $
- *  $Revision: 1.2 $  $Date: 2004/02/03 23:18:13 $ 
+ *  $Revision: 1.3 $  $Date: 2005/02/15 23:00:16 $ 
  */
 package org.eclipse.jem.tests.proxy.initParser.tree;
 
