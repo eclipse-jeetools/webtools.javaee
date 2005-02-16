1638d23709c08c6acf07153074f081a65f60427d
cbridgha <cbridgha>
2005-02-16 15:46:14 +0000

    [no bug-BLB]: <new Flexable Project Java Doc - formated> + Committed for M3

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/internal/modulecore/util/EARArtifactEdit.java
+++ plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/internal/modulecore/util/EARArtifactEdit.java
@@ -1,9 +1,14 @@
-/*
- * Created on Feb 3, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
+/*******************************************************************************
+ * Copyright (c) 2003, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.jst.j2ee.internal.modulecore.util;
 
 import org.eclipse.emf.common.util.URI;
@@ -14,34 +19,75 @@ import org.eclipse.jst.j2ee.commonarchivecore.internal.helpers.ArchiveConstants;
 import org.eclipse.wst.common.modulecore.ArtifactEditModel;
 
 /**
- * @author cbridgha
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * <p>
+ * EARArtifactEdit utilizes the facade function of ArtifactEdit {@see ArtifactEdit}to obtain
+ * Application specific data from an ApplicationXMIResource (@see ApplicationXMIResource ). The
+ * ApplicationXMIResource is retrieved from the ArtifactEditModel {@see ArtifactEditModel}using a
+ * cached constant (@see ArchiveConstants.APPLICATION_DD_URI). Defined methods extract data from the
+ * resource.
+ * </p>
  */
+
 public class EARArtifactEdit extends EnterpriseArtifactEdit {
 
+
 	public static String TYPE_ID = "EAR_TYPE";
+
 	/**
-	 * @param model
+	 * <p>
+	 * Creates an instance facade for the given {@see ArtifactEditModel}.
+	 * </p>
+	 * 
+	 * @param anArtifactEditModel
 	 */
 	public EARArtifactEdit(ArtifactEditModel model) {
 		super(model);
 	}
-	/* (non-Javadoc)
-	 * @see org.eclipse.jst.j2ee.internal.modulecore.util.EnterpriseArtifactEdit#getJ2EEVersion()
+
+	/***********************************************************************************************
+	 * <p>
+	 * Retrieves J2EE version information from ApplicationXMIResource.
+	 * </p>
+	 * 
+	 * @return an interger representation of a J2EE Spec version
+	 *  
 	 */
 	public int getJ2EEVersion() {
-		
 		return getApplicationXmiResource().getJ2EEVersionID();
 	}
+
+	/***********************************************************************************************
+	 * 
+	 * @return ApplicationResource from (@link getDeploymentDescriptorResource())
+	 *  
+	 */
+
 	public ApplicationResource getApplicationXmiResource() {
-		return (ApplicationResource)getDeploymentDescriptorResource();
+		return (ApplicationResource) getDeploymentDescriptorResource();
 	}
 
+	/***********************************************************************************************
+	 * <p>
+	 * Obtains the Application (@see Application) root object from the ApplicationResource, the root
+	 * object contains all other resource defined objects.
+	 * </p>
+	 * 
+	 * @return Application
+	 *  
+	 */
+
 	public Application getApplication() {
-		return (Application)getDeploymentDescriptorRoot();
+		return (Application) getDeploymentDescriptorRoot();
 	}
+
+	/***********************************************************************************************
+	 * <p>
+	 * Retrieves the resource from the ArtifactEditModel using defined URI.
+	 * </p>
+	 * 
+	 * @return Resource
+	 *  
+	 */
 
 	public Resource getDeploymentDescriptorResource() {
 		return getArtifactEditModel().getResource(URI.createURI(ArchiveConstants.APPLICATION_DD_URI));
