9afa73fd58b8af415fa8c15887e1bf01c413f011
cbridgha <cbridgha>
2005-03-04 04:01:24 +0000

    Adding in ConnectorArtifactEdit and ConnectorEditAdapterFactory - commit for vkb

---
+++ plugins/org.eclipse.jst.j2ee.jca/rarproject/org/eclipse/jst/j2ee/jca/internal/module/util/ConnectorEditAdapterFactory.java
@@ -0,0 +1,33 @@
+package org.eclipse.jst.j2ee.jca.internal.module.util;
+
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.wst.common.modulecore.ArtifactEdit;
+import org.eclipse.wst.common.modulecore.ArtifactEditModel;
+
+public class ConnectorEditAdapterFactory implements IAdapterFactory {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapter(java.lang.Object, java.lang.Class)
+	 */
+	public Object getAdapter(Object adaptableObject, Class adapterType) {
+		if (adapterType == ConnectorArtifactEdit.ADAPTER_TYPE || adapterType == ArtifactEdit.ADAPTER_TYPE) {
+			ArtifactEditModel editModel = (ArtifactEditModel) adaptableObject;
+			if (editModel.getModuleType().equals(ConnectorArtifactEdit.TYPE_ID))
+				return new ConnectorArtifactEdit((ArtifactEditModel) adaptableObject);
+		}
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapterList()
+	 */
+	public Class[] getAdapterList() {
+		return new Class[]{ArtifactEdit.class, ConnectorEditAdapterFactory.class};
+	}
+
+
+}
