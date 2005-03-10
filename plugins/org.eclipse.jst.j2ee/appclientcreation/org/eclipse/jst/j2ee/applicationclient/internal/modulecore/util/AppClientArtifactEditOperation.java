3166fa249214932be2f31790e4ee24565dbe84a8
cbridgha <cbridgha>
2005-03-10 16:56:20 +0000

    [NOBUG] initial artifactEditDM/Operation for individual modules. Committing for DFH

---
+++ plugins/org.eclipse.jst.j2ee/appclientcreation/org/eclipse/jst/j2ee/applicationclient/internal/modulecore/util/AppClientArtifactEditOperation.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2003, 2004, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jst.j2ee.applicationclient.internal.modulecore.util;
+
+import org.eclipse.wst.common.modulecore.ArtifactEdit;
+import org.eclipse.wst.common.modulecore.WorkbenchModule;
+import org.eclipse.wst.common.modulecore.internal.operation.ArtifactEditOperation;
+
+public class AppClientArtifactEditOperation extends ArtifactEditOperation {
+
+    public AppClientArtifactEditOperation(AppClientArtifactEditOperationDataModel operationDataModel) {
+        super(operationDataModel);
+    }
+    protected ArtifactEdit getArtifactEditForModule(WorkbenchModule module) {
+        return AppClientArtifactEdit.getArtifactEditForWrite(module);
+    }
+    
+    protected AppClientArtifactEdit getAppClientArtifactEdit() {
+        return (AppClientArtifactEdit)getArtifactEdit();
+    }
+}
