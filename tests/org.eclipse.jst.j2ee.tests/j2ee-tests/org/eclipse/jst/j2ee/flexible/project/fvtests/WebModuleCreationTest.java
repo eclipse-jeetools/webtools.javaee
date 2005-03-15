ba882ed1253e32227f3eae1c92f2869eb3cc2002
cbridgha <cbridgha>
2005-03-15 23:11:41 +0000

    commited for NA

--- tests/org.eclipse.jst.j2ee.tests/j2ee-tests/org/eclipse/jst/j2ee/flexible/project/fvtests/WebModuleCreationTest.java
+++ tests/org.eclipse.jst.j2ee.tests/j2ee-tests/org/eclipse/jst/j2ee/flexible/project/fvtests/WebModuleCreationTest.java
@@ -30,7 +30,7 @@ public class WebModuleCreationTest extends AbstractModuleCreationTest {
 	
 	public void testVaild12WebProjectNameCreationWithAlphabetChars() throws Exception {
 	
-		setupWebModule(J2EEVersionConstants.WEB_2_2_ID);
+		setupWebModule(J2EEVersionConstants.WEB_2_3_ID);
 		
 //		ProjectUtility.deleteAllProjects();
 //		for (int i = 0; i < RandomObjectGenerator.createRandomProjectNumber(); i++)
