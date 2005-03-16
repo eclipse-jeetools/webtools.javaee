d1c8f9e6ddcae7f782016a9445d73214445fc388
cbridgha <cbridgha>
2005-03-16 20:40:21 +0000

    [nobug] Renaming ops/dms

--- tests/org.eclipse.jst.j2ee.tests/j2ee-tests/org/eclipse/wtp/j2ee/headless/tests/j2ee/operations/J2EEFlexibleProjectCreationOperationTest.java
+++ tests/org.eclipse.jst.j2ee.tests/j2ee-tests/org/eclipse/wtp/j2ee/headless/tests/j2ee/operations/J2EEFlexibleProjectCreationOperationTest.java
@@ -4,6 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.eclipse.jst.j2ee.application.operations.EnterpriseApplicationCreationDataModelOld;
+import org.eclipse.jst.j2ee.application.operations.FlexibleJavaProjectCreationDataModel;
 import org.eclipse.jst.j2ee.application.operations.J2EEJavaProjectCreationDataModel;
 import org.eclipse.jst.j2ee.application.operations.J2EEModuleCreationDataModelOld;
 import org.eclipse.jst.j2ee.application.operations.J2EEModuleImportDataModel;
@@ -33,20 +34,20 @@ public class J2EEFlexibleProjectCreationOperationTest extends OperationTestCase 
     }
 
     public void createSimpleProject(String projectName) throws Exception {
-        J2EEProjectCreationDataModel dataModel = getProjectCreationDataModel();
-        dataModel.setProperty(J2EEProjectCreationDataModel.PROJECT_NAME, projectName);
+		FlexibleJavaProjectCreationDataModel dataModel = getProjectCreationDataModel();
+        dataModel.setProperty(FlexibleJavaProjectCreationDataModel.PROJECT_NAME, projectName);
 		setServerTargetProperty(dataModel);
         runAndVerify(dataModel);
     }
     
-    public J2EEProjectCreationDataModel getProjectCreationDataModel(){
-		return new J2EEJavaProjectCreationDataModel();
+    public FlexibleJavaProjectCreationDataModel getProjectCreationDataModel(){
+		return new FlexibleJavaProjectCreationDataModel();
     }
 	/**
 	 * @param dataModel
 	 */
-	public void setServerTargetProperty(J2EEProjectCreationDataModel dataModel) {
-		dataModel.setProperty(J2EEProjectCreationDataModel.SERVER_TARGET_ID, AllTests.JONAS_TOMCAT_RUNTIME.getId());
+	public void setServerTargetProperty(FlexibleJavaProjectCreationDataModel dataModel) {
+		dataModel.setProperty(FlexibleJavaProjectCreationDataModel.SERVER_TARGET_ID, AllTests.JONAS_TOMCAT_RUNTIME.getId());
 	}
 
 }
