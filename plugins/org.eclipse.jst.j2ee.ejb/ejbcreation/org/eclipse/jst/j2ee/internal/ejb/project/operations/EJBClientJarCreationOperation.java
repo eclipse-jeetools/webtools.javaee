97f7a462600ccf9ffa71e065894406e4c05342c2
cbridgha <cbridgha>
2005-03-16 21:49:03 +0000

    [NOBUG] initial artifactEditDM/Operation for modules. Committing for DFH

--- plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/internal/ejb/project/operations/EJBClientJarCreationOperation.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/internal/ejb/project/operations/EJBClientJarCreationOperation.java
@@ -269,7 +269,8 @@ public class EJBClientJarCreationOperation extends AbstractEJBClientJAROperation
 		String ejbURI = runtime.getJARUri(ejbProject);
 		String earRelativeClientURI = ArchiveUtil.deriveEARRelativeURI(clientJARRelativeURI, ejbURI);
 		AddUtilityProjectToEARDataModel utilModel = new AddUtilityProjectToEARDataModel();
-		utilModel.setProperty(AddArchiveToEARDataModel.ARCHIVE_PROJECT,  ProjectCreationDataModel.getProjectHandleFromProjectName(ejbClientDataModel.getNestedJavaProjectCreationDM().getStringProperty(ProjectCreationDataModel.PROJECT_NAME)));
+		//TODO: In the flexible world Clients are just JST_UTILITYs so we need to instead set ARCHIVE_MODULE to the name of the client
+		//utilModel.setProperty(AddArchiveToEARDataModel.ARCHIVE_PROJECT,  ProjectCreationDataModel.getProjectHandleFromProjectName(ejbClientDataModel.getNestedJavaProjectCreationDM().getStringProperty(ProjectCreationDataModel.PROJECT_NAME)));
 		utilModel.setProperty(AddArchiveToEARDataModel.ARCHIVE_URI, earRelativeClientURI);
 		utilModel.setProperty(EditModelOperationDataModel.PROJECT_NAME, runtime.getProject().getName());
 		try {
