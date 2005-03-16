b5425248e321d04bde5b18f6184cb3553541460d
cbridgha <cbridgha>
2005-03-16 14:21:52 +0000

    [nobug]:[BLB] < Server API Reaction> + Committed for M3

--- plugins/org.eclipse.jst.j2ee/j2eeplugin/org/eclipse/jst/j2ee/internal/deployables/EnterpriseDeployableArtifactAdapterFactory.java
+++ plugins/org.eclipse.jst.j2ee/j2eeplugin/org/eclipse/jst/j2ee/internal/deployables/EnterpriseDeployableArtifactAdapterFactory.java
@@ -8,7 +8,7 @@ package org.eclipse.jst.j2ee.internal.deployables;
 
 import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.wst.server.core.ILaunchable;
+import org.eclipse.debug.ui.actions.ILaunchable;
 import org.eclipse.wst.server.core.IModuleArtifact;
 import org.eclipse.wst.server.core.model.ModuleArtifactAdapterDelegate;
 
