44e220bf9344749e6b0523cdb9f7adc96a89e9f1
cbridgha <cbridgha>
2005-03-25 18:26:11 +0000

    commited for NA

--- plugins/org.eclipse.jst.j2ee/archiveops/org/eclipse/jst/j2ee/internal/archive/operations/JavaComponentCreationDataModel.java
+++ plugins/org.eclipse.jst.j2ee/archiveops/org/eclipse/jst/j2ee/internal/archive/operations/JavaComponentCreationDataModel.java
@@ -1,8 +1,14 @@
 package org.eclipse.jst.j2ee.internal.archive.operations;
 
+import java.util.List;
+
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.jst.j2ee.application.internal.operations.J2EEComponentCreationDataModel;
 import org.eclipse.jst.j2ee.application.internal.operations.JavaUtilityComponentCreationOperation;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.CommonarchivePackage;
+import org.eclipse.jst.j2ee.internal.J2EEConstants;
+import org.eclipse.jst.j2ee.internal.J2EEVersionConstants;
 import org.eclipse.wst.common.frameworks.internal.operations.WTPOperation;
 import org.eclipse.wst.common.frameworks.internal.operations.WTPPropertyDescriptor;
 import org.eclipse.wst.common.modulecore.internal.operation.ComponentCreationDataModel;
@@ -18,10 +24,17 @@ public class JavaComponentCreationDataModel extends ComponentCreationDataModel {
 	 * type String
 	 */
 	public static final String MANIFEST_FOLDER = "J2EEComponentCreationDataModel.MANIFEST_FOLDER";
+	
+	
+	protected void initValidBaseProperties() {
+		super.initValidBaseProperties();
+		addValidBaseProperty(JAVASOURCE_FOLDER);
+		addValidBaseProperty(MANIFEST_FOLDER);
+	}
+	
 
 	public JavaComponentCreationDataModel() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected EClass getComponentType() {
@@ -56,4 +69,34 @@ public class JavaComponentCreationDataModel extends ComponentCreationDataModel {
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.wst.common.modulecore.internal.operation.ComponentCreationDataModel#getVersion()
+	 */
+	protected String getVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.wst.common.modulecore.internal.operation.ComponentCreationDataModel#getProperties()
+	 */
+	protected List getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public String getJavaSourceFolder() {
+		return getStringProperty(JAVASOURCE_FOLDER);
+	}
+	public String getManifestFolder() {
+		return getStringProperty(MANIFEST_FOLDER);
+	}	
+	
+	protected Object getDefaultProperty(String propertyName) {
+		if (propertyName.equals(JAVASOURCE_FOLDER)) {
+			return  getComponentName();
+		}else if (propertyName.equals(MANIFEST_FOLDER)) {
+			return  getComponentName() +  "/" + J2EEConstants.META_INF; //$NON-NLS-1$
+		}	
+		return super.getDefaultProperty(propertyName);
+	}	
 }
--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/application/internal/operations/JavaUtilityComponentCreationOperation.java
+++ plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/application/internal/operations/JavaUtilityComponentCreationOperation.java
@@ -14,15 +14,28 @@
  */
 package org.eclipse.jst.j2ee.application.internal.operations;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jst.common.jdt.internal.integration.JavaProjectCreationDataModel;
-import org.eclipse.jst.common.jdt.internal.integration.JavaProjectCreationOperation;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jem.util.logger.proxy.Logger;
+import org.eclipse.jst.j2ee.internal.J2EEConstants;
 import org.eclipse.jst.j2ee.internal.archive.operations.JavaComponentCreationDataModel;
+import org.eclipse.jst.j2ee.internal.project.ManifestFileCreationAction;
+import org.eclipse.wst.common.modulecore.ModuleCore;
 import org.eclipse.wst.common.modulecore.internal.operation.ComponentCreationOperation;
 import org.eclipse.wst.common.modulecore.internal.util.IModuleConstants;
+import org.eclipse.wst.common.modulecore.resources.IVirtualContainer;
+import org.eclipse.wst.common.modulecore.resources.IVirtualFolder;
 
 public class JavaUtilityComponentCreationOperation extends ComponentCreationOperation {
 	/**
@@ -34,24 +47,96 @@ public class JavaUtilityComponentCreationOperation extends ComponentCreationOper
 
 
 	 protected void execute(IProgressMonitor monitor) throws CoreException, InvocationTargetException, InterruptedException {
-	        super.execute(monitor);
-	    }
-
-
-	protected void createAndLinkJ2EEComponents() throws CoreException {
-		// TODO Auto-generated method stub
+	 	JavaComponentCreationDataModel dm = (JavaComponentCreationDataModel)operationDataModel;
+	    super.execute(IModuleConstants.JST_UTILITY_MODULE, monitor);
+		createManifest(monitor);
+	    addSrcFolderToProject();
+	 }
+  
+	protected void createComponent() {
+			   	
+		JavaComponentCreationDataModel dm = (JavaComponentCreationDataModel)operationDataModel;	   	
+		IVirtualContainer component = ModuleCore.create(dm.getProject(), dm.getComponentDeployName());
+		try {
+			component.commit();
+			//create and link javaSource Source Folder
+			IVirtualFolder javaSourceFolder = component.getFolder(new Path("/")); //$NON-NLS-1$		
+			javaSourceFolder.createLink(new Path("/" + dm.getComponentName()), 0, null);	
+				    	
+			//create and link META-INF folder
+			IVirtualFolder metaInfFolder = component.getFolder(new Path("/" + J2EEConstants.META_INF)); //$NON-NLS-1$		
+			metaInfFolder.createLink(new Path("/" + dm.getComponentName() + "/" + J2EEConstants.META_INF), 0, null);
+		}
+		catch (CoreException e) {
+			Logger.getLogger().log(e);
+		}
+	}
+	   
+	protected void createManifest(IProgressMonitor monitor) throws CoreException, InvocationTargetException, InterruptedException {
 		
-	}
-
-
-	protected void createDeploymentDescriptor(IProgressMonitor monitor) throws CoreException, InvocationTargetException, InterruptedException {
-		// TODO Auto-generated method stub
+	 	JavaComponentCreationDataModel dm = (JavaComponentCreationDataModel)operationDataModel;		
+		String manifestFolder = dm.getStringProperty(J2EEComponentCreationDataModel.MANIFEST_FOLDER);
+		IContainer container = dm.getProject().getFolder( manifestFolder );
+	
+		IFile file = container.getFile( new Path(J2EEConstants.MANIFEST_SHORT_NAME));
 		
-	}
+		try {
+			ManifestFileCreationAction.createManifestFile(file, dm.getProject());
+		}
+		catch (CoreException e) {
+			Logger.getLogger().log(e);
+		}
+		catch (IOException e) {
+			Logger.getLogger().log(e);
+		}
 
-
-	protected String getVersion() {
-		// TODO Auto-generated method stub
-		return null;
+	}	
+	    
+	private IClasspathEntry[] getClasspathEntries() {
+		IClasspathEntry[] sourceEntries = null;
+		sourceEntries = getSourceClasspathEntries();
+		return sourceEntries;
+	}	
+		
+	private IClasspathEntry[] getSourceClasspathEntries() {
+		
+	 	JavaComponentCreationDataModel dm = (JavaComponentCreationDataModel)operationDataModel;			
+		String sourceFolder = dm.getJavaSourceFolder();
+		ArrayList list = new ArrayList();
+		list.add(JavaCore.newSourceEntry(dm.getProject().getFullPath().append(sourceFolder)));
+		
+		IClasspathEntry[] classpath = new IClasspathEntry[list.size()];
+		for (int i = 0; i < classpath.length; i++) {
+			classpath[i] = (IClasspathEntry) list.get(i);
+		}
+		return classpath;
 	}
+	
+	private void addSrcFolderToProject() {
+	 	JavaComponentCreationDataModel dm = (JavaComponentCreationDataModel)operationDataModel;			
+		IJavaProject javaProject = JavaCore.create( dm.getProject());
+		try {
+	
+			IClasspathEntry[] oldEntries = javaProject.getRawClasspath();
+			IClasspathEntry[] newEntries = getClasspathEntries();
+			
+			int oldSize = oldEntries.length;
+			int newSize = newEntries.length;
+			
+			IClasspathEntry[] classpathEnties = new IClasspathEntry[oldSize + newSize];
+			int k = 0;
+			for (int i = 0; i < oldEntries.length; i++) {
+				classpathEnties[i] = oldEntries[i];
+				k++;
+			}
+			for( int j=0; j< newEntries.length; j++){
+				classpathEnties[k] = newEntries[j];
+				k++;
+			}
+			javaProject.setRawClasspath(classpathEnties, null);
+		}
+		catch (JavaModelException e) {
+			Logger.getLogger().logError(e);
+		}
+	}	
 }
