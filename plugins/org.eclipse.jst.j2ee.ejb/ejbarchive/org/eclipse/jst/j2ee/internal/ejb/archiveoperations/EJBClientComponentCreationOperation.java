0d94686db2d2acdab6a3b9569019309228b2205e
cbridgha <cbridgha>
2005-03-28 21:02:59 +0000

    commited for NA

--- plugins/org.eclipse.jst.j2ee.ejb/ejbarchive/org/eclipse/jst/j2ee/internal/ejb/archiveoperations/EJBClientComponentCreationOperation.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejbarchive/org/eclipse/jst/j2ee/internal/ejb/archiveoperations/EJBClientComponentCreationOperation.java
@@ -12,17 +12,28 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.jem.util.emf.workbench.ProjectUtilities;
+import org.eclipse.jem.util.logger.proxy.Logger;
 import org.eclipse.jst.j2ee.application.internal.operations.AddComponentToEnterpriseApplicationDataModel;
 import org.eclipse.jst.j2ee.application.internal.operations.AddComponentToEnterpriseApplicationOperation;
 import org.eclipse.jst.j2ee.application.internal.operations.JavaUtilityComponentCreationOperation;
+import org.eclipse.jst.j2ee.application.internal.operations.UpdateManifestDataModel;
+import org.eclipse.jst.j2ee.application.internal.operations.UpdateManifestOperation;
+import org.eclipse.jst.j2ee.ejb.internal.modulecore.util.EJBArtifactEdit;
+import org.eclipse.jst.j2ee.internal.ejb.impl.EJBJarImpl;
 import org.eclipse.wst.common.modulecore.ModuleCore;
 import org.eclipse.wst.common.modulecore.ModuleCoreFactory;
 import org.eclipse.wst.common.modulecore.ReferencedComponent;
 import org.eclipse.wst.common.modulecore.WorkbenchComponent;
 import org.eclipse.wst.common.modulecore.internal.util.IModuleConstants;
+import org.eclipse.wst.common.modulecore.resources.IVirtualContainer;
+import org.eclipse.wst.common.modulecore.resources.IVirtualFile;
 
 public class EJBClientComponentCreationOperation extends JavaUtilityComponentCreationOperation {
 
@@ -38,6 +49,8 @@ public class EJBClientComponentCreationOperation extends JavaUtilityComponentCre
 		super.execute(IModuleConstants.JST_UTILITY_MODULE, monitor);
 		runAddToEAROperation(monitor);
 		runAddToEJBOperation(monitor);
+		modifyEJBModuleJarDependency(monitor);
+		updateEJBDD(monitor);
 	}	
 	 
 	protected void runAddToEAROperation(IProgressMonitor monitor) throws CoreException, InvocationTargetException, InterruptedException {
@@ -47,6 +60,8 @@ public class EJBClientComponentCreationOperation extends JavaUtilityComponentCre
 		try {
 			core = ModuleCore.getModuleCoreForRead(dm.getProject());
 			WorkbenchComponent wc = core.findWorkbenchModuleByDeployName(dm.getComponentDeployName());
+			
+
 			
 			AddComponentToEnterpriseApplicationDataModel addComponentToEARDataModel = new AddComponentToEnterpriseApplicationDataModel();;
 			
@@ -98,5 +113,98 @@ public class EJBClientComponentCreationOperation extends JavaUtilityComponentCre
 		       }
 		 }				
 	}
-	 
+	private void modifyEJBModuleJarDependency(IProgressMonitor aMonitor) throws InvocationTargetException, InterruptedException {
+		
+		
+		EJBClientComponentDataModel dm = (EJBClientComponentDataModel)getOperationDataModel();
+		
+		String ejbprojectName = dm.getEJBProjectName();		
+		String ejbComponentName = dm.getEJBComponentName();
+		String ejbComponentDeployName = dm.getEJBDeployName();
+
+		String clientProjectName = dm.getProject().getName();
+		String clientDeployName  = dm.getComponentDeployName();
+
+
+		IProject ejbProject = ProjectUtilities.getProject( ejbprojectName );
+		
+		ModuleCore moduleCore = null;
+		IFile manifestmf = null;
+		try{
+			moduleCore = ModuleCore.getModuleCoreForRead(ejbProject);
+			WorkbenchComponent ejbwc = moduleCore.findWorkbenchModuleByDeployName(ejbComponentDeployName);
+			ejbwc = moduleCore.findWorkbenchModuleByDeployName(ejbComponentDeployName);
+			IVirtualContainer component = ModuleCore.create( ejbProject, ejbwc.getName());
+			IVirtualFile vf = component.getFile( new Path("/META-INF/MANIFEST.MF"));
+			manifestmf = vf.getRealFile();
+		}finally {
+		       if (null != moduleCore) {
+	            moduleCore.dispose();
+	       }
+		}	
+	
+		if( clientProjectName.equals( ejbprojectName )){
+			UpdateManifestDataModel updateManifestDataModel = new UpdateManifestDataModel();
+			updateManifestDataModel.setProperty(UpdateManifestDataModel.PROJECT_NAME, ejbprojectName);
+			updateManifestDataModel.setBooleanProperty(UpdateManifestDataModel.MERGE, false);
+			updateManifestDataModel.setProperty(UpdateManifestDataModel.MANIFEST_FILE, manifestmf);
+			updateManifestDataModel.setProperty(UpdateManifestDataModel.JAR_LIST, UpdateManifestDataModel.convertClasspathStringToList(clientDeployName) );
+			
+			UpdateManifestOperation op = new UpdateManifestOperation(updateManifestDataModel);		
+			op.run(aMonitor);
+		}else{
+	        //JARDependencyDataModel dataModel = new JARDependencyDataModel();
+	        //dataModel.setProperty(JARDependencyDataModel.PROJECT_NAME, ejbprojectName);
+//	        dataModel.setProperty(JARDependencyDataModel.REFERENCED_PROJECT_NAME, clientprojectName);
+//	        if (earNatures !=null && earNatures.length>0)
+//	        	dataModel.setProperty(JARDependencyDataModel.EAR_PROJECT_NAME, earNatures[0].getProject().getName());
+//	        dataModel.setIntProperty(JARDependencyDataModel.JAR_MANIPULATION_TYPE, JARDependencyDataModel.JAR_MANIPULATION_ADD);
+//	        JARDependencyOperation op = new JARDependencyOperation(dataModel);
+//	        op.run(createSubProgressMonitor(1));			
+		}	
+		
+
+	}
+	/**
+	 * Update the deployment descriptor for the ejb project
+	 */
+	private void updateEJBDD(IProgressMonitor monitor) {
+		
+		EJBClientComponentDataModel dm = (EJBClientComponentDataModel)getOperationDataModel();
+		
+		String ejbprojectName = dm.getEJBProjectName();		
+		IProject ejbProject = ProjectUtilities.getProject( ejbprojectName );	
+		String ejbComponentDeployName = dm.getEJBDeployName();
+		String clientDeployName  = dm.getComponentDeployName();
+		
+		ModuleCore moduleCore = null;
+
+		try{
+			moduleCore = ModuleCore.getModuleCoreForRead(ejbProject);
+			WorkbenchComponent ejbwc = moduleCore.findWorkbenchModuleByDeployName(ejbComponentDeployName);
+			ejbwc = moduleCore.findWorkbenchModuleByDeployName(ejbComponentDeployName);
+			
+            EJBArtifactEdit ejbEdit = null;
+           	try{
+           		ejbEdit = EJBArtifactEdit.getEJBArtifactEditForWrite(ejbwc);
+           		if(ejbEdit != null) {
+           			EJBJarImpl ejbres = (EJBJarImpl)ejbEdit.getDeploymentDescriptorRoot();
+           			ejbres.setEjbClientJar(clientDeployName);
+           			ejbEdit.saveIfNecessary(monitor);
+           		}
+           	}
+           	catch(Exception e){
+           		Logger.getLogger().logError(e);
+           		e.printStackTrace();
+           	} finally {
+           		if(ejbEdit != null)
+           			ejbEdit.dispose();
+          	}
+			
+		}finally {
+		       if (null != moduleCore) {
+	            moduleCore.dispose();
+	       }
+		}	
+	}	
 }
