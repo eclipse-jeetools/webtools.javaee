7012d1abab181a536d0b6ea63f673e93441bdc0f
cbridgha <cbridgha>
2005-04-11 22:49:05 +0000

    This commit was manufactured by cvs2svn to create tag 'v20050411a'.

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/internal/earcreation/IEarComponentCreationDataModelProperties.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.jst.j2ee.internal.earcreation;
-
-import org.eclipse.jst.j2ee.application.internal.operations.IJ2EEComponentCreationDataModelProperties;
-
-public interface IEarComponentCreationDataModelProperties extends IJ2EEComponentCreationDataModelProperties {
-	public static final String J2EE_COMPONENT_LIST = "IEarComponentCreationDataModel.J2EE_COMPONENT_LIST"; //$NON-NLS-1$
-
-}
