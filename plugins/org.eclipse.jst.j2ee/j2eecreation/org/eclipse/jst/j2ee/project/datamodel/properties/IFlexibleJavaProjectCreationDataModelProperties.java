2fe49ac9dc6b581d96a4e091420598e3e4ebfcda
vbhadrir <vbhadrir>
2005-04-13 14:50:35 +0000

    [NOBUG] dm port for creation, JavaDoc. Committing for DFH

--- plugins/org.eclipse.jst.j2ee/j2eecreation/org/eclipse/jst/j2ee/project/datamodel/properties/IFlexibleJavaProjectCreationDataModelProperties.java
+++ plugins/org.eclipse.jst.j2ee/j2eecreation/org/eclipse/jst/j2ee/project/datamodel/properties/IFlexibleJavaProjectCreationDataModelProperties.java
@@ -11,25 +11,42 @@
 package org.eclipse.jst.j2ee.project.datamodel.properties;
 
 import org.eclipse.wst.common.frameworks.datamodel.properties.IFlexibleProjectCreationDataModelProperties;
-
+/**
+ * <p>
+ * IFlexibleJavaProjectCreationDataModelProperties provides properties to the DataModel associated with the 
+ * FlexibleJavaProjectCreationDataModelProvider
+ * @see org.eclipse.jst.j2ee.application.internal.operations.FlexibleJavaProjectCreationDataModelProvider
+ * </p>
+ * <p>
+ * This interface is not intended to be implemented by clients.
+ * </p>
+ * 
+ * @see org.eclipse.wst.common.frameworks.datamodel.IDataModelProvider
+ * @see org.eclipse.wst.common.frameworks.datamodel.DataModelFactory
+ * @see org.eclipse.wst.common.frameworks.datamodel.IDataModelProperties
+ * 
+ * @since 1.0
+ */
 public interface IFlexibleJavaProjectCreationDataModelProperties extends IFlexibleProjectCreationDataModelProperties {
+    /**
+     * Required, type IDataModel. The user set IDataModel used to handle the creation of a server target for the project to be
+     * created.  Providers which currently exist for this IDataModel include J2EEProjectServerTargetDataModelProvider.
+     */
     public static final String NESTED_MODEL_SERVER_TARGET = "IFlexibleProjectCreationDataModelProperties.NESTED_MODEL_SERVER_TARGET"; //$NON-NLS-1$
     
     /**
-     * An optional dataModel property for a <code>Boolean</code> type. The default value is
-     * <code>Boolean.TRUE</code>. If this property is set to <code>Boolean.TRUE</code> then the
-     * server target specified by dataModel property <code>SERVER_TARGET_ID</code> will be set on
-     * the generated artifact.
+     * Optional, type Boolean The default value is <code>Boolean.TRUE</code>. If this property is set 
+     * to <code>Boolean.TRUE</code> then the server target specified by dataModel property <code>SERVER_TARGET_ID</code> 
+     * will be set on the generated artifact.
      * 
      * @see SERVER_TARGET_ID
      */
     public static final String ADD_SERVER_TARGET = "IFlexibleProjectCreationDataModelProperties.ADD_SERVER_TARGET"; //$NON-NLS-1$
     /**
-     * An optional dataModel property for a <code>java.lang.String</code> type. This is used to
-     * specify the server target and is required if the <code>ADD_SERVER_TARGET</code> property is
-     * set to <code>Boolean.TRUE</code>.
+     * Optional, type String. This is used to specify the server target and is required if 
+     * the <code>ADD_SERVER_TARGET</code> property is set to <code>Boolean.TRUE</code>.
      * 
-     * @see ServerTargetDataModel.RUNTIME_TARGET_ID
+     * @see org.eclipse.jst.j2ee.internal.servertarget.ServerTargetDataModel
      */
     public static final String SERVER_TARGET_ID = IJ2EEProjectServerTargetDataModelProperties.RUNTIME_TARGET_ID;
 
