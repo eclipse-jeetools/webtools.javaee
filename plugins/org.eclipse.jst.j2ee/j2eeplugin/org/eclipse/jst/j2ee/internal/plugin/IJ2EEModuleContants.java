c7fe0cfb9990641f9737e1de6f113e81a80584f9
vbhadrir <vbhadrir>
2005-04-15 19:25:04 +0000

    Adding IJ2EEModuleContants

---
+++ plugins/org.eclipse.jst.j2ee/j2eeplugin/org/eclipse/jst/j2ee/internal/plugin/IJ2EEModuleContants.java
@@ -0,0 +1,9 @@
+package org.eclipse.jst.j2ee.internal.plugin;
+
+import org.eclipse.wst.common.componentcore.internal.util.IModuleConstants;
+
+public interface IJ2EEModuleContants extends IModuleConstants {
+	
+	String J2EE_PLUGIN_ID = "org.eclipse.jst.j2ee"; //$NON-NLS-1$
+
+}
