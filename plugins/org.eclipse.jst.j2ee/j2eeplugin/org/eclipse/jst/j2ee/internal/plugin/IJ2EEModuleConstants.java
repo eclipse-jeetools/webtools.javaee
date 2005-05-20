f8c614b3528d75b125dd75028137ab5b10c9127b
cbridgha <cbridgha>
2005-05-20 21:02:34 +0000

    This commit was manufactured by cvs2svn to create tag 'I20050526'.

--- plugins/org.eclipse.jst.j2ee/j2eeplugin/org/eclipse/jst/j2ee/internal/plugin/IJ2EEModuleConstants.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.jst.j2ee.internal.plugin;
-
-import org.eclipse.wst.common.componentcore.internal.util.IModuleConstants;
-
-public interface IJ2EEModuleConstants extends IModuleConstants {
-	
-	String J2EE_PLUGIN_ID = "org.eclipse.jst.j2ee"; //$NON-NLS-1$
-
-}
