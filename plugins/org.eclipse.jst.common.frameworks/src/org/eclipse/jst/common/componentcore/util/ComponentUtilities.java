5e60fcc98507451654465446192e6cab5564ff77
vbhadrir <vbhadrir>
2005-06-01 21:41:51 +0000

    removed deprecation api usage

--- plugins/org.eclipse.jst.common.frameworks/src/org/eclipse/jst/common/componentcore/util/ComponentUtilities.java
+++ plugins/org.eclipse.jst.common.frameworks/src/org/eclipse/jst/common/componentcore/util/ComponentUtilities.java
@@ -7,6 +7,7 @@ import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourceAttributes;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.URI;
@@ -65,11 +66,13 @@ public class ComponentUtilities {
 	 */
 	public static void ensureContainerNotReadOnly(IResource resource) {
 		if (resource != null && !resource.exists()) { //it must be new
-			IContainer container = resource.getParent();
-			while (container != null && !container.isReadOnly())
-				container = container.getParent();
-			if (container != null)
-				container.setReadOnly(false);
+			IContainer container = resource.getParent(); 
+			if (container != null ) {
+				ResourceAttributes attr = container.getResourceAttributes();
+				if(!attr.isReadOnly())
+					container = container.getParent();
+				attr.setReadOnly(false);
+			}	
 		}
 	}
 
