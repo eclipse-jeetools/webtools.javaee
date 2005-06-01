a6021dbddafd6314d52cdce204cd335cfda09516
vbhadrir <vbhadrir>
2005-06-01 19:23:43 +0000

    [nobug] continue creation port, remove old classes. Committing for DFH

--- plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/internal/ejb/project/EJBEditModel.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/internal/ejb/project/EJBEditModel.java
@@ -16,7 +16,6 @@ import java.util.Set;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jst.common.jdt.internal.integration.WorkingCopyProvider;
-import org.eclipse.jst.j2ee.application.internal.operations.DefaultModuleProjectCreationOperation;
 import org.eclipse.jst.j2ee.ejb.EJBResource;
 import org.eclipse.jst.j2ee.internal.J2EEConstants;
 import org.eclipse.jst.j2ee.internal.common.XMLResource;
@@ -245,6 +244,6 @@ public class EJBEditModel extends AbstractEJBEditModel implements WorkingCopyPro
 	}
 
 	public String getDevelopmentAcivityID() {
-		return DefaultModuleProjectCreationOperation.ENTERPRISE_JAVA;
+		return ENTERPRISE_JAVA;
 	}
 }
