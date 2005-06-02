ceb621281f91846f9e95c3bac2e21ee9bae015bd
vbhadrir <vbhadrir>
2005-06-02 22:34:25 +0000

    added asEJBJarFile

--- plugins/org.eclipse.jst.j2ee/common/org/eclipse/jst/j2ee/internal/common/J2EEArtifactEditUtilities.java
+++ plugins/org.eclipse.jst.j2ee/common/org/eclipse/jst/j2ee/internal/common/J2EEArtifactEditUtilities.java
@@ -2,6 +2,7 @@ package org.eclipse.jst.j2ee.internal.common;
 
 import org.eclipse.jst.j2ee.commonarchivecore.internal.ApplicationClientFile;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.EARFile;
+import org.eclipse.jst.j2ee.commonarchivecore.internal.EJBJarFile;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.exception.OpenFailureException;
 import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 
@@ -27,6 +28,29 @@ public class J2EEArtifactEditUtilities {
 		
 		return null;
 		
+	}
+	
+/**
+	 * @return the EARFile representation of the IVirtualComponent
+	 * @throws OpenFailureException
+	 */
+	
+	public static EJBJarFile asEJBJarFile(IVirtualComponent virtualComponent) throws OpenFailureException {
+		/*IProject proj = getProject();
+		if (proj == null)
+			return null;
+
+		if (isBinaryProject()) {
+			String location = ((J2EEModuleWorkbenchURIConverterImpl) getJ2EEWorkbenchURIConverter()).getInputJARLocation().toOSString();
+			ArchiveOptions options = new ArchiveOptions();
+			options.setIsReadOnly(true);
+			return getCommonArchiveFactory().openEJB11JarFile(options, location);
+		}
+		EJBProjectLoadStrategyImpl loader = new EJBProjectLoadStrategyImpl(proj);
+		loader.setExportSource(shouldExportSource);
+		loader.setResourceSet(this.getResourceSet());
+		return getCommonArchiveFactory().openEJB11JarFile(loader, proj.getName());*/
+		return null;
 	}
 	
 	public static ApplicationClientFile asApplicationClientFile(IVirtualComponent virtualComponent, boolean shouldExportSource) throws OpenFailureException {
