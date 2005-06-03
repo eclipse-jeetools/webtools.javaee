dc8cca4afd0b97ce0c54d66a599b8beada6130a6
vbhadrir <vbhadrir>
2005-06-03 14:23:46 +0000

    removed asEJBJarFile needs to be EJBArtifactEditUtilities

--- plugins/org.eclipse.jst.j2ee/common/org/eclipse/jst/j2ee/internal/common/J2EEArtifactEditUtilities.java
+++ plugins/org.eclipse.jst.j2ee/common/org/eclipse/jst/j2ee/internal/common/J2EEArtifactEditUtilities.java
@@ -1,9 +1,11 @@
 package org.eclipse.jst.j2ee.internal.common;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.ApplicationClientFile;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.EARFile;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.EJBJarFile;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.exception.OpenFailureException;
+import org.eclipse.jst.j2ee.internal.archive.operations.EJBArchiveOpsResourceHandler;
 import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 
 public class J2EEArtifactEditUtilities {
@@ -30,29 +32,6 @@ public class J2EEArtifactEditUtilities {
 		
 	}
 	
-/**
-	 * @return the EARFile representation of the IVirtualComponent
-	 * @throws OpenFailureException
-	 */
-	
-	public static EJBJarFile asEJBJarFile(IVirtualComponent virtualComponent) throws OpenFailureException {
-		/*IProject proj = getProject();
-		if (proj == null)
-			return null;
-
-		if (isBinaryProject()) {
-			String location = ((J2EEModuleWorkbenchURIConverterImpl) getJ2EEWorkbenchURIConverter()).getInputJARLocation().toOSString();
-			ArchiveOptions options = new ArchiveOptions();
-			options.setIsReadOnly(true);
-			return getCommonArchiveFactory().openEJB11JarFile(options, location);
-		}
-		EJBProjectLoadStrategyImpl loader = new EJBProjectLoadStrategyImpl(proj);
-		loader.setExportSource(shouldExportSource);
-		loader.setResourceSet(this.getResourceSet());
-		return getCommonArchiveFactory().openEJB11JarFile(loader, proj.getName());*/
-		return null;
-	}
-	
 	public static ApplicationClientFile asApplicationClientFile(IVirtualComponent virtualComponent, boolean shouldExportSource) throws OpenFailureException {
 		
 		/*IProject proj = getProject();
@@ -70,5 +49,4 @@ public class J2EEArtifactEditUtilities {
 		return null;
 	}
 	
-
 }
