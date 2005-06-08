9716470c0143dbbbe20a2a788e776a3ed2e33fbb
cbridgha <cbridgha>
2005-06-08 17:13:02 +0000

    replace workbenchComponent with IVirtualComponent

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/application/internal/operations/AddComponentToEnterpriseApplicationOperation.java
+++ plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/application/internal/operations/AddComponentToEnterpriseApplicationOperation.java
@@ -2,6 +2,7 @@ package org.eclipse.jst.j2ee.application.internal.operations;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Properties;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
@@ -19,15 +20,15 @@ import org.eclipse.jst.j2ee.componentcore.util.EARArtifactEdit;
 import org.eclipse.jst.j2ee.internal.J2EEConstants;
 import org.eclipse.jst.j2ee.internal.application.ApplicationPackage;
 import org.eclipse.jst.j2ee.internal.modulecore.util.EARArtifactEditOperation;
-import org.eclipse.wst.common.componentcore.internal.ComponentcoreFactory;
-import org.eclipse.wst.common.componentcore.internal.Property;
-import org.eclipse.wst.common.componentcore.internal.ReferencedComponent;
+import org.eclipse.wst.common.componentcore.ComponentCore;
 import org.eclipse.wst.common.componentcore.internal.StructureEdit;
 import org.eclipse.wst.common.componentcore.internal.WorkbenchComponent;
 import org.eclipse.wst.common.componentcore.internal.impl.ModuleURIUtil;
 import org.eclipse.wst.common.componentcore.internal.operation.ArtifactEditOperationDataModel;
 import org.eclipse.wst.common.componentcore.internal.util.IModuleConstants;
 import org.eclipse.wst.common.componentcore.resources.ComponentHandle;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
+import org.eclipse.wst.common.componentcore.resources.IVirtualReference;
 
 public class AddComponentToEnterpriseApplicationOperation extends EARArtifactEditOperation {
 	public static final String metaInfFolderDeployPath = "/"; //$NON-NLS-1$
@@ -41,32 +42,26 @@ public class AddComponentToEnterpriseApplicationOperation extends EARArtifactEdi
 	}
 
 	private void addComponentToEnterpriseApplication(IProgressMonitor monitor) {
-		StructureEdit moduleCore = null;
-		try{
-			String earProj = operationDataModel.getStringProperty(AddComponentToEnterpriseApplicationDataModel.EAR_PROJECT_NAME);
-			IProject proj = ProjectUtilities.getProject(earProj);
-			
-			moduleCore = StructureEdit.getStructureEditForWrite(proj);
-			WorkbenchComponent earComp = moduleCore.findComponentByName(operationDataModel.getStringProperty(AddComponentToEnterpriseApplicationDataModel.EAR_MODULE_NAME));
-			
-			AddComponentToEnterpriseApplicationDataModel dm = (AddComponentToEnterpriseApplicationDataModel)getOperationDataModel();
-			IPath runtimePath = new Path(metaInfFolderDeployPath);
-			List list = (List)dm.getProperty(AddComponentToEnterpriseApplicationDataModel.MODULE_LIST);
-			if (list != null && list.size() > 0) {
-				for (int i = 0; i < list.size(); i++) {
-					ReferencedComponent rc = ComponentcoreFactory.eINSTANCE.createReferencedComponent();
-					WorkbenchComponent wc = (WorkbenchComponent)list.get(i);
-					rc.setHandle(normalize(earComp, wc));
-					rc.setRuntimePath(runtimePath);
-					earComp.getReferencedComponents().add(rc);
-				}
+		// get ear component
+		String earProjName = operationDataModel.getStringProperty(AddComponentToEnterpriseApplicationDataModel.EAR_PROJECT_NAME);
+		IProject earProj = ProjectUtilities.getProject(earProjName);
+		String earCompName = operationDataModel.getStringProperty(AddComponentToEnterpriseApplicationDataModel.EAR_MODULE_NAME);
+		IVirtualComponent earComp = ComponentCore.createComponent(earProj, earCompName);
+		// set up references
+		AddComponentToEnterpriseApplicationDataModel dm = (AddComponentToEnterpriseApplicationDataModel)getOperationDataModel();
+		IPath runtimePath = new Path(metaInfFolderDeployPath);
+		List list = (List)dm.getProperty(AddComponentToEnterpriseApplicationDataModel.MODULE_LIST);
+		int size = list.size();
+		if (list != null && size > 0) {
+			IVirtualReference[] refs = new IVirtualReference[size];
+			for (int i = 0; i < size; i++) {
+				IVirtualComponent refedComp = (IVirtualComponent)list.get(i);
+				refs[i] = ComponentCore.createReference(earComp, refedComp);
+				refs[i].setRuntimePath(runtimePath);
 			}
-			moduleCore.saveIfNecessary(monitor); 
-		} finally {
-		       if (null != moduleCore) {
-		            moduleCore.dispose();
-		       }
-		 }		
+			earComp.setReferences(refs);
+		}
+		
 		updateEARDD(monitor);
 	}
 
@@ -83,8 +78,8 @@ public class AddComponentToEnterpriseApplicationOperation extends EARArtifactEdi
 				List list = (List)dm.getProperty(AddComponentToEnterpriseApplicationDataModel.MODULE_LIST);
 				if (list != null && list.size() > 0) {
 					for (int i = 0; i < list.size(); i++) {
-						WorkbenchComponent wc = (WorkbenchComponent)list.get(i);
-						addModule(application, wc);	
+						IVirtualComponent comp = (IVirtualComponent)list.get(i);
+						addModule(application, comp);	
 					}
 				}
 			}	
@@ -97,9 +92,10 @@ public class AddComponentToEnterpriseApplicationOperation extends EARArtifactEdi
 				earEdit.dispose();
        	}		
 	}
-	protected Module createNewModule(WorkbenchComponent wc) {
+
+	protected Module createNewModule(IVirtualComponent vc) {
 		
-		String type = wc.getComponentType().getComponentTypeId();
+		String type = vc.getComponentTypeId();
 		if ( type.equals(IModuleConstants.JST_WEB_MODULE) ){
 			return ((ApplicationPackage) EPackage.Registry.INSTANCE.getEPackage(ApplicationPackage.eNS_URI)).getApplicationFactory().createWebModule();
 		}else if ( type.equals(IModuleConstants.JST_EJB_MODULE)) {
@@ -112,14 +108,10 @@ public class AddComponentToEnterpriseApplicationOperation extends EARArtifactEdi
 		return null;
 	}
 	
-	protected void addModule(Application application, WorkbenchComponent wc) {
-		Application dd = application;
-		
-		Module m = createNewModule(wc);
-
-		String name = wc.getName();
-		String type = wc.getComponentType().getComponentTypeId();
-		
+	protected void addModule(Application application, IVirtualComponent vc) {
+		Module m = createNewModule(vc);
+		String name = vc.getName();
+		String type = vc.getComponentTypeId();
 		if ( type.equals(IModuleConstants.JST_WEB_MODULE) ){
 			name += ".war"; //$NON-NLS-1$
 		}else if ( type.equals(IModuleConstants.JST_EJB_MODULE)) {
@@ -129,27 +121,18 @@ public class AddComponentToEnterpriseApplicationOperation extends EARArtifactEdi
 		}else if ( type.equals(IModuleConstants.JST_CONNECTOR_MODULE)) {
 			name += ".rar"; //$NON-NLS-1$
 		}
-		
 		if( m!= null){
 			m.setUri(name);
 			if (m instanceof WebModule) {
-				
-				List props = wc.getComponentType().getProperties();
-				String contextroot = ""; //$NON-NLS-1$
-				for( int i=0; i< props.size(); i++){
-					Property prop = (Property)props.get(i);
-					if( prop.getName().equals(J2EEConstants.CONTEXTROOT)){
-						contextroot = prop.getValue();
-						break;
-					}
-				}
-				((WebModule) m).setContextRoot(contextroot);
-			}		
-			dd.getModules().add(m);	
+				// set up context root
+				Properties props = vc.getMetaProperties();
+				String contextroot = props.getProperty(J2EEConstants.CONTEXTROOT);
+				if (contextroot != null)
+					((WebModule) m).setContextRoot(contextroot);
+			}
+			application.getModules().add(m);	
 		}	
-	}
-	
-	
+	}	
 	
 	private URI normalize(WorkbenchComponent aComponent, WorkbenchComponent aReferencedComponent) {   
 		// same project doesn't matter
