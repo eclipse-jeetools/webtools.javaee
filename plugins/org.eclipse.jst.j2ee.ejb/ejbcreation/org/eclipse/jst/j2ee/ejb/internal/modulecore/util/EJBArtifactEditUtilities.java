692e6764c5bc7e379894bc5d45096e21bda5a4d2
vbhadrir <vbhadrir>
2005-06-09 15:22:50 +0000

    *** empty log message ***

--- plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/ejb/internal/modulecore/util/EJBArtifactEditUtilities.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/ejb/internal/modulecore/util/EJBArtifactEditUtilities.java
@@ -3,8 +3,6 @@ package org.eclipse.jst.j2ee.ejb.internal.modulecore.util;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jem.util.emf.workbench.ProjectUtilities;
 import org.eclipse.jem.util.emf.workbench.WorkbenchURIConverter;
-import org.eclipse.jst.j2ee.commonarchivecore.internal.EJBJarFile;
-import org.eclipse.jst.j2ee.commonarchivecore.internal.exception.OpenFailureException;
 import org.eclipse.jst.j2ee.ejb.EJBJar;
 import org.eclipse.jst.j2ee.ejb.EnterpriseBean;
 import org.eclipse.jst.j2ee.ejb.componentcore.util.EJBArtifactEdit;
@@ -16,25 +14,6 @@ import org.eclipse.wst.common.componentcore.resources.IFlexibleProject;
 import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 
 public class EJBArtifactEditUtilities {
-
-	public static EJBJarFile asEJBJarFile(IVirtualComponent virtualComponent, boolean shouldExportSource) throws OpenFailureException {
-
-		/*IProject proj = getProject();
-		if (proj == null)
-			return null;
-
-		if (isBinaryProject()) {
-			String location = ((J2EEModuleWorkbenchURIConverterImpl) getJ2EEWorkbenchURIConverter()).getInputJARLocation().toOSString();
-			ArchiveOptions options = new ArchiveOptions();
-			options.setIsReadOnly(true);
-			return getCommonArchiveFactory().openEJB11JarFile(options, location);
-		}
-		EJBProjectLoadStrategyImpl loader = new EJBProjectLoadStrategyImpl(proj);
-		loader.setExportSource(shouldExportSource);
-		loader.setResourceSet(this.getResourceSet());
-		return getCommonArchiveFactory().openEJB11JarFile(loader, proj.getName());*/
-		return null;
-	}
 	
 	public static IVirtualComponent getEJBComponent(EnterpriseBean bean) {
 		IProject project = ProjectUtilities.getProject(bean);
