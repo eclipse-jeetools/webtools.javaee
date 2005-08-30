a8d332c16f177614284c75ce65730daf5197556d
cbridgha <cbridgha>
2005-08-30 14:21:20 +0000

    [108023] Removing UI dep

--- plugins/org.eclipse.jst.servlet.ui/servlet_ui/org/eclipse/jst/servlet/ui/internal/deployables/WebDeployableArtifactAdapterFactory.java
+++ plugins/org.eclipse.jst.servlet.ui/servlet_ui/org/eclipse/jst/servlet/ui/internal/deployables/WebDeployableArtifactAdapterFactory.java
@@ -4,10 +4,11 @@
  * TODO To change the template for this generated file go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-package org.eclipse.jst.j2ee.internal.web.deployables;
+package org.eclipse.jst.servlet.ui.internal.deployables;
 
 import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.debug.ui.actions.ILaunchable;
+import org.eclipse.jst.j2ee.internal.web.deployables.WebDeployableArtifactUtil;
 import org.eclipse.wst.server.core.IModuleArtifact;
 import org.eclipse.wst.server.core.model.ModuleArtifactAdapterDelegate;
 
