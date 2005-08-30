32b9768aeabbba49a7d7187ad0e69959014a99f4
cbridgha <cbridgha>
2005-08-30 01:28:25 +0000

    [nobug] Removing UI dep

--- plugins/org.eclipse.jst.ejb.ui/ejb_ui/org/eclipse/jst/ejb/ui/internal/deployables/EJBDeployableArtifactAdapterFactory.java
+++ plugins/org.eclipse.jst.ejb.ui/ejb_ui/org/eclipse/jst/ejb/ui/internal/deployables/EJBDeployableArtifactAdapterFactory.java
@@ -4,10 +4,11 @@
  * TODO To change the template for this generated file go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-package org.eclipse.jst.j2ee.ejb.internal.deployables;
+package org.eclipse.jst.ejb.ui.internal.deployables;
 
 import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.debug.ui.actions.ILaunchable;
+import org.eclipse.jst.j2ee.ejb.internal.deployables.EJBDeployableArtifactAdapterUtil;
 import org.eclipse.wst.server.core.IModuleArtifact;
 import org.eclipse.wst.server.core.model.ModuleArtifactAdapterDelegate;
 
