6a7a68061bc795d516e5f6f992cc64a37ea81b91
vbhadrir <vbhadrir>
2005-09-02 19:37:51 +0000

    [108412] fixed findFile in compUtilities: committed for JL

--- tests/org.eclipse.jst.j2ee.tests/ejbmodule-tests/org/eclipse/wtp/j2ee/headless/tests/ejb/operations/EJBProjectCreationOperationTest.java
+++ tests/org.eclipse.jst.j2ee.tests/ejbmodule-tests/org/eclipse/wtp/j2ee/headless/tests/ejb/operations/EJBProjectCreationOperationTest.java
@@ -1,17 +1,23 @@
 
 package org.eclipse.wtp.j2ee.headless.tests.ejb.operations;
 
+import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jst.common.componentcore.util.ComponentUtilities;
 import org.eclipse.jst.j2ee.internal.ejb.archiveoperations.EjbComponentCreationDataModelProvider;
+import org.eclipse.wst.common.componentcore.internal.util.IModuleConstants;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wtp.j2ee.headless.tests.j2ee.operations.ModuleProjectCreationOperationTest;
 
 public class EJBProjectCreationOperationTest extends ModuleProjectCreationOperationTest {
 
-    /**
+	 /**
 	 * @param name
 	 */
 	public EJBProjectCreationOperationTest(String name) {
@@ -27,7 +33,19 @@ public class EJBProjectCreationOperationTest extends ModuleProjectCreationOperat
      * @see org.eclipse.wtp.j2ee.headless.tests.j2ee.operations.ModuleProjectCreationOperationTest#getProjectCreationDataModel()
      */
     public IDataModel getComponentCreationDataModel() {
-        return DataModelFactory.createDataModel(new EjbComponentCreationDataModelProvider());
+    	return DataModelFactory.createDataModel(new EjbComponentCreationDataModelProvider());
+    }
+    
+    public void testFindFilesUtility() {
+    	IFile file = null;
+    	try {
+    		testDefaults();
+    		IVirtualComponent comp = ComponentUtilities.getAllComponentsInWorkspaceOfType(IModuleConstants.JST_EJB_MODULE)[0];
+    		file = ComponentUtilities.findFile(comp, new Path("/META-INF/ejb-jar.xml")); //$NON-NLS-1$
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	Assert.assertTrue(file != null && file.exists());
     }
   
 }
