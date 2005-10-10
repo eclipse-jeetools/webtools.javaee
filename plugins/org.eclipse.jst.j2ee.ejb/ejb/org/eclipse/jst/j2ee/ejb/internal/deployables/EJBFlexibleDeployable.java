449801bf26e1cc36d53897048a3a7ff64e1cbe82
vbhadrir <vbhadrir>
2005-10-10 14:08:26 +0000

    removed old methods from deployables: committed for JL

--- plugins/org.eclipse.jst.j2ee.ejb/ejb/org/eclipse/jst/j2ee/ejb/internal/deployables/EJBFlexibleDeployable.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejb/org/eclipse/jst/j2ee/ejb/internal/deployables/EJBFlexibleDeployable.java
@@ -7,8 +7,6 @@
 package org.eclipse.jst.j2ee.ejb.internal.deployables;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jst.j2ee.ejb.EJBJar;
 import org.eclipse.jst.j2ee.ejb.componentcore.util.EJBArtifactEdit;
 import org.eclipse.jst.j2ee.internal.EjbModuleExtensionHelper;
@@ -16,27 +14,20 @@ import org.eclipse.jst.j2ee.internal.IEJBModelExtenderManager;
 import org.eclipse.jst.j2ee.internal.common.J2EEVersionUtil;
 import org.eclipse.jst.j2ee.internal.deployables.J2EEFlexProjDeployable;
 import org.eclipse.jst.server.core.IEJBModule;
-import org.eclipse.wst.common.componentcore.internal.util.IModuleConstants;
 import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 
 
 public class EJBFlexibleDeployable extends J2EEFlexProjDeployable implements IEJBModule {
 
-	public static String EJB_TYPE = IModuleConstants.JST_EJB_MODULE;
-
 	public EJBFlexibleDeployable(IProject project, String aFactoryId, IVirtualComponent aComponent) {
 		super(project, aFactoryId, aComponent);
-
-
 	}
 
 	public String getJ2EESpecificationVersion() {
 		if (component != null)
 			return J2EEVersionUtil.convertVersionIntToString(J2EEVersionUtil.convertEJBVersionStringToJ2EEVersionID(component.getVersion()));
-		else
-			return null;
+		return null;
 	}
-
 
 	public String getJNDIName(String ejbName) {
 		EjbModuleExtensionHelper modHelper = null;
@@ -54,28 +45,16 @@ public class EJBFlexibleDeployable extends J2EEFlexProjDeployable implements IEJ
 			if (ejbEdit != null)
 				ejbEdit.dispose();
 		}
-
-
 		return modHelper == null ? null : modHelper.getJNDIName(jar, jar.getEnterpriseBeanNamed(ejbName));
-
 	}
 
 	public String getType() {
 		return "j2ee.ejb"; //$NON-NLS-1$
 	}
 
-
-
-	public IStatus validate(IProgressMonitor monitor) {
-		return null;
-	}
-
-
 	public String getEJBSpecificationVersion() {
 		if (component != null)
 			return component.getVersion();
-		else
-			return null;
+		return null;
 	}
-
 }
