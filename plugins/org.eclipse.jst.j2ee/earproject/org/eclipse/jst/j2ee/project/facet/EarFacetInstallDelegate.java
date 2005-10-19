7cf1d3def5240dd295b82af1c597708eecb331d8
vbhadrir <vbhadrir>
2005-10-19 15:42:48 +0000

    [94609] removed component type and reaction to facets: committed for JL

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/project/facet/EarFacetInstallDelegate.java
+++ plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/project/facet/EarFacetInstallDelegate.java
@@ -12,7 +12,6 @@
 package org.eclipse.jst.j2ee.project.facet;
 
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -20,134 +19,21 @@ import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.project.facet.core.IDelegate;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 
-/**
- * @author <a href="mailto:kosta@bea.com">Konstantin Komissarchik</a>
- */
+public final class EarFacetInstallDelegate implements IDelegate {
+	
+	public void execute(final IProject project, final IProjectFacetVersion fv, final Object cfg, final IProgressMonitor monitor) throws CoreException {
+		if (monitor != null)
+			monitor.beginTask("", 1); //$NON-NLS-1$
 
-public final class EarFacetInstallDelegate 
-
-    implements IDelegate
-    
-{
-    public void execute( final IProject project, 
-                         final IProjectFacetVersion fv,
-                         final Object cfg,
-                         final IProgressMonitor monitor )
-    
-        throws CoreException
-        
-    {
-        if( monitor != null )
-        {
-            monitor.beginTask( "", 1 );
-        }
-        
-    	IDataModel model = (IDataModel)cfg;
-    	try {
+		IDataModel model = (IDataModel) cfg;
+		try {
 			model.getDefaultOperation().execute(monitor, null);
 		} catch (ExecutionException e) {
 			e.printStackTrace();
+		} finally {
+			if (monitor != null) {
+				monitor.done();
+			}
 		}
-       
-
-    	
-//        try
-//        {
-//            final EarFacetInstallConfig config 
-//                = cfg != null 
-//                  ? (EarFacetInstallConfig) cfg 
-//                  : new EarFacetInstallConfig();
-//
-//            // Add WTP natures.
-//          
-//            WtpUtils.addNatures( project );
-//          
-//            // Create the directory structure.
-//            
-//            final IWorkspace ws = ResourcesPlugin.getWorkspace();
-//            final IPath pjpath = project.getFullPath();
-//            
-//            final IPath earcontent = pjpath.append( config.getContentDir() );
-//            mkdirs( ws.getRoot().getFolder( earcontent ) );
-//
-//            final IPath metainf = earcontent.append( "META-INF" );
-//            mkdirs( ws.getRoot().getFolder( metainf ) );
-//            
-//            // Setup the flexible project structure. 
-//            
-//            final IVirtualComponent c
-//                = ComponentCore.createComponent( project, project.getName() );
-//
-//            c.create( 0, null );
-//
-//            final ComponentType ctype 
-//                = ComponentcoreFactory.eINSTANCE.createComponentType();
-//            
-//            ctype.setComponentTypeId( "jst.ear" );
-//            ctype.setVersion( fv.getVersionString() );
-//            
-//            final StructureEdit edit 
-//                = StructureEdit.getStructureEditForWrite( project );
-//            
-//            try
-//            {
-//                StructureEdit.setComponentType( c, ctype );
-//                edit.saveIfNecessary( null );
-//            }
-//            finally
-//            {
-//                edit.dispose();
-//            }
-//            
-//            final IVirtualFolder approot = c.getRootFolder();
-//            
-//            approot.createLink( new Path( "/" + config.getContentDir() ), 
-//                                0, null );
-//            
-//            // Create the deployment descriptor (application.xml).
-//            
-//            
-//            final EARArtifactEdit eardd
-//                = EARArtifactEdit.getEARArtifactEditForWrite( project );
-//            
-//            try
-//            {
-//                eardd.createModelRoot( 14 );
-//                eardd.saveIfNecessary( null );
-//            }
-//            finally
-//            {
-//                eardd.dispose();
-//            }
-            
-//            if( monitor != null )
-//            {
-//                monitor.worked( 1 );
-//            }
-//        }
-        finally
-        {
-            if( monitor != null )
-            {
-                monitor.done();
-            }
-        }
-    }
-    
-    private static void mkdirs( final IFolder folder )
-    
-        throws CoreException
-        
-    {
-        if( ! folder.exists() )
-        {
-            if( folder.getParent() instanceof IFolder )
-            {
-                mkdirs( (IFolder) folder.getParent() );
-            }
-            
-            folder.create( true, true, null );
-        }
-    }
-
+	}
 }
