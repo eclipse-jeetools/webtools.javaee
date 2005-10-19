9d53579208ec6fd9f2f7edca5b6b473dec496de0
vbhadrir <vbhadrir>
2005-10-19 15:42:45 +0000

    [94609] removed component type and reaction to facets: committed for JL

--- plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/ejb/project/facet/EjbFacetInstallDelegate.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/ejb/project/facet/EjbFacetInstallDelegate.java
@@ -8,23 +8,21 @@ import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.project.facet.core.IDelegate;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 
+public class EjbFacetInstallDelegate implements IDelegate {
 
-public class EjbFacetInstallDelegate implements IDelegate{
-
-	public void execute(IProject project,
-			IProjectFacetVersion fv, Object config,
-			IProgressMonitor monitor) throws CoreException {
-
-    	IDataModel model = (IDataModel)config;
-    	try {
+	public void execute(IProject project, IProjectFacetVersion fv, Object config, IProgressMonitor monitor) throws CoreException {
+		if (monitor != null)
+			monitor.beginTask("", 1); //$NON-NLS-1$
+		
+		IDataModel model = (IDataModel) config;
+		try {
 			model.getDefaultOperation().execute(monitor, null);
 		} catch (ExecutionException e) {
 			e.printStackTrace();
+		} finally {
+			if (monitor != null) {
+				monitor.done();
+			}
 		}
-		if(true){
-			return;
-		}
-		
 	}
-
 }
