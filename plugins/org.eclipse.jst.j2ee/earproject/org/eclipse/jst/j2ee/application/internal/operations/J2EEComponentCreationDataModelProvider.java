b1feb94b9c67dd6a7eb44519b11adb12f45fa6bb
vbhadrir <vbhadrir>
2005-10-25 20:30:42 +0000

    [113621] reaction to deployable changes: committed for JL

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/application/internal/operations/J2EEComponentCreationDataModelProvider.java
+++ plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/application/internal/operations/J2EEComponentCreationDataModelProvider.java
@@ -206,8 +206,7 @@ public abstract class J2EEComponentCreationDataModelProvider extends JavaCompone
 			return MODULEVERSION_NOT_SUPPORTED;
 		}
 		Integer version = (Integer) model.getProperty(COMPONENT_VERSION);
-		int nj2eeVer = convertModuleVersionToJ2EEVersion(version.intValue());
-		String j2eeVer = J2EEVersionUtil.getJ2EETextVersion(nj2eeVer);
+		String j2eeVer = J2EEVersionUtil.convertVersionIntToString(version.intValue());
 		return isTypeSupported(runtime.getRuntimeType(), getJ2EEProjectType(), j2eeVer);
 	}
 	
