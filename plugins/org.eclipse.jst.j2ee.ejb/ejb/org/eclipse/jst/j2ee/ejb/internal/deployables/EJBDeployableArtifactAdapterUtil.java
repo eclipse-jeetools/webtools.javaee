9e50d241954e11fc8179cb7aca67b071ecd2deb7
vbhadrir <vbhadrir>
2005-10-25 14:58:24 +0000

    [113621] reaction to deployable changes: committed for JL

--- plugins/org.eclipse.jst.j2ee.ejb/ejb/org/eclipse/jst/j2ee/ejb/internal/deployables/EJBDeployableArtifactAdapterUtil.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejb/org/eclipse/jst/j2ee/ejb/internal/deployables/EJBDeployableArtifactAdapterUtil.java
@@ -140,7 +140,7 @@ public class EJBDeployableArtifactAdapterUtil {
 
 	protected static IModule getModule(IProject project, IVirtualComponent component) {
 		IModule deployable = null;
-		Iterator iterator = Arrays.asList(ServerUtil.getModules("j2ee.ejb")).iterator();  //$NON-NLS-1$
+		Iterator iterator = Arrays.asList(ServerUtil.getModules(J2EEProjectUtilities.EJB)).iterator();  //$NON-NLS-1$
 		String componentName = null;
 		if (component != null)
 			componentName = component.getName();
