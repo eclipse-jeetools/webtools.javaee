f09a6724c8b78cb7474d5a21098fad8f4aa56e6c
jeffliu <jeffliu>
2005-10-26 05:16:22 +0000

    [111234] Fix up all the copyrights in WTP

--- plugins/org.eclipse.jst.common.annotations.controller/controller/org/eclipse/jst/common/internal/annotations/registry/AnnotationTagDynamicInitializer.java
+++ plugins/org.eclipse.jst.common.annotations.controller/controller/org/eclipse/jst/common/internal/annotations/registry/AnnotationTagDynamicInitializer.java
@@ -1,14 +1,13 @@
-/* ***************************************************************************************************
- * Licensed Materials - Property of IBM
- *
- * 5724-I66
+/*******************************************************************************
+ * Copyright (c) 2003, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
- * © Copyright IBM Corporation 2004. All Rights Reserved.
- * 
- * Note to U.S. Government Users Restricted Rights:  Use, duplication or disclosure restricted by GSA 
- * ADP  Schedule Contract with IBM Corp.
- *  
- *****************************************************************************************************/
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.jst.common.internal.annotations.registry;
 
 /**
