fa2d735a255e0299cb0dd6d5828f113c8f7ac80c
cbridgha <cbridgha>
2005-10-26 05:16:23 +0000

    This commit was manufactured by cvs2svn to create tag 'v20051026_0121'.

--- plugins/org.eclipse.jst.j2ee/j2eecreation/org/eclipse/jst/j2ee/project/facet/IJ2EEFacetInstallDataModelProperties.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.jst.j2ee.project.facet;
-
-import org.eclipse.wst.common.componentcore.datamodel.properties.IFacetInstalDataModelProperties;
-
-public interface IJ2EEFacetInstallDataModelProperties extends IFacetInstalDataModelProperties {
-
-	public static final String EAR_PROJECT_NAME = "IJ2EEFacetInstallDataModelProperties.EAR_PROJECT_NAME";
-	public static final String CONFIG_FOLDER = "IJ2EEFacetInstallDataModelProperties.CONFIG_FOLDER";	
-}
