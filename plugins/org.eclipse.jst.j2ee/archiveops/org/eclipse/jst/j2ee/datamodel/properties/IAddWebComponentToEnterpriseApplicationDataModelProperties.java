ffb7061fe83023fe8a83ceb102c0e40aac5a12f9
cbridgha <cbridgha>
2005-11-01 03:38:20 +0000

    This commit was manufactured by cvs2svn to create tag 'v20051031_2240'.

--- plugins/org.eclipse.jst.j2ee/archiveops/org/eclipse/jst/j2ee/datamodel/properties/IAddWebComponentToEnterpriseApplicationDataModelProperties.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.jst.j2ee.datamodel.properties;
-
-import org.eclipse.wst.common.componentcore.datamodel.properties.ICreateReferenceComponentsDataModelProperties;
-
-public interface IAddWebComponentToEnterpriseApplicationDataModelProperties extends ICreateReferenceComponentsDataModelProperties{
-
-	public static final String CONTEXT_ROOT = "IAddWebComponentToEnterpriseApplicationDataModelProperties.CONTEXT_ROOT"; //$NON-NLS-1$
-	
-}
