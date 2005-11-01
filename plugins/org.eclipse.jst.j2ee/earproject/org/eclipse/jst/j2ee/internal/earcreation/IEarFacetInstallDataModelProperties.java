ff61f367e3deed2210572d7fadfd4742d6ce4052
cbridgha <cbridgha>
2005-11-01 05:20:42 +0000

    This commit was manufactured by cvs2svn to create tag 'v20051101_0020'.

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/internal/earcreation/IEarFacetInstallDataModelProperties.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.jst.j2ee.internal.earcreation;
-
-import org.eclipse.jst.j2ee.project.facet.IJ2EEFacetInstallDataModelProperties;
-import org.eclipse.wst.common.componentcore.datamodel.properties.IFacetInstalDataModelProperties;
-
-public interface IEarFacetInstallDataModelProperties extends IFacetInstalDataModelProperties {
-
-	public static final String RUNTIME_TARGET_ID = IJ2EEFacetInstallDataModelProperties.RUNTIME_TARGET_ID;
-}
