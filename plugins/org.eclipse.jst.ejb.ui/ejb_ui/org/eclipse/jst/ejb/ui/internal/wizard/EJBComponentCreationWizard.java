da85a1297da63f133e49345cb9ba80b4f69c2200
cbridgha <cbridgha>
2005-11-08 14:53:51 +0000

    [115080] JS

--- plugins/org.eclipse.jst.ejb.ui/ejb_ui/org/eclipse/jst/ejb/ui/internal/wizard/EJBComponentCreationWizard.java
+++ plugins/org.eclipse.jst.ejb.ui/ejb_ui/org/eclipse/jst/ejb/ui/internal/wizard/EJBComponentCreationWizard.java
@@ -7,10 +7,9 @@
  * 
  * Contributors:
  * IBM Corporation - initial API and implementation
- *******************************************************************************/ 
+ *******************************************************************************/
 package org.eclipse.jst.ejb.ui.internal.wizard;
 
-import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jst.ejb.ui.internal.util.EJBUIMessages;
 import org.eclipse.jst.j2ee.ejb.datamodel.properties.IEjbComponentCreationDataModelProperties;
 import org.eclipse.jst.j2ee.internal.ejb.archiveoperations.EjbComponentCreationDataModelProvider;
@@ -19,38 +18,37 @@ import org.eclipse.jst.j2ee.internal.plugin.J2EEUIPluginIcons;
 import org.eclipse.jst.j2ee.internal.wizard.J2EEComponentCreationWizard;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModelProvider;
+import org.eclipse.wst.common.frameworks.internal.datamodel.ui.IDMPageHandler;
 
-/** 
+/**
  * <p>
- * Wizard used to create J2EE Enterprise Java Bean 
- * (EJB) module structures in Eclipse Projects.
+ * Wizard used to create J2EE Enterprise Java Bean (EJB) module structures in Eclipse Projects.
  * </p>
  */
-public final class EJBComponentCreationWizard extends J2EEComponentCreationWizard implements IEjbComponentCreationDataModelProperties{
-	
+public final class EJBComponentCreationWizard extends J2EEComponentCreationWizard implements IEjbComponentCreationDataModelProperties {
+
 	/**
 	 * <p>
-	 * The Wizard ID of the ConnectorModuleCreationWizard. Used for 
-	 * internal purposes and activities management.
+	 * The Wizard ID of the ConnectorModuleCreationWizard. Used for internal purposes and activities
+	 * management.
 	 * </p>
 	 */
 	public static final String WIZARD_ID = EJBComponentCreationWizard.class.getName();
-	
+
 	/**
 	 * <p>
 	 * The identifer for the EJB Client page
 	 * </p>
 	 */
 	protected static final String CLIENT_PG = "client"; //$NON-NLS-1$
-	
+
 	/* A convenience reference to the client page. Initialized in doAddPages() */
 	private EJBClientComponentCreationWizardPage clientPage;
 
 	/**
 	 * <p>
-	 * The default constructor. Creates a wizard with no selection, 
-	 * no model instance, and no operation instance. The model and 
-	 * operation will be created as needed.
+	 * The default constructor. Creates a wizard with no selection, no model instance, and no
+	 * operation instance. The model and operation will be created as needed.
 	 * </p>
 	 */
 	public EJBComponentCreationWizard() {
@@ -59,24 +57,26 @@ public final class EJBComponentCreationWizard extends J2EEComponentCreationWizar
 
 	/**
 	 * <p>
-	 * The model is used to prepopulate the wizard controls
-	 * and interface with the operation.
+	 * The model is used to prepopulate the wizard controls and interface with the operation.
 	 * </p>
-	 * @param model The model parameter is used to pre-populate wizard controls and interface with the operation
+	 * 
+	 * @param model
+	 *            The model parameter is used to pre-populate wizard controls and interface with the
+	 *            operation
 	 */
 	public EJBComponentCreationWizard(IDataModel model) {
 		super(model);
 	}
 
 	/**
-	 * {@inheritDoc}   
+	 * {@inheritDoc}
 	 * 
 	 * <p>
-	 * Sets up the dialog window title and default page image. 
-	 * </p> 
+	 * Sets up the dialog window title and default page image.
+	 * </p>
 	 * 
 	 */
-	protected void doInit() { 
+	protected void doInit() {
 		setWindowTitle(EJBUIMessages.getResourceString(EJBUIMessages.EJB_PROJECT_WIZ_TITLE));
 		setDefaultPageImageDescriptor(J2EEUIPlugin.getDefault().getImageDescriptor(J2EEUIPluginIcons.EJB_PROJECT_WIZARD_BANNER));
 		preFillSelectedEARProject();
@@ -88,14 +88,12 @@ public final class EJBComponentCreationWizard extends J2EEComponentCreationWizar
 	 * <p>
 	 * Skips the EJB Client settings page based on the return value of shouldSkipClientPage()
 	 * </p>
-	 * 
-	 * @see org.eclipse.jface.wizard.IWizard#getNextPage(org.eclipse.jface.wizard.IWizardPage)
 	 */
-	public IWizardPage getNextPage(IWizardPage page) {
-		IWizardPage nextPage = super.getNextPage(page);
-		if (nextPage != null && nextPage.getName().equals(CLIENT_PG) && shouldSkipClientPage())
-			return super.getNextPage(nextPage);
-		return nextPage;
+	public String getNextPage(String currentPageName, String expectedNextPageName) {
+		if (shouldSkipClientPage() && expectedNextPageName.equals(CLIENT_PG)) {
+			return IDMPageHandler.PAGE_AFTER;
+		}
+		return super.getNextPage(currentPageName, expectedNextPageName);
 	}
 
 
@@ -106,29 +104,26 @@ public final class EJBComponentCreationWizard extends J2EEComponentCreationWizar
 	 * Skips the EJB Client settings page based on the return value of shouldSkipClientPage()
 	 * </p>
 	 * 
-	 * @see org.eclipse.jface.wizard.IWizard#getPreviousPage(org.eclipse.jface.wizard.IWizardPage)
 	 */
-	public IWizardPage getPreviousPage(IWizardPage page) {
-		IWizardPage previousPage = super.getPreviousPage(page);
-		if (previousPage != null && previousPage.getName().equals(CLIENT_PG) && shouldSkipClientPage()) {
-			IWizardPage temp = getPage(MAIN_PG);
-			if (temp != null)
-				return temp;
+	public String getPreviousPage(String currentPageName, String expectedPreviousPageName) {
+		if (shouldSkipClientPage() && expectedPreviousPageName.equals(CLIENT_PG)) {
+			return MAIN_PG;
 		}
-		return previousPage;
+		return super.getPreviousPage(currentPageName, expectedPreviousPageName);
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 * 
 	 * <p>
-	 * Adds a {@link EJBComponentCreationWizardPage} as the {@link J2EEComponentCreationWizard#MAIN_PG} 
-	 * and a {@link EJBClientCreationWizardPage} as the {@link #CLIENT_PG}.
+	 * Adds a {@link EJBComponentCreationWizardPage} as the
+	 * {@link J2EEComponentCreationWizard#MAIN_PG} and a {@link EJBClientCreationWizardPage} as the
+	 * {@link #CLIENT_PG}.
 	 * </p>
 	 */
 	public void doAddPages() {
 		addPage(new EJBComponentCreationWizardPage(getDataModel(), MAIN_PG));
-		clientPage = new EJBClientComponentCreationWizardPage((IDataModel)getDataModel().getProperty(NESTED_MODEL_EJB_CLIENT_CREATION), CLIENT_PG);
+		clientPage = new EJBClientComponentCreationWizardPage((IDataModel) getDataModel().getProperty(NESTED_MODEL_EJB_CLIENT_CREATION), CLIENT_PG);
 		addPage(clientPage);
 		super.doAddPages();
 	}
@@ -137,7 +132,8 @@ public final class EJBComponentCreationWizard extends J2EEComponentCreationWizar
 	 * {@inheritDoc}
 	 * 
 	 * @see org.eclipse.jface.wizard.IWizard#canFinish()
-	 * @return true if the parent Wizard class is ready and EJB Client Creation settings are complete
+	 * @return true if the parent Wizard class is ready and EJB Client Creation settings are
+	 *         complete
 	 */
 	public boolean canFinish() {
 		if (!getDataModel().getBooleanProperty(CREATE_CLIENT)) {
@@ -145,14 +141,16 @@ public final class EJBComponentCreationWizard extends J2EEComponentCreationWizar
 		}
 		return super.canFinish();
 	}
+
 	/**
-	 * @return true if the client page should be skipped (based on the value of {@see EJBProjectCreationDataModel#CREATE_CLIENT}.
+	 * @return true if the client page should be skipped (based on the value of
+	 *         {@see EJBProjectCreationDataModel#CREATE_CLIENT}.
 	 */
 	protected final boolean shouldSkipClientPage() {
 		return !getDataModel().getBooleanProperty(CREATE_CLIENT);
 	}
 
-    protected IDataModelProvider getDefaultProvider() {
-        return new EjbComponentCreationDataModelProvider();
-    }
+	protected IDataModelProvider getDefaultProvider() {
+		return new EjbComponentCreationDataModelProvider();
+	}
 }
