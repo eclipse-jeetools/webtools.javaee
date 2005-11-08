48b97b51cf0d6a2f0e3e97fec35aaebda576765f
vbhadrir <vbhadrir>
2005-11-08 20:14:34 +0000

    [115516] fix for plugin ID: committed for JL

--- plugins/org.eclipse.jst.j2ee.webservice.ui/webservices_ui/org/eclipse/jst/j2ee/internal/webservice/plugin/WebServiceUIPlugin.java
+++ plugins/org.eclipse.jst.j2ee.webservice.ui/webservices_ui/org/eclipse/jst/j2ee/internal/webservice/plugin/WebServiceUIPlugin.java
@@ -18,7 +18,7 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
  */
 public class WebServiceUIPlugin extends AbstractUIPlugin {
 	
-	public static final String PLUGIN_ID = "org.eclipse.jst.j2ee.webservices.ui"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.jst.j2ee.webservice.ui"; //$NON-NLS-1$
 	
 	//	The shared instance.
 	private static WebServiceUIPlugin plugin;
