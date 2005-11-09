22bc7ff146731cc5a856fec880f59a782e3d44ea
vbhadrir <vbhadrir>
2005-11-09 20:20:00 +0000

    [115543]commited for NA

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/project/facet/JavaUtilityComponentCreationDataModelProvider.java
+++ plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/project/facet/JavaUtilityComponentCreationDataModelProvider.java
@@ -6,6 +6,23 @@ import java.util.Set;
 import org.eclipse.jst.j2ee.internal.archive.operations.JavaComponentCreationDataModelProvider;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModelOperation;
 
+/**
+ * This class is used to create a JavaUtility component, e.g. 
+ *  <pre>
+ * IDataModel dm = DataModelFactory.createDataModel(new JavaUtilityComponentCreationDataModelProvider());
+ * dm.setProperty(JavaComponentCreationDataModelProvider.PROJECT_NAME,
+ * "utility");
+ * 
+ * dm.setProperty(JavaComponentCreationDataModelProvider.JAVASOURCE_FOLDER,
+ * "src"));
+ *
+ * Setting RUNTIME_TARGET_ID is optional 
+ * dm.setProperty(JavaComponentCreationDataModelProvider.RUNTIME_TARGET_ID,
+ * runtimeId);
+ *				
+ * dm.getDefaultOperation().execute(monitor, null);
+ *</pre>
+ */
 
 public class JavaUtilityComponentCreationDataModelProvider
 	extends JavaComponentCreationDataModelProvider{
