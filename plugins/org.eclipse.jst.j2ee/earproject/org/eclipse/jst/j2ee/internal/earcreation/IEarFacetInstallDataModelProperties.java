34ef57fa8f20d642136e32ab98b866d07d1d9f72
cbridgha <cbridgha>
2005-11-10 22:36:29 +0000

    This commit was manufactured by cvs2svn to create tag 'v20051110_1737'.

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/internal/earcreation/IEarFacetInstallDataModelProperties.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.jst.j2ee.internal.earcreation;
-
-import org.eclipse.wst.common.componentcore.datamodel.properties.IFacetInstalDataModelProperties;
-
-public interface IEarFacetInstallDataModelProperties extends IFacetInstalDataModelProperties {
-
-    public static final String CONTENT_DIR = "IEarFacetInstallDataModelProperties.CONTENT_DIR";
-}
