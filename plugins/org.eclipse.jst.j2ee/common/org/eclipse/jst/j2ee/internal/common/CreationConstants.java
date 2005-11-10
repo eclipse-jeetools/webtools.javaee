8a7ad4149844b7cfaa1840c87402794062aa1b79
vbhadrir <vbhadrir>
2005-11-10 16:16:31 +0000

    [115701] commited for NA

--- plugins/org.eclipse.jst.j2ee/common/org/eclipse/jst/j2ee/internal/common/CreationConstants.java
+++ plugins/org.eclipse.jst.j2ee/common/org/eclipse/jst/j2ee/internal/common/CreationConstants.java
@@ -5,5 +5,7 @@ public interface CreationConstants {
 	String DEFAULT_WEB_SOURCE_FOLDER =	"src"; //$NON-NLS-1$ 
 	String DEFAULT_EJB_SOURCE_FOLDER = "ejbModule";//$NON-NLS-1$
 	String DEFAULT_CONNECTOR_SOURCE_FOLDER = "connectorModule";//$NON-NLS-1$
-	String DEFAULT_APPCLIENT_SOURCE_FOLDER = "appClientModule";//$NON-NLS-1$ 	
+	String DEFAULT_APPCLIENT_SOURCE_FOLDER = "appClientModule";//$NON-NLS-1$ 
+	String EJB_CLIENT_NAME ="ClientProject";//$NON-NLS-1$ 
+	String CLIENT_JAR_URI="ClientJARURI";//$NON-NLS-1$ 
 }
