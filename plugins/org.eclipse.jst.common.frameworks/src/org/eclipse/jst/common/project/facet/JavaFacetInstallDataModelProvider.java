eb5257256507f92d69fb0472e247cb2cca04ac43
cbridgha <cbridgha>
2005-11-10 21:09:56 +0000

    [115877] JS

--- plugins/org.eclipse.jst.common.frameworks/src/org/eclipse/jst/common/project/facet/JavaFacetInstallDataModelProvider.java
+++ plugins/org.eclipse.jst.common.frameworks/src/org/eclipse/jst/common/project/facet/JavaFacetInstallDataModelProvider.java
@@ -12,6 +12,9 @@ package org.eclipse.jst.common.project.facet;
 
 import java.util.Set;
 
+import org.eclipse.jdt.launching.IVMInstall;
+import org.eclipse.jdt.launching.IVMInstall2;
+import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.wst.common.componentcore.datamodel.FacetInstallDataModelProvider;
 import org.eclipse.wst.common.componentcore.internal.util.IModuleConstants;
 
@@ -31,7 +34,15 @@ public class JavaFacetInstallDataModelProvider extends FacetInstallDataModelProv
 		if (FACET_ID.equals(propertyName)) {
 			return IModuleConstants.JST_JAVA;
 		} else if (FACET_VERSION.equals(propertyName)) {
-			return JavaFacetUtils.JAVA_14;
+			IVMInstall vmInstall = JavaRuntime.getDefaultVMInstall();
+			IVMInstall2 vmInstall2 = (IVMInstall2) vmInstall;
+			String jvmver = vmInstall2.getJavaVersion();
+			if (jvmver.startsWith("1.3")) { //$NON-NLS-1$
+				return JavaFacetUtils.JAVA_13;
+			} else if (jvmver.startsWith("1.4")) { //$NON-NLS-1$
+				return JavaFacetUtils.JAVA_14;
+			}
+			return JavaFacetUtils.JAVA_50;
 		} else if (SOURCE_FOLDER_NAME.equals(propertyName)) {
 			return "src";
 		}
