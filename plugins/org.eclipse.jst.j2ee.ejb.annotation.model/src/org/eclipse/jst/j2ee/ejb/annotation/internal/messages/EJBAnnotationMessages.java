8efa74c0a805fb8fa1005ee0b0fbc73bed896b2c
jeffliu <jeffliu>
2005-11-10 01:34:13 +0000

    [108513] Use the new Eclipse way of doing message bundles

--- plugins/org.eclipse.jst.j2ee.ejb.annotation.model/src/org/eclipse/jst/j2ee/ejb/annotation/internal/messages/EJBAnnotationMessages.java
+++ plugins/org.eclipse.jst.j2ee.ejb.annotation.model/src/org/eclipse/jst/j2ee/ejb/annotation/internal/messages/EJBAnnotationMessages.java
@@ -9,47 +9,103 @@
 
 package org.eclipse.jst.j2ee.ejb.annotation.internal.messages;
 
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
-
-import org.eclipse.wst.common.frameworks.internal.Messages;
+import org.eclipse.osgi.util.NLS;
 
 
-public class EJBAnnotationMessages extends Messages {
+public class EJBAnnotationMessages extends NLS {
 
-	public static final String IMAGE_LOAD_ERROR = "0"; //$NON-NLS-1$
-	
-
-	
-	private static final EJBAnnotationMessages INSTANCE = new EJBAnnotationMessages();
-
+	private static final String BUNDLE_NAME = "web_ui";//$NON-NLS-1$
 
 	private EJBAnnotationMessages() {
-		super();
+		// Do not instantiate
 	}
 
-	/**
-	 * Returns the string from the resource bundle, or 'key' if not found.
-	 */
-	public static String getResourceString(String key) {
-		return INSTANCE.doGetResourceString(key);
+	public static String ERR_TRANSACTION_EMPTY;
+	public static String FOLDER_LABEL;
+	public static String ADD_EJB_WIZARD_WINDOW_TITLE;
+	public static String DESTINATION_LABEL;
+	public static String ERR_STATELESS_VALUE;
+	public static String TRANSACTION_LABEL;
+	public static String JAVA_CLASS_MODIFIERS_LABEL;
+	public static String DISPLAY_NAME_LABEL;
+	public static String ERR_DESTINATIONTYPE_EMPTY;
+	public static String ERR_EJB_TYPE_VALUE;
+	public static String SUPERCLASS_LABEL;
+	public static String ERR_CLASS_NAME_MUSTEND_WITH_BEAN;
+	public static String PACKAGE_SELECTION_DIALOG_MSG_NONE;
+	public static String JAVA_CLASS_ABSTRACT_CHECKBOX_LABEL;
+	public static String DESTINATION_NAME_LABEL;
+	public static String _9;
+	public static String JAVA_CLASS_MAIN_CHECKBOX_LABEL;
+	public static String _8;
+	public static String ERR_JNDI_NAME_VALUE;
+	public static String _7;
+	public static String _6;
+	public static String _5;
+	public static String _4;
+	public static String _3;
+	public static String _2;
+	public static String _1;
+	public static String ADD_EJB_WIZARD_PAGE_DESC;
+	public static String _0;
+	public static String EMPTY_LIST_MSG;
+	public static String ERR_DISPLAY_NAME_EMPTY;
+	public static String NO_WEB_PROJECTS;
+	public static String INTERFACE_SELECTION_DIALOG_MSG_NONE;
+	public static String ERR_EJB_NAME_ENDS_WITH_BEAN;
+	public static String SUPERCLASS_SELECTION_DIALOG_TITLE;
+	public static String PACKAGE_SELECTION_DIALOG_DESC;
+	public static String JAVA_CLASS_BROWER_DIALOG_TITLE;
+	public static String CONTAINER_SELECTION_DIALOG_DESC;
+	public static String NEW_JAVA_CLASS_DESTINATION_WIZARD_PAGE_DESC;
+	public static String JAVA_CLASS_PUBLIC_CHECKBOX_LABEL;
+	public static String URL_PATTERN_LABEL;
+	public static String CONTAINER_SELECTION_DIALOG_TITLE;
+	public static String URL_PATTERN_TITLE;
+	public static String CONTAINER_SELECTION_DIALOG_VALIDATOR_MESG;
+	public static String DESCRIPTION_LABEL;
+	public static String JAVA_CLASS_FINAL_CHECKBOX_LABEL;
+	public static String ERR_STATELESS_EMPTY;
+	public static String EJB_NAME_LABEL;
+	public static String ERR_EJB_DISPLAY_NAME_USED;
+	public static String ERR_EJB_TYPE_EXISTS;
+	public static String ERR_MUST_ENTER_A_PACKAGE_NAME;
+	public static String ERR_EJB_TYPE_EMPTY;
+	public static String INTERFACE_SELECTION_DIALOG_DESC;
+	public static String ADD_LABEL;
+	public static String JAVA_CLASS_BROWER_DIALOG_MESSAGE;
+	public static String ERR_DESTINATIONTYPE_VALUE;
+	public static String JAVA_CLASS_METHOD_STUBS_LABEL;
+	public static String JAVA_CLASS_CONSTRUCTOR_CHECKBOX_LABEL;
+	public static String DEFAULT_PACKAGE;
+	public static String BROWSE_BUTTON_LABEL;
+	public static String REMOVE_BUTTON;
+	public static String ERR_JNDI_NAME_EMPTY;
+	public static String ERR_EJB_CLASS_NAME_USED;
+	public static String CLASS_NAME_LABEL;
+	public static String STATELESS_LABEL;
+	public static String JAVA_PACKAGE_LABEL;
+	public static String PACKAGE_SELECTION_DIALOG_TITLE;
+	public static String ADD_BUTTON_LABEL;
+	public static String ADD_EJB_WIZARD_PAGE_TITLE;
+	public static String _10;
+	public static String SUPERCLASS_SELECTION_DIALOG_DESC;
+	public static String NEW_JAVA_CLASS_OPTIONS_WIZARD_PAGE_DESC;
+	public static String REMOVE_BUTTON_LABEL;
+	public static String ERR_EJB_NAME_EMPTY;
+	public static String INTERFACE_SELECTION_DIALOG_TITLE;
+	public static String NAME_LABEL;
+	public static String VALUE_LABEL;
+	public static String JAVA_CLASS_INHERIT_CHECKBOX_LABEL;
+	public static String JNDI_NAME_LABEL;
+	public static String JAVA_CLASS_INTERFACES_LABEL;
+	public static String SELECT_CLASS_TITLE;
+	public static String NO_EJB_PROJECTS;
+	public static String ERR_TRANSACTION_VALUE;
+
+	static {
+		NLS.initializeMessages(BUNDLE_NAME, EJBAnnotationMessages.class);
 	}
 
-	public static String getResourceString(String key, Object[] args) {
-		return INSTANCE.doGetResourceString(key, args);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.wst.common.frameworks.internal.Messages#initializeBundle()
-	 */
-	protected void initializeBundle() {
-		try {
-			resourceBundle = ResourceBundle.getBundle("web_ui"); //$NON-NLS-1$
-		} catch (MissingResourceException x) {
-			// Do nothing
-		}
-	}
-
+	public static final String IMAGE_LOAD_ERROR = _0;
 }
--- plugins/org.eclipse.jst.j2ee.ejb.annotation.model/src/org/eclipse/jst/j2ee/ejb/annotation/internal/messages/IEJBAnnotationConstants.java
+++ plugins/org.eclipse.jst.j2ee.ejb.annotation.model/src/org/eclipse/jst/j2ee/ejb/annotation/internal/messages/IEJBAnnotationConstants.java
@@ -12,82 +12,82 @@ package org.eclipse.jst.j2ee.ejb.annotation.internal.messages;
 
 public interface IEJBAnnotationConstants {
 	
-	public static final String ERR_EJB_CLASS_NAME_USED = EJBAnnotationMessages.getResourceString("ERR_EJB_CLASS_NAME_USED"); //$NON-NLS-1$
-	public static final String ERR_EJB_NAME_EMPTY = EJBAnnotationMessages.getResourceString("ERR_EJB_NAME_EMPTY"); //$NON-NLS-1$
-	public static final String ERR_EJB_TYPE_EMPTY = EJBAnnotationMessages.getResourceString("ERR_EJB_TYPE_EMPTY"); //$NON-NLS-1$;
-	public static final String ERR_EJB_TYPE_EXISTS = EJBAnnotationMessages.getResourceString("ERR_EJB_TYPE_EXISTS"); //$NON-NLS-1$;
-	public static final String ERR_EJB_TYPE_VALUE = EJBAnnotationMessages.getResourceString("ERR_EJB_TYPE_VALUE"); //$NON-NLS-1$;
-	public static final String ERR_CLASS_NAME_MUSTEND_WITH_BEAN = EJBAnnotationMessages.getResourceString("ERR_CLASS_NAME_MUSTEND_WITH_BEAN"); //$NON-NLS-1$
-	public static final String ERR_EJB_NAME_ENDS_WITH_BEAN = EJBAnnotationMessages.getResourceString("ERR_EJB_NAME_ENDS_WITH_BEAN"); //$NON-NLS-1$
-	public static final String ERR_DISPLAY_NAME_EMPTY = EJBAnnotationMessages.getResourceString("ERR_DISPLAY_NAME_EMPTY"); //$NON-NLS-1$
-	public static final String ERR_EJB_DISPLAY_NAME_USED = EJBAnnotationMessages.getResourceString("ERR_EJB_DISPLAY_NAME_USED"); //$NON-NLS-1$
-	public static final String ERR_STATELESS_EMPTY = EJBAnnotationMessages.getResourceString("ERR_STATELESS_EMPTY"); //$NON-NLS-1$
-	public static final String ERR_STATELESS_VALUE = EJBAnnotationMessages.getResourceString("ERR_STATELESS_VALUE"); //$NON-NLS-1$
-	public static final String ERR_JNDI_NAME_EMPTY = EJBAnnotationMessages.getResourceString("ERR_JNDI_NAME_EMPTY"); //$NON-NLS-1$
-	public static final String ERR_JNDI_NAME_VALUE = EJBAnnotationMessages.getResourceString("ERR_JNDI_NAME_VALUE"); //$NON-NLS-1$
-	public static final String ERR_TRANSACTION_EMPTY = EJBAnnotationMessages.getResourceString("ERR_TRANSACTION_EMPTY"); //$NON-NLS-1$
-	public static final String ERR_TRANSACTION_VALUE = EJBAnnotationMessages.getResourceString("ERR_TRANSACTION_VALUE"); //$NON-NLS-1$
-	public static final String ERR_DESTINATIONTYPE_EMPTY = EJBAnnotationMessages.getResourceString("ERR_DESTINATIONTYPE_EMPTY"); //$NON-NLS-1$
-	public static final String ERR_DESTINATIONTYPE_VALUE = EJBAnnotationMessages.getResourceString("ERR_DESTINATIONTYPE_VALUE"); //$NON-NLS-1$
-	public static final String ERR_MUST_ENTER_A_PACKAGE_NAME = EJBAnnotationMessages.getResourceString("ERR_MUST_ENTER_A_PACKAGE_NAME"); //$NON-NLS-1$
+	public static final String ERR_EJB_CLASS_NAME_USED = EJBAnnotationMessages.ERR_EJB_CLASS_NAME_USED;
+	public static final String ERR_EJB_NAME_EMPTY = EJBAnnotationMessages.ERR_EJB_NAME_EMPTY;
+	public static final String ERR_EJB_TYPE_EMPTY = EJBAnnotationMessages.ERR_EJB_TYPE_EMPTY;;
+	public static final String ERR_EJB_TYPE_EXISTS = EJBAnnotationMessages.ERR_EJB_TYPE_EXISTS;;
+	public static final String ERR_EJB_TYPE_VALUE = EJBAnnotationMessages.ERR_EJB_TYPE_VALUE;;
+	public static final String ERR_CLASS_NAME_MUSTEND_WITH_BEAN = EJBAnnotationMessages.ERR_CLASS_NAME_MUSTEND_WITH_BEAN;
+	public static final String ERR_EJB_NAME_ENDS_WITH_BEAN = EJBAnnotationMessages.ERR_EJB_NAME_ENDS_WITH_BEAN;
+	public static final String ERR_DISPLAY_NAME_EMPTY = EJBAnnotationMessages.ERR_DISPLAY_NAME_EMPTY;
+	public static final String ERR_EJB_DISPLAY_NAME_USED = EJBAnnotationMessages.ERR_EJB_DISPLAY_NAME_USED;
+	public static final String ERR_STATELESS_EMPTY = EJBAnnotationMessages.ERR_STATELESS_EMPTY;
+	public static final String ERR_STATELESS_VALUE = EJBAnnotationMessages.ERR_STATELESS_VALUE;
+	public static final String ERR_JNDI_NAME_EMPTY = EJBAnnotationMessages.ERR_JNDI_NAME_EMPTY;
+	public static final String ERR_JNDI_NAME_VALUE = EJBAnnotationMessages.ERR_JNDI_NAME_VALUE;
+	public static final String ERR_TRANSACTION_EMPTY = EJBAnnotationMessages.ERR_TRANSACTION_EMPTY;
+	public static final String ERR_TRANSACTION_VALUE = EJBAnnotationMessages.ERR_TRANSACTION_VALUE;
+	public static final String ERR_DESTINATIONTYPE_EMPTY = EJBAnnotationMessages.ERR_DESTINATIONTYPE_EMPTY;
+	public static final String ERR_DESTINATIONTYPE_VALUE = EJBAnnotationMessages.ERR_DESTINATIONTYPE_VALUE;
+	public static final String ERR_MUST_ENTER_A_PACKAGE_NAME = EJBAnnotationMessages.ERR_MUST_ENTER_A_PACKAGE_NAME;
 
 	// AddEjbWizard
-	public final static String ADD_EJB_WIZARD_WINDOW_TITLE = EJBAnnotationMessages.getResourceString("ADD_EJB_WIZARD_WINDOW_TITLE"); //$NON-NLS-1$
-	public final static String ADD_EJB_WIZARD_PAGE_TITLE = EJBAnnotationMessages.getResourceString("ADD_EJB_WIZARD_PAGE_TITLE"); //$NON-NLS-1$
-	public static final String ADD_EJB_WIZARD_PAGE_DESC = EJBAnnotationMessages.getResourceString("ADD_EJB_WIZARD_PAGE_DESC"); //$NON-NLS-1$
+	public final static String ADD_EJB_WIZARD_WINDOW_TITLE = EJBAnnotationMessages.ADD_EJB_WIZARD_WINDOW_TITLE;
+	public final static String ADD_EJB_WIZARD_PAGE_TITLE = EJBAnnotationMessages.ADD_EJB_WIZARD_PAGE_TITLE;
+	public static final String ADD_EJB_WIZARD_PAGE_DESC = EJBAnnotationMessages.ADD_EJB_WIZARD_PAGE_DESC;
 
-	public final static String DEFAULT_PACKAGE = EJBAnnotationMessages.getResourceString("DEFAULT_PACKAGE"); //$NON-NLS-1$
-	public final static String SELECT_CLASS_TITLE = EJBAnnotationMessages.getResourceString("SELECT_CLASS_TITLE"); //$NON-NLS-1$
+	public final static String DEFAULT_PACKAGE = EJBAnnotationMessages.DEFAULT_PACKAGE;
+	public final static String SELECT_CLASS_TITLE = EJBAnnotationMessages.SELECT_CLASS_TITLE;
 	
 	
 	// common
 	
-	public final static String EJB_NAME_LABEL = EJBAnnotationMessages.getResourceString("EJB_NAME_LABEL"); //$NON-NLS-1$
-	public final static String DESCRIPTION_LABEL = EJBAnnotationMessages.getResourceString("DESCRIPTION_LABEL"); //$NON-NLS-1$
-	public static final String NAME_LABEL = EJBAnnotationMessages.getResourceString("NAME_LABEL"); //$NON-NLS-1$;
-	public static final String JNDI_NAME_LABEL = EJBAnnotationMessages.getResourceString("JNDI_NAME_LABEL"); //$NON-NLS-1$;
-	public static final String STATELESS_LABEL =  EJBAnnotationMessages.getResourceString("STATELESS_LABEL"); //$NON-NLS-1$;;
-	public static final String TRANSACTION_LABEL = EJBAnnotationMessages.getResourceString("TRANSACTION_LABEL"); //$NON-NLS-1$;;;
-	public static final String DESTINATION_LABEL = EJBAnnotationMessages.getResourceString("DESTINATION_LABEL"); //$NON-NLS-1$;;;
-	public static final String DESTINATION_NAME_LABEL = EJBAnnotationMessages.getResourceString("DESTINATION_NAME_LABEL"); //$NON-NLS-1$;;;
-	public static final String DISPLAY_NAME_LABEL = EJBAnnotationMessages.getResourceString("DISPLAY_NAME_LABEL"); //$NON-NLS-1$;
+	public final static String EJB_NAME_LABEL = EJBAnnotationMessages.EJB_NAME_LABEL;
+	public final static String DESCRIPTION_LABEL = EJBAnnotationMessages.DESCRIPTION_LABEL;
+	public static final String NAME_LABEL = EJBAnnotationMessages.NAME_LABEL;
+	public static final String JNDI_NAME_LABEL = EJBAnnotationMessages.JNDI_NAME_LABEL;
+	public static final String STATELESS_LABEL =  EJBAnnotationMessages.STATELESS_LABEL;
+	public static final String TRANSACTION_LABEL = EJBAnnotationMessages.TRANSACTION_LABEL;
+	public static final String DESTINATION_LABEL = EJBAnnotationMessages.DESTINATION_LABEL;
+	public static final String DESTINATION_NAME_LABEL = EJBAnnotationMessages.DESTINATION_NAME_LABEL;
+	public static final String DISPLAY_NAME_LABEL = EJBAnnotationMessages.DISPLAY_NAME_LABEL;
 
 	
-	public final static String ADD_BUTTON_LABEL = EJBAnnotationMessages.getResourceString("ADD_BUTTON_LABEL"); //$NON-NLS-1$
-	public final static String FOLDER_LABEL = EJBAnnotationMessages.getResourceString("FOLDER_LABEL"); //$NON-NLS-1$
-	public final static String JAVA_PACKAGE_LABEL = EJBAnnotationMessages.getResourceString("JAVA_PACKAGE_LABEL"); //$NON-NLS-1$
-	public final static String CLASS_NAME_LABEL = EJBAnnotationMessages.getResourceString("CLASS_NAME_LABEL"); //$NON-NLS-1$
-	public final static String SUPERCLASS_LABEL = EJBAnnotationMessages.getResourceString("SUPERCLASS_LABEL"); //$NON-NLS-1$
-	public final static String CONTAINER_SELECTION_DIALOG_TITLE = EJBAnnotationMessages.getResourceString("CONTAINER_SELECTION_DIALOG_TITLE"); //$NON-NLS-1$
-	public final static String CONTAINER_SELECTION_DIALOG_DESC = EJBAnnotationMessages.getResourceString("CONTAINER_SELECTION_DIALOG_DESC"); //$NON-NLS-1$
-	public final static String CONTAINER_SELECTION_DIALOG_VALIDATOR_MESG = EJBAnnotationMessages.getResourceString("CONTAINER_SELECTION_DIALOG_VALIDATOR_MESG"); //$NON-NLS-1$
-	public final static String PACKAGE_SELECTION_DIALOG_TITLE = EJBAnnotationMessages.getResourceString("PACKAGE_SELECTION_DIALOG_TITLE"); //$NON-NLS-1$
-	public final static String PACKAGE_SELECTION_DIALOG_DESC = EJBAnnotationMessages.getResourceString("PACKAGE_SELECTION_DIALOG_DESC"); //$NON-NLS-1$
-	public final static String PACKAGE_SELECTION_DIALOG_MSG_NONE = EJBAnnotationMessages.getResourceString("PACKAGE_SELECTION_DIALOG_MSG_NONE"); //$NON-NLS-1$
-	public final static String SUPERCLASS_SELECTION_DIALOG_TITLE = EJBAnnotationMessages.getResourceString("SUPERCLASS_SELECTION_DIALOG_TITLE"); //$NON-NLS-1$
-	public final static String SUPERCLASS_SELECTION_DIALOG_DESC = EJBAnnotationMessages.getResourceString("SUPERCLASS_SELECTION_DIALOG_DESC"); //$NON-NLS-1$
-	public final static String INTERFACE_SELECTION_DIALOG_TITLE = EJBAnnotationMessages.getResourceString("INTERFACE_SELECTION_DIALOG_TITLE"); //$NON-NLS-1$
-	public final static String INTERFACE_SELECTION_DIALOG_DESC = EJBAnnotationMessages.getResourceString("INTERFACE_SELECTION_DIALOG_DESC"); //$NON-NLS-1$
-	public final static String INTERFACE_SELECTION_DIALOG_MSG_NONE = EJBAnnotationMessages.getResourceString("INTERFACE_SELECTION_DIALOG_MSG_NONE"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_MODIFIERS_LABEL = EJBAnnotationMessages.getResourceString("JAVA_CLASS_MODIFIERS_LABEL"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_INTERFACES_LABEL = EJBAnnotationMessages.getResourceString("JAVA_CLASS_INTERFACES_LABEL"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_METHOD_STUBS_LABEL = EJBAnnotationMessages.getResourceString("JAVA_CLASS_METHOD_STUBS_LABEL"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_PUBLIC_CHECKBOX_LABEL = EJBAnnotationMessages.getResourceString("JAVA_CLASS_PUBLIC_CHECKBOX_LABEL"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_ABSTRACT_CHECKBOX_LABEL = EJBAnnotationMessages.getResourceString("JAVA_CLASS_ABSTRACT_CHECKBOX_LABEL"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_FINAL_CHECKBOX_LABEL = EJBAnnotationMessages.getResourceString("JAVA_CLASS_FINAL_CHECKBOX_LABEL"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_CONSTRUCTOR_CHECKBOX_LABEL = EJBAnnotationMessages.getResourceString("JAVA_CLASS_CONSTRUCTOR_CHECKBOX_LABEL"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_MAIN_CHECKBOX_LABEL = EJBAnnotationMessages.getResourceString("JAVA_CLASS_MAIN_CHECKBOX_LABEL"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_INHERIT_CHECKBOX_LABEL = EJBAnnotationMessages.getResourceString("JAVA_CLASS_INHERIT_CHECKBOX_LABEL"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_BROWER_DIALOG_TITLE = EJBAnnotationMessages.getResourceString("JAVA_CLASS_BROWER_DIALOG_TITLE"); //$NON-NLS-1$
-	public final static String JAVA_CLASS_BROWER_DIALOG_MESSAGE = EJBAnnotationMessages.getResourceString("JAVA_CLASS_BROWER_DIALOG_MESSAGE"); //$NON-NLS-1$
+	public final static String ADD_BUTTON_LABEL = EJBAnnotationMessages.ADD_BUTTON_LABEL;
+	public final static String FOLDER_LABEL = EJBAnnotationMessages.FOLDER_LABEL;
+	public final static String JAVA_PACKAGE_LABEL = EJBAnnotationMessages.JAVA_PACKAGE_LABEL;
+	public final static String CLASS_NAME_LABEL = EJBAnnotationMessages.CLASS_NAME_LABEL;
+	public final static String SUPERCLASS_LABEL = EJBAnnotationMessages.SUPERCLASS_LABEL;
+	public final static String CONTAINER_SELECTION_DIALOG_TITLE = EJBAnnotationMessages.CONTAINER_SELECTION_DIALOG_TITLE;
+	public final static String CONTAINER_SELECTION_DIALOG_DESC = EJBAnnotationMessages.CONTAINER_SELECTION_DIALOG_DESC;
+	public final static String CONTAINER_SELECTION_DIALOG_VALIDATOR_MESG = EJBAnnotationMessages.CONTAINER_SELECTION_DIALOG_VALIDATOR_MESG;
+	public final static String PACKAGE_SELECTION_DIALOG_TITLE = EJBAnnotationMessages.PACKAGE_SELECTION_DIALOG_TITLE;
+	public final static String PACKAGE_SELECTION_DIALOG_DESC = EJBAnnotationMessages.PACKAGE_SELECTION_DIALOG_DESC;
+	public final static String PACKAGE_SELECTION_DIALOG_MSG_NONE = EJBAnnotationMessages.PACKAGE_SELECTION_DIALOG_MSG_NONE;
+	public final static String SUPERCLASS_SELECTION_DIALOG_TITLE = EJBAnnotationMessages.SUPERCLASS_SELECTION_DIALOG_TITLE;
+	public final static String SUPERCLASS_SELECTION_DIALOG_DESC = EJBAnnotationMessages.SUPERCLASS_SELECTION_DIALOG_DESC;
+	public final static String INTERFACE_SELECTION_DIALOG_TITLE = EJBAnnotationMessages.INTERFACE_SELECTION_DIALOG_TITLE;
+	public final static String INTERFACE_SELECTION_DIALOG_DESC = EJBAnnotationMessages.INTERFACE_SELECTION_DIALOG_DESC;
+	public final static String INTERFACE_SELECTION_DIALOG_MSG_NONE = EJBAnnotationMessages.INTERFACE_SELECTION_DIALOG_MSG_NONE;
+	public final static String JAVA_CLASS_MODIFIERS_LABEL = EJBAnnotationMessages.JAVA_CLASS_MODIFIERS_LABEL;
+	public final static String JAVA_CLASS_INTERFACES_LABEL = EJBAnnotationMessages.JAVA_CLASS_INTERFACES_LABEL;
+	public final static String JAVA_CLASS_METHOD_STUBS_LABEL = EJBAnnotationMessages.JAVA_CLASS_METHOD_STUBS_LABEL;
+	public final static String JAVA_CLASS_PUBLIC_CHECKBOX_LABEL = EJBAnnotationMessages.JAVA_CLASS_PUBLIC_CHECKBOX_LABEL;
+	public final static String JAVA_CLASS_ABSTRACT_CHECKBOX_LABEL = EJBAnnotationMessages.JAVA_CLASS_ABSTRACT_CHECKBOX_LABEL;
+	public final static String JAVA_CLASS_FINAL_CHECKBOX_LABEL = EJBAnnotationMessages.JAVA_CLASS_FINAL_CHECKBOX_LABEL;
+	public final static String JAVA_CLASS_CONSTRUCTOR_CHECKBOX_LABEL = EJBAnnotationMessages.JAVA_CLASS_CONSTRUCTOR_CHECKBOX_LABEL;
+	public final static String JAVA_CLASS_MAIN_CHECKBOX_LABEL = EJBAnnotationMessages.JAVA_CLASS_MAIN_CHECKBOX_LABEL;
+	public final static String JAVA_CLASS_INHERIT_CHECKBOX_LABEL = EJBAnnotationMessages.JAVA_CLASS_INHERIT_CHECKBOX_LABEL;
+	public final static String JAVA_CLASS_BROWER_DIALOG_TITLE = EJBAnnotationMessages.JAVA_CLASS_BROWER_DIALOG_TITLE;
+	public final static String JAVA_CLASS_BROWER_DIALOG_MESSAGE = EJBAnnotationMessages.JAVA_CLASS_BROWER_DIALOG_MESSAGE;
 	
-	public final static String VALUE_LABEL = EJBAnnotationMessages.getResourceString("VALUE_LABEL"); //$NON-NLS-1$
+	public final static String VALUE_LABEL = EJBAnnotationMessages.VALUE_LABEL;
 	public final static String EMPTY_STRING = ""; //$NON-NLS-1$
-	public final static String BROWSE_BUTTON_LABEL = EJBAnnotationMessages.getResourceString("BROWSE_BUTTON_LABEL"); //$NON-NLS-1$
-	public static final String REMOVE_BUTTON = EJBAnnotationMessages.getResourceString("REMOVE_BUTTON"); //$NON-NLS-1$
+	public final static String BROWSE_BUTTON_LABEL = EJBAnnotationMessages.BROWSE_BUTTON_LABEL;
+	public static final String REMOVE_BUTTON = EJBAnnotationMessages.REMOVE_BUTTON;
     
-	public final static String NEW_JAVA_CLASS_DESTINATION_WIZARD_PAGE_DESC = EJBAnnotationMessages.getResourceString("NEW_JAVA_CLASS_DESTINATION_WIZARD_PAGE_DESC"); //$NON-NLS-1$
-	public final static String NEW_JAVA_CLASS_OPTIONS_WIZARD_PAGE_DESC = EJBAnnotationMessages.getResourceString("NEW_JAVA_CLASS_OPTIONS_WIZARD_PAGE_DESC"); //$NON-NLS-1$
+	public final static String NEW_JAVA_CLASS_DESTINATION_WIZARD_PAGE_DESC = EJBAnnotationMessages.NEW_JAVA_CLASS_DESTINATION_WIZARD_PAGE_DESC;
+	public final static String NEW_JAVA_CLASS_OPTIONS_WIZARD_PAGE_DESC = EJBAnnotationMessages.NEW_JAVA_CLASS_OPTIONS_WIZARD_PAGE_DESC;
 	
-	public final static String NO_EJB_PROJECTS = EJBAnnotationMessages.getResourceString("NO_EJB_PROJECTS"); //$NON-NLS-1$
+	public final static String NO_EJB_PROJECTS = EJBAnnotationMessages.NO_EJB_PROJECTS;
 }
