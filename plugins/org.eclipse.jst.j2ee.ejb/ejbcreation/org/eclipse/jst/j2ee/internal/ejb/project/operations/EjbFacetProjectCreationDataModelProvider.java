46964cedeefd177db1482c3385842d45028b0daf
vbhadrir <vbhadrir>
2005-11-14 14:22:46 +0000

    [nobug] commited for NA

--- plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/internal/ejb/project/operations/EjbFacetProjectCreationDataModelProvider.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/internal/ejb/project/operations/EjbFacetProjectCreationDataModelProvider.java
@@ -1,6 +1,8 @@
 package org.eclipse.jst.j2ee.internal.ejb.project.operations;
 
+import org.eclipse.jst.common.project.facet.IJavaFacetInstallDataModelProperties;
 import org.eclipse.jst.common.project.facet.JavaFacetInstallDataModelProvider;
+import org.eclipse.jst.j2ee.project.facet.IJ2EEModuleFacetInstallDataModelProperties;
 import org.eclipse.wst.common.componentcore.datamodel.FacetProjectCreationDataModelProvider;
 import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
@@ -17,8 +19,9 @@ public class EjbFacetProjectCreationDataModelProvider extends
 		FacetDataModelMap map = (FacetDataModelMap) getProperty(FACET_DM_MAP);
 		IDataModel javaFacet = DataModelFactory.createDataModel(new JavaFacetInstallDataModelProvider());
 		map.add(javaFacet);
-		IDataModel webFacet = DataModelFactory.createDataModel(new EjbFacetInstallDataModelProvider());
-		map.add(webFacet);
+		IDataModel ejbFacet = DataModelFactory.createDataModel(new EjbFacetInstallDataModelProvider());
+		map.add(ejbFacet);
+		javaFacet.setProperty(IJavaFacetInstallDataModelProperties.SOURCE_FOLDER_NAME,ejbFacet.getStringProperty(IJ2EEModuleFacetInstallDataModelProperties.CONFIG_FOLDER));
 	}
 	
 }
