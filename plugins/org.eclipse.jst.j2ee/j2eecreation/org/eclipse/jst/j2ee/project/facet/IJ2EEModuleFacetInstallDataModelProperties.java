ab8c6a3ddb2d79b8dd79c39b226f24c9948a8d39
cbridgha <cbridgha>
2005-11-19 20:27:45 +0000

    This commit was manufactured by cvs2svn to create tag 'v20051207_1540'.

--- plugins/org.eclipse.jst.j2ee/j2eecreation/org/eclipse/jst/j2ee/project/facet/IJ2EEModuleFacetInstallDataModelProperties.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.jst.j2ee.project.facet;
-
-public interface IJ2EEModuleFacetInstallDataModelProperties extends IJ2EEFacetInstallDataModelProperties{
-
-	public static final String ADD_TO_EAR = "IJ2EEFacetInstallDataModelProperties.ADD_TO_EAR"; //$NON-NLS-1$
-	public static final String EAR_PROJECT_NAME = "IJ2EEFacetInstallDataModelProperties.EAR_PROJECT_NAME"; //$NON-NLS-1$
-	public static final String CONFIG_FOLDER = "IJ2EEFacetInstallDataModelProperties.CONFIG_FOLDER"; //$NON-NLS-1$
-	
-}
