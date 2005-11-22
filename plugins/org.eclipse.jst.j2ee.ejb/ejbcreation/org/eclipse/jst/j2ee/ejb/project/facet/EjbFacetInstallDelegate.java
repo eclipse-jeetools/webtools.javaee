59057a1513e3e6489f62030df101bfe8ccf7f478
vbhadrir <vbhadrir>
2005-11-22 16:53:27 +0000

    [117421] fix for adding runtime to EARs: committed for JL

--- plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/ejb/project/facet/EjbFacetInstallDelegate.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejbcreation/org/eclipse/jst/j2ee/ejb/project/facet/EjbFacetInstallDelegate.java
@@ -55,7 +55,9 @@ import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModelOperation;
 import org.eclipse.wst.common.project.facet.core.IDelegate;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
 import org.eclipse.wst.common.project.facet.core.runtime.IRuntime;
 
 public class EjbFacetInstallDelegate extends J2EEFacetInstallDelegate implements IDelegate {
@@ -145,8 +147,8 @@ public class EjbFacetInstallDelegate extends J2EEFacetInstallDelegate implements
 
 						String ver = fv.getVersionString();
 						String j2eeVersionText = J2EEVersionUtil.convertVersionIntToString(J2EEVersionUtil.convertWebVersionStringToJ2EEVersionID(ver));
-
-						installEARFacet(j2eeVersionText, earProjectName, monitor);
+						IFacetedProject facetedProject = ProjectFacetsManager.create(project); 
+						installEARFacet(j2eeVersionText, earProjectName, facetedProject.getRuntime(), monitor);
 
 						IProject earProject = ProjectUtilities.getProject(earProjectName);
 						IVirtualComponent earComp = ComponentCore.createComponent(earProject);
