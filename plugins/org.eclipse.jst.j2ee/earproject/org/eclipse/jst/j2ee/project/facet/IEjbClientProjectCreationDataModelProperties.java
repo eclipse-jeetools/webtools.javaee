16b0bb31623188f1678b2c487f0f28869d4cf9cd
cbridgha <cbridgha>
2005-11-29 15:14:00 +0000

    This commit was manufactured by cvs2svn to create tag 'I20051130'.

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/project/facet/IEjbClientProjectCreationDataModelProperties.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.jst.j2ee.project.facet;
-
-import org.eclipse.jst.j2ee.project.facet.IJavaUtilityProjectCreationDataModelProperties;
-
-public interface IEjbClientProjectCreationDataModelProperties extends IJavaUtilityProjectCreationDataModelProperties{
-
-	public static final String EJB_PROJECT_NAME = "IEjbClientProjectCreationDataModelProperties.EJB_PROJECT_NAME"; //$NON-NLS-1$
-}
