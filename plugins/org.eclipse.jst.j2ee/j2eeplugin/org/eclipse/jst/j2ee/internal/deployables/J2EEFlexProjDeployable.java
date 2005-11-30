6bf1d99a3cbe3a3ffe880fddf7de7e5fba27b7ea
jlanuti <jlanuti>
2005-11-30 20:38:26 +0000

    [118161] reworked deployable hierarchy to fix static webs

--- plugins/org.eclipse.jst.j2ee/j2eeplugin/org/eclipse/jst/j2ee/internal/deployables/J2EEFlexProjDeployable.java
+++ plugins/org.eclipse.jst.j2ee/j2eeplugin/org/eclipse/jst/j2ee/internal/deployables/J2EEFlexProjDeployable.java
@@ -17,9 +17,7 @@ import java.util.List;
 import java.util.Properties;
 
 import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
@@ -41,23 +39,20 @@ import org.eclipse.jst.server.core.IJ2EEModule;
 import org.eclipse.jst.server.core.IWebModule;
 import org.eclipse.wst.common.componentcore.ArtifactEdit;
 import org.eclipse.wst.common.componentcore.ComponentCore;
+import org.eclipse.wst.common.componentcore.internal.ComponentDeployable;
 import org.eclipse.wst.common.componentcore.internal.util.ComponentUtilities;
 import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 import org.eclipse.wst.common.componentcore.resources.IVirtualFolder;
 import org.eclipse.wst.common.componentcore.resources.IVirtualReference;
 import org.eclipse.wst.server.core.IModule;
-import org.eclipse.wst.server.core.internal.ModuleFile;
 import org.eclipse.wst.server.core.internal.ModuleFolder;
 import org.eclipse.wst.server.core.model.IModuleFolder;
 import org.eclipse.wst.server.core.model.IModuleResource;
-import org.eclipse.wst.server.core.util.ProjectModule;
 /**
  * J2EE module superclass.
  */
-public class J2EEFlexProjDeployable extends ProjectModule implements IJ2EEModule, IEnterpriseApplication, IApplicationClientModule, IConnectorModule, IEJBModule, IWebModule {
+public class J2EEFlexProjDeployable extends ComponentDeployable implements IJ2EEModule, IEnterpriseApplication, IApplicationClientModule, IConnectorModule, IEJBModule, IWebModule {
 	private static final IPath WEB_CLASSES_PATH = new Path("WEB-INF").append("classes"); //$NON-NLS-1$ //$NON-NLS-2$
-    protected IVirtualComponent component = null;
-    private List members = new ArrayList();
 
 	/**
 	 * Constructor for J2EEFlexProjDeployable.
@@ -67,14 +62,6 @@ public class J2EEFlexProjDeployable extends ProjectModule implements IJ2EEModule
 	 */
 	public J2EEFlexProjDeployable(IProject project, IVirtualComponent aComponent) {
 		super(project);
-		component = aComponent;
-	}
-
-	/**
-	 * @see org.eclipse.jst.server.core.IJ2EEModule#isBinary()
-	 */
-	public boolean isBinary() {
-		return false;
 	}
 
 	/**
@@ -150,68 +137,7 @@ public class J2EEFlexProjDeployable extends ProjectModule implements IJ2EEModule
 		return mr;
 	}
 	
-	private IModuleResource getExistingModuleResource(List aList, IPath path) {
-    	IModuleResource result = null;
-    	// If the list is empty, return null
-    	if (aList==null || aList.isEmpty())
-    		return null;
-    	// Otherwise recursively check to see if given resource matches current resource or if it is a child
-    	int i=0;
-    	do {
-	    	IModuleResource moduleResource = (IModuleResource) aList.get(i);
-	    		if (moduleResource.getModuleRelativePath().append(moduleResource.getName()).equals(path))
-	    			result = moduleResource;
-	    		// if it is a folder, check its children for the resource path
-	    		else if (moduleResource instanceof IModuleFolder) {
-	    			result = getExistingModuleResource(Arrays.asList(((IModuleFolder)moduleResource).members()),path);
-	    		}
-	    		i++;
-    	} while (result == null && i<aList.size() );
-    	return result;
-    }
-
-	/**
-	 * Find the module resources for a given container and path. Inserts in the java containers
-	 * at a given path if not null.
-	 * 
-	 * @param cont a container
-	 * @param path the current module relative path
-	 * @param javaPath the path where Java resources fit in the root
-	 * @param javaCont
-	 * @return a possibly-empty array of module resources
-	 * @throws CoreException
-	 */
-	protected IModuleResource[] getMembers(IContainer cont, IPath path, IPath javaPath, IContainer[] javaCont) throws CoreException {
-		IResource[] res = cont.members();
-		int size2 = res.length;
-		List list = new ArrayList(size2);
-		for (int j = 0; j < size2; j++) {
-			if (res[j] instanceof IContainer) {
-				IContainer cc = (IContainer) res[j];
-				// Retrieve already existing module folder if applicable
-				ModuleFolder mf = (ModuleFolder) getExistingModuleResource(members,new Path(cc.getName()));
-				if (mf == null)
-					mf = new ModuleFolder(cc, cc.getName(), path);
-				IModuleResource[] mr = getMembers(cc, path.append(cc.getName()), javaPath, javaCont);
-				IPath curPath = path.append(cc.getName());
-				
-				if (javaPath != null && curPath.isPrefixOf(javaPath))
-					mr = handleJavaPath(path, javaPath, curPath, javaCont, mr, cc);
-
-				addMembersToModuleFolder(mf, mr);
-				list.add(mf);
-			} else {
-				IFile f = (IFile) res[j];
-				ModuleFile mf = new ModuleFile(f, f.getName(), path, f.getModificationStamp());
-				list.add(mf);
-			}
-		}
-		IModuleResource[] mr = new IModuleResource[list.size()];
-		list.toArray(mr);
-		return mr;
-	}
-	
-	private IModuleResource[] handleJavaPath(IPath path, IPath javaPath, IPath curPath, IContainer[] javaCont, IModuleResource[] mr, IContainer cc) throws CoreException {
+	protected IModuleResource[] handleJavaPath(IPath path, IPath javaPath, IPath curPath, IContainer[] javaCont, IModuleResource[] mr, IContainer cc) throws CoreException {
 		if (curPath.equals(javaPath)) {
 			int size = javaCont.length;
 			for (int i = 0; i < size; i++) {
@@ -256,29 +182,6 @@ public class J2EEFlexProjDeployable extends ProjectModule implements IJ2EEModule
 		}
 		return mr;
 	}
-	
-	private void addMembersToModuleFolder(ModuleFolder mf, IModuleResource[] mr) {
-		IModuleResource[] existingMembers = mf.members();
-		if (existingMembers==null)
-			existingMembers = new IModuleResource[0];
-		List membersJoin = new ArrayList();
-		membersJoin.addAll(Arrays.asList(existingMembers));
-		List newMembers = Arrays.asList(mr);
-		for (int i=0; i<newMembers.size(); i++) {
-			boolean found = false;
-			IModuleResource newMember = (IModuleResource) newMembers.get(i);
-			for (int k=0; k<membersJoin.size(); k++) {
-				IModuleResource existingResource = (IModuleResource) membersJoin.get(k);
-				if (existingResource.equals(newMember)) {
-					found = true;
-					break;
-				}
-			}
-			if (!found)
-				membersJoin.add(newMember);
-		}
-		mf.setMembers((IModuleResource[]) membersJoin.toArray(new IModuleResource[membersJoin.size()]));
-	}
 
     /**
      * Returns the classpath as a list of absolute IPaths.
@@ -315,30 +218,6 @@ public class J2EEFlexProjDeployable extends ProjectModule implements IJ2EEModule
 				ejbEdit.dispose();
 		}
 		return modHelper == null ? null : modHelper.getJNDIName(jar, jar.getEnterpriseBeanNamed(ejbName));
-	}
-
-    /**
-     * Returns the child modules of this module.
-     * 
-     * @return org.eclipse.wst.server.core.model.IModule[]
-     */
-    public IModule[] getChildModules() {
-        return getModules();
-    }
-
-    public IModule[] getModules() {
-		List modules = new ArrayList();
-    	IVirtualReference[] components = component.getReferences();
-    	for (int i = 0; i < components.length; i++) {
-			IVirtualReference reference = components[i];
-			IVirtualComponent virtualComp = reference.getReferencedComponent();
-			if (virtualComp.getProject()!=component.getProject()) {
-				Object module = FlexibleProjectServerUtil.getModule(virtualComp.getProject());
-				if (module != null && !modules.contains(module))
-					modules.add(module);
-			}
-		}
-      return (IModule[]) modules.toArray(new IModule[modules.size()]);
 	}
 
     public String getURI(IModule module) {
