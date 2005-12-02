b08a9cb51a85d0ce1137a069d4afb9358ec3c4db
cbridgha <cbridgha>
2005-12-02 18:41:30 +0000

    This commit was manufactured by cvs2svn to create tag 'I20051205a'.

--- plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/project/facet/ISimpleWebFacetInstallDataModelProperties.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.wst.project.facet;
-
-public interface ISimpleWebFacetInstallDataModelProperties {
-
-	public static final String CONTENT_DIR = "IStaticWebFacetInstallDataModelProperties.CONTENT_DIR"; //$NON-NLS-1$
-}
