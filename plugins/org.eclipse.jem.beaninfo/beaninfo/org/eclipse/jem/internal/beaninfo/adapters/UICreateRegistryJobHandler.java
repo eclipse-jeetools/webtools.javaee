ac72307eb63cde5178c78f2bc57f9a77d312041a
rkulp <rkulp>
2006-02-01 23:39:31 +0000

    [126116] Deadlock when trying to start remote vm from ui thread when ui thread already has the build rule.

--- plugins/org.eclipse.jem.beaninfo/beaninfo/org/eclipse/jem/internal/beaninfo/adapters/UICreateRegistryJobHandler.java
+++ plugins/org.eclipse.jem.beaninfo/beaninfo/org/eclipse/jem/internal/beaninfo/adapters/UICreateRegistryJobHandler.java
@@ -10,7 +10,7 @@
  *******************************************************************************/
 /*
  *  $RCSfile: UICreateRegistryJobHandler.java,v $
- *  $Revision: 1.7 $  $Date: 2005/08/24 20:31:29 $ 
+ *  $Revision: 1.8 $  $Date: 2006/02/01 23:39:31 $ 
  */
 package org.eclipse.jem.internal.beaninfo.adapters;
 
@@ -19,86 +19,88 @@ import java.util.logging.Level;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.*;
-import org.eclipse.core.runtime.jobs.IJobManager;
-import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.core.runtime.jobs.*;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.progress.IProgressService;
 
 import org.eclipse.jem.internal.beaninfo.core.BeaninfoPlugin;
- 
 
 /**
- * This is the version which can actually reference and use the UI classes. It won't be
- * loaded except if ui plugin is available.
+ * This is the version which can actually reference and use the UI classes. It won't be loaded except if ui plugin is available.
  * 
- * It will check to see if UI is running, and if it is not, then let super class handle.
- * If it is running, then if this is the UI thread, do special but run in ui, else if not then
- * let super handle it normally.
+ * It will check to see if UI is running, and if it is not, then let super class handle. If it is running, then if this is the UI thread, do special
+ * but run in ui, else if not then let super handle it normally.
  * 
  * @since 1.0.0
  */
 class UICreateRegistryJobHandler extends CreateRegistryJobHandler {
-	
-	
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.jem.internal.beaninfo.adapters.CreateRegistryJobHandler#processCreateRegistry(org.eclipse.jem.internal.beaninfo.adapters.BeaninfoNature)
 	 */
 	protected void processCreateRegistry(final BeaninfoNature nature) {
 		if (PlatformUI.isWorkbenchRunning()) {
 			if (Display.getCurrent() == null)
-				super.processCreateRegistry(nature);	// We are not in the UI thread. Do normal.
+				super.processCreateRegistry(nature); // We are not in the UI thread. Do normal.
 			else {
-				// We are in the UI, so try to run in UI. Really don't want to do this, but we need to
-				// get the build rule, and no way to know if the UI thread already has the build rule.
-				// It would be nice to spawn off thread to do it, but if we had the build rule we couldn't
-				// get it for the spawned thread. So hopefully it won't be long.
-				IProgressService ps = PlatformUI.getWorkbench().getProgressService();				
+				IProgressService ps = PlatformUI.getWorkbench().getProgressService();
 				final ISchedulingRule buildRule = ResourcesPlugin.getWorkspace().getRuleFactory().buildRule();
-
-// TODO Note: For now, runInUI is just way too annoying. It always flashes up. It is supposed to only flash up
-// when it is blocked by the rule. I've opened 66683 to address this. I've also opened 66690 to be able
-// to test if the thread has the rule so that runInUI will only be used if UI thread had the rule.
-// For now take the risk that the UI thread won't ever have the rule. This is pretty likely to be true.
-//				final boolean[] gotRuleLocally = new boolean[] {false};				
-//				try {
-//					ps.runInUI(ps, new IRunnableWithProgress() {
-//
-//						public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-//							gotRuleLocally[0] = true;
-//							doCreateRegistry(nature, new NullProgressMonitor());
-//						}
-//					}, buildRule);
-//				} catch (InvocationTargetException e) {
-//					if (e.getCause() instanceof IllegalArgumentException && !gotRuleLocally[0]) { 
-//						// Error was because rule conflicted with currect rules for this thread. So do in separate thread, should be able to get rule there.
-						try {
-							ps.busyCursorWhile(new IRunnableWithProgress() {
-
-								public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-									IJobManager jm = Platform.getJobManager();
-									monitor.beginTask("", 200); //$NON-NLS-1$
-									try {
-										jm.beginRule(buildRule, new SubProgressMonitor(monitor, 100));
-										doCreateRegistry(nature, new SubProgressMonitor(monitor, 100));
-									} finally {
-										jm.endRule(buildRule);
-										monitor.done();
-									}
+				// First find out if we already have the build rule. If we do, we MUST run in the UI thread. Have no option, can't transfer the
+				// rule to the thread and then back to us when done.
+				// This means a busy cursor instead of a possible progress dialog to keep the UI active. Only possible deadlock
+				// situation is if some other thread has the build rule (other than the build) and is doing an asyncExec or
+				// is waiting on something from the UI thread.
+				final IJobManager jobManager = Platform.getJobManager();
+				Job currentJob = jobManager.currentJob();
+				if (currentJob == null || currentJob.getRule() == null || !currentJob.getRule().contains(buildRule)) {
+					// UI is not in a job, or it is and there is no rule, or there is a rule and it is not compatible with the buildRule.
+					// In that case we need to run in another thread. That is so that we can put up a progress dialog if it takes
+					// too long. 
+					// If no rule then we can't run in UI because some other job may have the rule, and there is no way to test
+					// for this. So we don't want to block the UI entirely waiting for the rule, so we can use progress dialog
+					// for this. Also if we have a rule and it conflicts with build rule then we should also go to another thread.
+					// busyCursorWhile will do this. It will run the create in another thread but keep the UI responsive with
+					// progress dialog after a "long operation" timeout.
+					try {
+						ps.busyCursorWhile(new IRunnableWithProgress() {
+	
+							public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+								monitor.beginTask("", 200); //$NON-NLS-1$
+								try {
+									jobManager.beginRule(buildRule, new SubProgressMonitor(monitor, 100));
+									doCreateRegistry(nature, new SubProgressMonitor(monitor, 100));
+								} finally {
+									jobManager.endRule(buildRule);
+									monitor.done();
 								}
-							});
-						} catch (InterruptedException e1) {
-						} catch (InvocationTargetException e2) {
-							BeaninfoPlugin.getPlugin().getLogger().log(e2.getCause(), Level.WARNING);
+							}
+						});
+					} catch (InterruptedException e1) {
+					} catch (InvocationTargetException e2) {
+						BeaninfoPlugin.getPlugin().getLogger().log(e2.getCause(), Level.WARNING);
+					}
+				} else {
+					// We are a job in the UI and we have or can contain the build rule. Therefor we must run in the UI thread itself. Otherwise
+					// there will be a deadlock because we have the build rule and the other thread won't be able to get it.
+					// Note: There is ProgressService.runInUI() but that guy is complicated with some interactions I'm not sure about. Safer to do this.
+					BusyIndicator.showWhile(Display.getCurrent(), new Runnable() {
+						public void run() {
+							try {
+								jobManager.beginRule(buildRule, new NullProgressMonitor());
+								doCreateRegistry(nature, new NullProgressMonitor());
+							} finally {
+								jobManager.endRule(buildRule);
+							}
 						}
-//					} else
-//						BeaninfoPlugin.getPlugin().getLogger().log(e.getCause(), Level.WARNING);						
-//				} catch (InterruptedException e) {
-//				}
+					});
+				}
 			}
 		} else
-			super.processCreateRegistry(nature);	// Workbench not running, do default.
+			super.processCreateRegistry(nature); // Workbench not running, do default.
 	}
 }
