825064f5031ca42827e47da4666d607983cf1526
vbhadrir <vbhadrir>
2006-03-15 20:08:13 +0000

    [129360] commited for NA

--- plugins/org.eclipse.jst.j2ee.jca/jca/org/eclipse/jst/j2ee/jca/project/facet/ConnectorFacetInstallDelegate.java
+++ plugins/org.eclipse.jst.j2ee.jca/jca/org/eclipse/jst/j2ee/jca/project/facet/ConnectorFacetInstallDelegate.java
@@ -10,6 +10,9 @@
  *******************************************************************************/
 package org.eclipse.jst.j2ee.jca.project.facet;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.List;
@@ -24,12 +27,14 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jem.util.emf.workbench.ProjectUtilities;
+import org.eclipse.jem.util.emf.workbench.WorkbenchByteArrayOutputStream;
 import org.eclipse.jem.util.logger.proxy.Logger;
 import org.eclipse.jst.common.project.facet.WtpUtils;
 import org.eclipse.jst.common.project.facet.core.ClasspathHelper;
@@ -38,8 +43,11 @@ import org.eclipse.jst.j2ee.application.Module;
 import org.eclipse.jst.j2ee.application.internal.operations.AddComponentToEnterpriseApplicationDataModelProvider;
 import org.eclipse.jst.j2ee.application.internal.operations.AddComponentToEnterpriseApplicationOp;
 import org.eclipse.jst.j2ee.application.internal.operations.IAddComponentToEnterpriseApplicationDataModelProperties;
+import org.eclipse.jst.j2ee.commonarchivecore.internal.util.ArchiveUtil;
 import org.eclipse.jst.j2ee.internal.J2EEConstants;
+import org.eclipse.jst.j2ee.internal.J2EEVersionConstants;
 import org.eclipse.jst.j2ee.internal.common.J2EEVersionUtil;
+import org.eclipse.jst.j2ee.jca.Connector;
 import org.eclipse.jst.j2ee.jca.modulecore.util.ConnectorArtifactEdit;
 import org.eclipse.jst.j2ee.project.facet.IJ2EEFacetInstallDataModelProperties;
 import org.eclipse.jst.j2ee.project.facet.IJ2EEModuleFacetInstallDataModelProperties;
@@ -62,6 +70,9 @@ import org.eclipse.wst.common.project.facet.core.runtime.IRuntime;
 
 public class ConnectorFacetInstallDelegate extends J2EEFacetInstallDelegate implements IDelegate {
 
+	public static final String CONNECTOR_XML_TEMPLATE_10		= "rartp10.xml";								//$NON-NLS-1$
+	public static final String CONNECTOR_XML_TEMPLATE_15		= "rartp15.xml"; //$NON-NLS-1$
+	
 	public void execute(IProject project, IProjectFacetVersion fv, Object config, IProgressMonitor monitor) throws CoreException {
 		if (monitor != null) {
 			monitor.beginTask("", 1); //$NON-NLS-1$
@@ -100,7 +111,46 @@ public class ConnectorFacetInstallDelegate extends J2EEFacetInstallDelegate impl
 			if (!sourceFolder.getFile(J2EEConstants.RAR_DD_URI).exists()) {
 				String ver = model.getStringProperty(IFacetDataModelProperties.FACET_VERSION_STR);
 				int nVer = J2EEVersionUtil.convertVersionStringToInt(ver);
-				ConnectorArtifactEdit.createDeploymentDescriptor(project, nVer);
+				
+				IFile aFile = sourceFolder.getFile(new Path(J2EEConstants.RAR_DD_URI));
+				OutputStream out = new WorkbenchByteArrayOutputStream(aFile);
+				String template = nVer == J2EEVersionConstants.JCA_1_0_ID
+						? CONNECTOR_XML_TEMPLATE_10
+						: CONNECTOR_XML_TEMPLATE_15;
+				InputStream in = getClass().getResourceAsStream(template);
+				if (in != null & out != null) {
+					try {
+						ArchiveUtil.copy(in, out);
+					} catch (IOException ioe) {
+						Logger.getLogger().logError(ioe);
+					} finally{
+						try{
+							if(null != out){
+								out.close();
+							} if(null != in){
+								in.close();
+							}
+						}catch (IOException ioe) {
+							Logger.getLogger().logError(ioe);
+						} 
+					}
+					
+					ConnectorArtifactEdit edit = new ConnectorArtifactEdit(project, false, true);
+					try{
+						if( edit != null ){
+							Connector connector = edit.getConnector();
+							connector.setDisplayName(project.getName());
+							edit.saveIfNecessary(new NullProgressMonitor());
+						}
+					}finally{
+						if( edit != null ){
+							edit.dispose();
+						}
+					}
+				} else {
+					//without a template
+					ConnectorArtifactEdit.createDeploymentDescriptor(project, nVer);
+				} // if				
 			}
 			
 			// add source folder maps
