6f89613f3accf64b2ee0489bceb9ef8e77bcfd97
jlanuti <jlanuti>
2006-03-16 18:33:32 +0000

    [130992] fix for link with editor

--- plugins/org.eclipse.jst.j2ee.navigator.ui/j2ee-navigator/org/eclipse/jst/j2ee/navigator/internal/J2EELabelProvider.java
+++ plugins/org.eclipse.jst.j2ee.navigator.ui/j2ee-navigator/org/eclipse/jst/j2ee/navigator/internal/J2EELabelProvider.java
@@ -150,7 +150,9 @@ public class J2EELabelProvider implements ICommonLabelProvider {
 		if(element instanceof J2EEJavaClassProviderHelper)
 			return ((J2EEJavaClassProviderHelper) element).getImage();
 		if (element instanceof File)
-			return J2EEUIPlugin.getDefault().getImage("jar_obj"); 
+			return J2EEUIPlugin.getDefault().getImage("jar_obj");
+		if (element instanceof IProject)
+			return null;
 		if (delegateLabelProvider != null)
 			return delegateLabelProvider.getImage(element);
 		return null;
@@ -165,6 +167,8 @@ public class J2EELabelProvider implements ICommonLabelProvider {
 			return ((J2EEJavaClassProviderHelper) element).getText();
 		if (element instanceof File)
 			return ((File)element).getName();
+		if (element instanceof IProject)
+			return ((IProject)element).getName();
 		if (delegateLabelProvider != null)
 			return delegateLabelProvider.getText(element);
 		return null;
