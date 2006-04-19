befde7e54146b2ee41ba73e6827ca3a1bee5b5c4
jlanuti <jlanuti>
2006-04-19 18:05:03 +0000

    [136524] fix for EARArtifactEdit module uri

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/componentcore/util/EARArtifactEdit.java
+++ plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/componentcore/util/EARArtifactEdit.java
@@ -418,7 +418,7 @@ public class EARArtifactEdit extends EnterpriseArtifactEdit implements IArtifact
 						
 				} 
 				else if (moduleComp.isBinary()) {
-					if (ref.getHandle().equals(ModuleURIUtil.archiveComponentfullyQualifyURI(moduleComp.getName())));
+					if (ref.getHandle().equals(ModuleURIUtil.archiveComponentfullyQualifyURI(moduleComp.getName())))
 						return ((Module)ref.getDependentObject()).getUri();
 				}	
 			}
