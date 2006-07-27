d77e46532a69ac56cf6d09cd457defeff2f4b7ff
cbridgha <cbridgha>
2006-07-27 17:03:43 +0000

    [nobug] JS preformance; no new String()

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/componentcore/util/EARVirtualRootFolder.java
+++ plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/componentcore/util/EARVirtualRootFolder.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.jst.j2ee.internal.common.classpath.J2EEComponentClasspathUpdater;
 import org.eclipse.wst.common.componentcore.internal.resources.VirtualFolder;
 import org.eclipse.wst.common.componentcore.resources.IVirtualResource;
 
@@ -15,6 +16,8 @@ public class EARVirtualRootFolder extends VirtualFolder {
 		super(aComponentProject, aRuntimePath);
 	}
 
+	public static String [] EXTENSIONS_TO_IGNORE = new String [] {".jar", ".zip", ".rar", ".war" };
+	
 	/**
 	 * For now, just rip out files with .jar, .rar, or .war file extensions, because these are
 	 * the only files automatically added dyamically
@@ -27,9 +30,10 @@ public class EARVirtualRootFolder extends VirtualFolder {
 			shouldAdd = true;
 			if (IVirtualResource.FILE == members[i].getType()) {
 				String archiveName = members[i].getName();
-				String lowerCase = archiveName.toLowerCase();
-				if (lowerCase.endsWith(".jar") || lowerCase.endsWith(".rar") || lowerCase.endsWith(".war")) {
-					shouldAdd = false;
+				for(int j = 0; j<EXTENSIONS_TO_IGNORE.length && shouldAdd; j++){
+					if(J2EEComponentClasspathUpdater.endsWithIgnoreCase(archiveName, EXTENSIONS_TO_IGNORE[j])){
+						shouldAdd = false;
+					}
 				}
 			}
 			if (shouldAdd) {
