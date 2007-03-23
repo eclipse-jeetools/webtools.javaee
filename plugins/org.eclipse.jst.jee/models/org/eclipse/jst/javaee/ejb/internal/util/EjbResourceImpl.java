53080d0ae645dae4c9a2a20787b8bbc36ee1811a
cbridgha <cbridgha>
2007-03-23 19:06:33 +0000

    [175139] Initial changes for extended model

--- plugins/org.eclipse.jst.jee/models/org/eclipse/jst/javaee/ejb/internal/util/EjbResourceImpl.java
+++ plugins/org.eclipse.jst.jee/models/org/eclipse/jst/javaee/ejb/internal/util/EjbResourceImpl.java
@@ -2,12 +2,13 @@
  * <copyright>
  * </copyright>
  *
- * $Id: EjbResourceImpl.java,v 1.1 2007/03/20 18:04:41 jsholl Exp $
+ * $Id: EjbResourceImpl.java,v 1.2 2007/03/23 19:06:33 cbridgha Exp $
  */
 package org.eclipse.jst.javaee.ejb.internal.util;
 
 import org.eclipse.emf.common.util.URI;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
 
 /**
@@ -27,6 +28,15 @@ public class EjbResourceImpl extends XMLResourceImpl {
 	 */
 	public EjbResourceImpl(URI uri) {
 		super(uri);
+	}
+	
+	/**
+	 * Return the first element in the EList.
+	 */
+	public EObject getRootObject() {
+		if (contents == null || contents.isEmpty())
+			return null;
+		return (EObject) getContents().get(0);
 	}
 
 } //EjbResourceImpl
