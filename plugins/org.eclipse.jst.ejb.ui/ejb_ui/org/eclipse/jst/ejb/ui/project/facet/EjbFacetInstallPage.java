77536c2a52a7cb147269878cf29e1564a279b52f
cbridgha <cbridgha>
2007-06-07 19:32:49 +0000

    [191373] copyright update for WTP 2.0

--- plugins/org.eclipse.jst.ejb.ui/ejb_ui/org/eclipse/jst/ejb/ui/project/facet/EjbFacetInstallPage.java
+++ plugins/org.eclipse.jst.ejb.ui/ejb_ui/org/eclipse/jst/ejb/ui/project/facet/EjbFacetInstallPage.java
@@ -1,12 +1,13 @@
-/***************************************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others. All rights reserved. This program and the
- * accompanying materials are made available under the terms of the Eclipse Public License v1.0
+/*******************************************************************************
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: IBM Corporation - initial API and implementation
+ *
+ * Contributors:
  *				 David Schneider, david.schneider@unisys.com - [142500] WTP properties pages fonts don't follow Eclipse preferences
- **************************************************************************************************/
+ *******************************************************************************/
 package org.eclipse.jst.ejb.ui.project.facet;
 
 import org.eclipse.jface.dialogs.Dialog;
