91fb62e03ff86ee643dafc85cafab32df6cfd43c
cbridgha <cbridgha>
2007-06-12 13:53:29 +0000

    [190198] sync JEE5 models w/ updates

--- plugins/org.eclipse.jst.jee/jeecreation/org/eclipse/jst/jee/model/internal/JEE5ModelProvider.java
+++ plugins/org.eclipse.jst.jee/jeecreation/org/eclipse/jst/jee/model/internal/JEE5ModelProvider.java
@@ -12,10 +12,18 @@ package org.eclipse.jst.jee.model.internal;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
@@ -24,6 +32,8 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.common.util.WrappedException;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
 import org.eclipse.jem.util.emf.workbench.ProjectResourceSet;
 import org.eclipse.jem.util.emf.workbench.WorkbenchResourceHelperBase;
@@ -37,6 +47,8 @@ public class JEE5ModelProvider implements IModelProvider{
 	protected XMLResourceImpl writableResource;
 	protected IProject proj;
 	protected IPath defaultResourcePath;
+	private static boolean resourceChangeListenerEnabled = false;
+	private static HashMap<IProject, HashSet<IPath>> modelResources = new HashMap<IProject, HashSet<IPath>>();
 
 	public JEE5ModelProvider() {
 		super();
@@ -66,7 +78,21 @@ public class JEE5ModelProvider implements IModelProvider{
 		XMLResourceImpl res = null;
 		try {
 			if (proj.getFile(uri.toString()).exists())
+			{
 				res = (XMLResourceImpl) resSet.getResource(uri,true);
+				HashSet<IPath> currentResources = modelResources.get(proj);
+				if (currentResources == null)
+				{
+					currentResources = new HashSet<IPath>();
+				}
+				currentResources.add(new Path(uri.toString()));
+				modelResources.put(proj, currentResources);
+				if (!resourceChangeListenerEnabled)
+				{
+					resourceChangeListenerEnabled = true;
+					ResourcesPlugin.getWorkspace().addResourceChangeListener(new ResourceChangeListener(), IResourceChangeEvent.POST_CHANGE);
+				}
+			}
 		} catch (WrappedException ex) {
 			if (ex.getCause() instanceof FileNotFoundException)
 				return null;
@@ -128,4 +154,77 @@ public class JEE5ModelProvider implements IModelProvider{
 		
 	}
 
+	private class ResourceChangeListener implements IResourceChangeListener {
+		public void resourceChanged(IResourceChangeEvent event) {
+			IResourceDelta delta= event.getDelta();
+			// make sure that there is a delta (since some events don't have one)
+			if (delta != null)
+			{
+				IResourceDelta[] affectedChildren= delta.getAffectedChildren(IResourceDelta.CHANGED | IResourceDelta.REMOVED , IResource.FILE);
+				IResourceDelta projectDelta = null;
+				IResource changedResource = null; 
+				IProject changedProject = null;
+				IPath resourcePath = null;
+
+				for (int i= 0; i < affectedChildren.length; i++) {
+					projectDelta = affectedChildren[i];
+					changedResource = projectDelta.getResource(); 
+					changedProject = changedResource.getProject();
+					HashSet<IPath> currentResources = modelResources.get(changedProject);
+					// only deal with the projects that have resources that have been loaded 
+					if (currentResources != null)
+					{
+						// if this is a project deletion, remove the project from the HashMap.
+						if (changedResource == changedProject && projectDelta.getKind() == IResourceDelta.REMOVED)
+						{
+							modelResources.remove(changedProject);
+							// if modelResources is empty, we should self-destruct
+							if (modelResources.isEmpty())
+							{
+								resourceChangeListenerEnabled = false;
+								ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
+							}
+						}
+						else
+						{
+							Iterator<IPath> iter = currentResources.iterator();
+							ArrayList<IPath> toUnload = new ArrayList<IPath>();
+							// check each resource that was loaded from the project to see if it is part of the change
+							while (iter.hasNext())
+							{
+								resourcePath = iter.next();
+								if (projectDelta.findMember(resourcePath) != null)
+								{
+									// limit the list of resources that need to be unloaded to those that have changed
+									toUnload.add(resourcePath);
+								}
+							}
+							if (toUnload.size() > 0)
+							{
+								Resource current = null;
+								ProjectResourceSet resourceSet = getResourceSet(changedProject);
+								URIConverter uriConverter = resourceSet.getURIConverter();
+								HashSet<URI> resourceURIs = new HashSet<URI>();
+								iter = toUnload.iterator();
+								while (iter.hasNext())
+								{
+									// convert all of the resources to URIs - this is a faster match during the compare
+									resourceURIs.add(uriConverter.normalize(URI.createURI(iter.next().toString())));
+								}
+								Iterator<Resource> iter2 = resourceSet.getResources().iterator();
+								while (iter2.hasNext())
+								{
+									current = iter2.next();
+									if (resourceURIs.contains(current.getURI()))
+									{
+										current.unload();
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
