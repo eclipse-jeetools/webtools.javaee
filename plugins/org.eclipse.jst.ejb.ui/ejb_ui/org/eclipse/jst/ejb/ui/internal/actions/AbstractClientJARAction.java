4a912685064cf9ef5ab956680633228141feeee5
canderson <canderson>
2008-07-31 15:46:48 +0000

    [241825] Remove ejbClient jar action not enabled for ejb 3.0

--- plugins/org.eclipse.jst.ejb.ui/ejb_ui/org/eclipse/jst/ejb/ui/internal/actions/AbstractClientJARAction.java
+++ plugins/org.eclipse.jst.ejb.ui/ejb_ui/org/eclipse/jst/ejb/ui/internal/actions/AbstractClientJARAction.java
@@ -13,14 +13,15 @@ package org.eclipse.jst.ejb.ui.internal.actions;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jem.util.emf.workbench.ProjectUtilities;
 import org.eclipse.jst.j2ee.ejb.EJBJar;
-import org.eclipse.jst.j2ee.ejb.componentcore.util.EJBArtifactEdit;
-import org.eclipse.jst.j2ee.internal.J2EEVersionConstants;
+import org.eclipse.jst.j2ee.internal.J2EEConstants;
 import org.eclipse.jst.j2ee.internal.actions.BaseAction;
-import org.eclipse.jst.j2ee.internal.common.J2EEVersionUtil;
 import org.eclipse.jst.j2ee.internal.ejb.project.operations.ClientJARCreationConstants;
 import org.eclipse.jst.j2ee.internal.project.J2EEProjectUtilities;
+import org.eclipse.jst.j2ee.model.IModelProvider;
+import org.eclipse.jst.j2ee.model.ModelProviderManager;
 
 
 
@@ -46,25 +47,18 @@ public abstract class AbstractClientJARAction extends BaseAction implements Clie
 
 		if (project != null && project.exists() && project.isAccessible()) {
 			if (J2EEProjectUtilities.isEJBProject(project)) {
-				String projectVersion = J2EEProjectUtilities
-						.getJ2EEProjectVersion(project);
-				if (projectVersion != null) {
-					int projectVersionInt = J2EEVersionUtil
-							.convertVersionStringToInt(projectVersion);
-					if (projectVersionInt != 0
-							&& projectVersionInt <= J2EEVersionConstants.EJB_2_1_ID) {
-
-						EJBArtifactEdit edit = null;
-						try {
-							edit = EJBArtifactEdit
-									.getEJBArtifactEditForRead(project);
-							if (edit != null && edit.hasEJBClientJARProject())
-								return true;
-						} finally {
-							if (edit != null)
-								edit.dispose();
-
-						}
+				IModelProvider provider = ModelProviderManager.getModelProvider(project);
+				Object mObj = provider.getModelObject();
+				if (mObj != null && provider.getModelObject(new Path(J2EEConstants.EJBJAR_DD_URI)) != null) {
+					if (mObj instanceof org.eclipse.jst.javaee.ejb.EJBJar) {
+						org.eclipse.jst.javaee.ejb.EJBJar ejbjar = (org.eclipse.jst.javaee.ejb.EJBJar) mObj;
+						if (ejbjar.getEjbClientJar() != null)
+							return true;
+					}
+					else if (mObj instanceof org.eclipse.jst.j2ee.ejb.EJBJar) {
+						org.eclipse.jst.j2ee.ejb.EJBJar ejbjar = (org.eclipse.jst.j2ee.ejb.EJBJar) mObj;
+						if (ejbjar.getEjbClientJar() != null)
+							return true;
 					}
 				}
 			}
