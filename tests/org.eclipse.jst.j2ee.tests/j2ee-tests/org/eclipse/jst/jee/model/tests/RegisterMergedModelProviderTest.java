1ad8a0db7ca2c994ae8c27fd93d0c7aecb1aa74f
kmitov <kmitov>
2008-09-03 07:55:50 +0000

    [214136] ModelProviderManager::registerProvider does not initialize the providers. Adding test case. Commit.

--- tests/org.eclipse.jst.j2ee.tests/j2ee-tests/org/eclipse/jst/jee/model/tests/RegisterMergedModelProviderTest.java
+++ tests/org.eclipse.jst.j2ee.tests/j2ee-tests/org/eclipse/jst/jee/model/tests/RegisterMergedModelProviderTest.java
@@ -15,10 +15,12 @@ import junit.framework.TestSuite;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jst.j2ee.model.IModelProvider;
+import org.eclipse.jst.j2ee.model.IModelProviderFactory;
 import org.eclipse.jst.j2ee.model.ModelProviderManager;
 import org.eclipse.jst.javaee.ejb.EJBJar;
 import org.eclipse.jst.jee.model.internal.EJB3MergedModelProvider;
 import org.eclipse.jst.jee.model.internal.Ejb3MergedModelProviderFactory;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
 
@@ -35,17 +37,14 @@ public class RegisterMergedModelProviderTest extends TestCase {
 		return suite;
 	}
 
-	// @Before
 	public void setUp() throws Exception {
 		fixture = AbstractTest.createEjbProject(RegisterMergedModelProviderTest.class.getName()).getProject();
 	}
 
-	// @After
 	public void tearDown() throws Exception {
 		AbstractTest.deleteProject(RegisterMergedModelProviderTest.class.getName());
 	}
 
-	// @Test
 	public void testRegister() {
 		IProjectFacetVersion version = ProjectFacetsManager.getProjectFacet("jst.ejb").getVersion("3.0");
 		IModelProvider oldProvider = ModelProviderManager.getModelProvider(getFixture());
@@ -56,6 +55,32 @@ public class RegisterMergedModelProviderTest extends TestCase {
 		assertTrue(EJB3MergedModelProvider.class.isInstance(provider));
 		assertNotNull(provider.getModelObject());
 		assertTrue(EJBJar.class.isInstance(provider.getModelObject()));
+	}
+
+	/**
+	 * Call register method first without calling getModelProvider method before
+	 * that. Test case for bug [214136]
+	 */
+	public void testCallRegisterMethodFirst() {
+		IProjectFacetVersion version = ProjectFacetsManager.getProjectFacet("jst.ejb").getVersion("3.0");
+		ModelProviderManager.registerProvider(new MyModelProviderFactory(), version, "3");
+		IModelProvider provider = ModelProviderManager.getModelProvider(getFixture());
+		assertNull(provider);
+		
+		// register the real model provider so that this test does not affect
+		// the ather tests.
+		ModelProviderManager.registerProvider(new Ejb3MergedModelProviderFactory(), version, "3");
+	}
+
+	private static class MyModelProviderFactory implements IModelProviderFactory {
+
+		public IModelProvider create(IProject project) {
+			return null;
+		}
+
+		public IModelProvider create(IVirtualComponent component) {
+			return null;
+		}
 	}
 
 	private IProject getFixture() {
