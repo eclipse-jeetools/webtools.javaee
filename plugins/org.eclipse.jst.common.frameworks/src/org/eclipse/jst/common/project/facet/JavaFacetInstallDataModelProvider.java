07907b60c3859cb73eb65a4c42ce8a7f9c74dd9c
kkomissarchik <kkomissarchik>
2008-11-13 21:20:29 +0000

    [250738] Cannot add second source folder to JavaFacetInstallConfig

--- plugins/org.eclipse.jst.common.frameworks/src/org/eclipse/jst/common/project/facet/JavaFacetInstallDataModelProvider.java
+++ plugins/org.eclipse.jst.common.frameworks/src/org/eclipse/jst/common/project/facet/JavaFacetInstallDataModelProvider.java
@@ -27,21 +27,58 @@ public class JavaFacetInstallDataModelProvider extends FacetInstallDataModelProv
 
     private final JavaFacetInstallConfig installConfig;
     
-	public JavaFacetInstallDataModelProvider() {
+	public JavaFacetInstallDataModelProvider() 
+	{
 		this( new JavaFacetInstallConfig() );
 	}
 	
 	public JavaFacetInstallDataModelProvider( final JavaFacetInstallConfig installConfig )
 	{
 	    this.installConfig = installConfig;
-	    
-	    final IEventListener<JavaFacetInstallConfig.ChangeEvent> listener
-	        = new IEventListener<JavaFacetInstallConfig.ChangeEvent>()
+	}
+
+	public Set getPropertyNames() 
+	{
+		Set propertyNames = super.getPropertyNames();
+		propertyNames.add(JAVA_FACET_INSTALL_CONFIG);
+		propertyNames.add(SOURCE_FOLDER_NAME);
+        propertyNames.add(DEFAULT_OUTPUT_FOLDER_NAME);
+		return propertyNames;
+	}
+
+    @Override
+    public void init()
+    {
+        super.init();
+        
+        final IDataModel dm = getDataModel();
+        
+        dm.setProperty( FACET_ID, IModuleConstants.JST_JAVA );
+        dm.setProperty( JAVA_FACET_INSTALL_CONFIG, this.installConfig );
+        
+        String sourceFolderName = null;
+        
+        if( ! this.installConfig.getSourceFolders().isEmpty() )
+        {
+            sourceFolderName = this.installConfig.getSourceFolders().get( 0 ).toPortableString();
+        }
+        
+        dm.setProperty( SOURCE_FOLDER_NAME, sourceFolderName );
+        
+        String defaultOutputFolderName = null;
+        
+        if( this.installConfig.getDefaultOutputFolder() != null )
+        {
+            defaultOutputFolderName = this.installConfig.getDefaultOutputFolder().toPortableString();
+        }
+        
+        dm.setProperty( DEFAULT_OUTPUT_FOLDER_NAME, defaultOutputFolderName );
+        
+        final IEventListener<JavaFacetInstallConfig.ChangeEvent> listener
+            = new IEventListener<JavaFacetInstallConfig.ChangeEvent>()
         {
             public void handleEvent( final ChangeEvent event )
             {
-                final IDataModel dm = getDataModel();
-
                 if( event.getType() == JavaFacetInstallConfig.ChangeEvent.Type.SOURCE_FOLDERS_CHANGED )
                 {
                     String val = null;
@@ -62,16 +99,8 @@ public class JavaFacetInstallDataModelProvider extends FacetInstallDataModelProv
         };
         
         this.installConfig.addListener( listener );
-	}
-
-	public Set getPropertyNames() {
-		Set propertyNames = super.getPropertyNames();
-		propertyNames.add(JAVA_FACET_INSTALL_CONFIG);
-		propertyNames.add(SOURCE_FOLDER_NAME);
-        propertyNames.add(DEFAULT_OUTPUT_FOLDER_NAME);
-		return propertyNames;
-	}
-
+    }
+	
 	public boolean propertySet( final String propertyName, 
 	                            final Object propertyValue )
     {
@@ -104,41 +133,5 @@ public class JavaFacetInstallDataModelProvider extends FacetInstallDataModelProv
         
         return super.propertySet( propertyName, propertyValue );
     }
-
-    public Object getDefaultProperty(String propertyName) {
-		if (FACET_ID.equals(propertyName)) {
-			return IModuleConstants.JST_JAVA;
-		}
-		else if( JAVA_FACET_INSTALL_CONFIG.equals( propertyName ) )
-		{
-		    return this.installConfig;
-		}
-		else if( SOURCE_FOLDER_NAME.equals( propertyName ) )
-		{
-		    if( this.installConfig.getSourceFolders().isEmpty() )
-		    {
-		        return null;
-		    }
-		    else
-		    {
-		        return this.installConfig.getSourceFolders().get( 0 ).toPortableString();
-		    }
-		}
-		else if( DEFAULT_OUTPUT_FOLDER_NAME.equals( propertyName ) )
-		{
-		    final IPath outputFolder = this.installConfig.getDefaultOutputFolder();
-		    
-		    if( outputFolder == null )
-		    {
-		        return null;
-		    }
-		    else
-		    {
-		        return outputFolder.toPortableString();
-		    }
-		}
-        
-		return super.getDefaultProperty(propertyName);
-	}
 
 }
