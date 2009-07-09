a391347a56b70daeae560bd053646d4caa865d76
jsholl <jsholl>
2009-07-09 03:20:07 +0000

    [282950] disconnect common archive from import

--- plugins/org.eclipse.jst.j2ee.ejb/ejbarchive/org/eclipse/jst/j2ee/internal/ejb/archiveoperations/EJBComponentImportOperation.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejbarchive/org/eclipse/jst/j2ee/internal/ejb/archiveoperations/EJBComponentImportOperation.java
@@ -14,14 +14,8 @@
  */
 package org.eclipse.jst.j2ee.internal.ejb.archiveoperations;
 
-import java.lang.reflect.InvocationTargetException;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jst.j2ee.commonarchivecore.internal.strategy.SaveStrategy;
-import org.eclipse.jst.j2ee.ejb.datamodel.properties.IEJBComponentImportDataModelProperties;
 import org.eclipse.jst.j2ee.internal.archive.ComponentArchiveSaveAdapter;
 import org.eclipse.jst.j2ee.internal.archive.EJBComponentArchiveSaveAdapter;
-import org.eclipse.jst.j2ee.internal.archive.operations.EJBComponentSaveStrategyImpl;
 import org.eclipse.jst.j2ee.internal.archive.operations.J2EEArtifactImportOperation;
 import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
@@ -32,33 +26,6 @@ public class EJBComponentImportOperation extends J2EEArtifactImportOperation {
 		super(dataModel);
 	}
 
-	@Override
-	protected void importModuleFile(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-		if (null == model.getProperty(IEJBComponentImportDataModelProperties.EXTENDED_IMPORT_FACTORY)) {
-			super.importModuleFile(monitor);
-			// EJBModuleImportDataModel dm = (EJBModuleImportDataModel) getOperationDataModel();
-			// IProject project = dm.getProject();
-			// dm.getJ2EEModuleCreationDataModel().getJ2EEVersion();
-			// if (!J2EEPlugin.hasDevelopmentRole() && project != null &&
-			// dm.getJ2EEModuleCreationDataModel().getJ2EEVersion() >=
-			// J2EEVersionConstants.J2EE_1_3_ID) {
-			// EJBUnknownPrimKeyHelper helper = new EJBUnknownPrimKeyHelper(dm);
-			// helper.setUpUnknowPrimaryKey();
-			// }
-		}
-		// else {
-		// ExtendedImportFactory factory = (ExtendedImportFactory)
-		// operationDataModel.getProperty(J2EEModuleImportDataModel.EXTENDED_IMPORT_FACTORY);
-		// factory.importModuleFile((J2EEModuleImportDataModel) operationDataModel, monitor);
-		// }
-	}
-
-	@Override
-	protected SaveStrategy createSaveStrategy(IVirtualComponent virtualComponent) {
-		EJBComponentSaveStrategyImpl saveStrat = new EJBComponentSaveStrategyImpl(virtualComponent);
-		return saveStrat;
-	}
-	
 	@Override
 	protected ComponentArchiveSaveAdapter getArchiveSaveAdapter(IVirtualComponent virtualComponent) {
 		return new EJBComponentArchiveSaveAdapter(virtualComponent);
