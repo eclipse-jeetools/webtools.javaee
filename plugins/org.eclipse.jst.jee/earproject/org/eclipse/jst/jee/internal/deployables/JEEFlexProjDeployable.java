990c5d80603b41108983b6466eaec245bb5601fd
rstryker <rstryker>
2009-09-02 21:05:45 +0000

    Bug 241332

--- plugins/org.eclipse.jst.jee/earproject/org/eclipse/jst/jee/internal/deployables/JEEFlexProjDeployable.java
+++ plugins/org.eclipse.jst.jee/earproject/org/eclipse/jst/jee/internal/deployables/JEEFlexProjDeployable.java
@@ -13,22 +13,21 @@ package org.eclipse.jst.jee.internal.deployables;
 import java.util.Iterator;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jst.j2ee.componentcore.J2EEModuleVirtualArchiveComponent;
-import org.eclipse.jst.j2ee.componentcore.util.EARArtifactEdit;
 import org.eclipse.jst.j2ee.internal.EjbModuleExtensionHelper;
 import org.eclipse.jst.j2ee.internal.IEJBModelExtenderManager;
+import org.eclipse.jst.j2ee.internal.J2EEVersionConstants;
+import org.eclipse.jst.j2ee.internal.componentcore.JavaEEBinaryComponentHelper;
 import org.eclipse.jst.j2ee.internal.deployables.J2EEFlexProjDeployable;
-import org.eclipse.jst.j2ee.internal.project.J2EEProjectUtilities;
 import org.eclipse.jst.j2ee.model.IEARModelProvider;
 import org.eclipse.jst.j2ee.model.IModelProvider;
 import org.eclipse.jst.j2ee.model.ModelProviderManager;
-import org.eclipse.jst.javaee.application.Application;
+import org.eclipse.jst.j2ee.project.JavaEEProjectUtilities;
 import org.eclipse.jst.javaee.ejb.EJBJar;
 import org.eclipse.jst.javaee.ejb.EnterpriseBeans;
 import org.eclipse.jst.javaee.ejb.SessionBean;
+import org.eclipse.jst.jee.util.internal.JavaEEQuickPeek;
 import org.eclipse.wst.common.componentcore.ArtifactEdit;
-import org.eclipse.wst.common.componentcore.ComponentCore;
 import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 import org.eclipse.wst.common.componentcore.resources.IVirtualReference;
 import org.eclipse.wst.server.core.IModule;
@@ -62,14 +61,14 @@ public class JEEFlexProjDeployable extends J2EEFlexProjDeployable {
 		// If the component module is an EAR we know all archives are filtered out of virtual component members
 		// and we will return only those archives which are not binary J2EE modules in the EAR DD.  These J2EE modules will
 		// be returned by getChildModules()
-		if (J2EEProjectUtilities.isEARProject(component.getProject()))
+		if (JavaEEProjectUtilities.isEARProject(component.getProject()))
 			return virtualComp != null && virtualComp.isBinary() && !isNestedJ2EEModule(virtualComp, references, model);
 		else 
 			return super.shouldIncludeUtilityComponent(virtualComp, references, ArtifactEdit.class.isInstance(model) ? (ArtifactEdit)model : null);
 	}
 	    
     public String getJNDIName(String ejbName, String interfaceName) {
-    	if (!J2EEProjectUtilities.isEJBProject(component.getProject()))
+    	if (!JavaEEProjectUtilities.isEJBProject(component.getProject()))
     		return null;
 
 		EjbModuleExtensionHelper modHelper = null;
@@ -100,62 +99,41 @@ public class JEEFlexProjDeployable extends J2EEFlexProjDeployable {
     	return null;
     }
 
-    
-    
-    private String getContainedURI(IModule module) {
-    	if (component instanceof J2EEModuleVirtualArchiveComponent || isBinaryModuleArchive(module))
-    		return new Path(module.getName()).lastSegment();
-    	
-    	IVirtualComponent comp = ComponentCore.createComponent(module.getProject());
-    	String aURI = null;
-    	if (comp!=null && component!=null && J2EEProjectUtilities.isEARProject(component.getProject())) {
-			//TODO when new JEE model api is available look at this code again
-    		// see bug 183320
-    		IVirtualReference [] refs = component.getReferences();
-    		for(int i=0; i<refs.length; i++){
-    			if(refs[i].getReferencedComponent().equals(comp)){
-    				aURI = refs[i].getArchiveName();
-    				break;
-    			}
-    		}
-    		EARArtifactEdit earEdit = null;
-			try {
-				earEdit = EARArtifactEdit.getEARArtifactEditForRead(component);
-				if (earEdit != null)
-					aURI = earEdit.getModuleURI(comp);
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				if (earEdit != null)
-					earEdit.dispose();
-			}
-    	}
-    	if (aURI !=null && aURI.length()>1 && aURI.startsWith("/")) //$NON-NLS-1$
-    		aURI = aURI.substring(1);
-    	return aURI;
-	} 
-    
     protected IModule gatherModuleReference(IVirtualComponent component, IVirtualComponent targetComponent ) {
     	IModule module = super.gatherModuleReference(component, targetComponent);
     	// Handle binary module components
     	if (targetComponent instanceof J2EEModuleVirtualArchiveComponent) {
-    		if (J2EEProjectUtilities.isEARProject(component.getProject()) || targetComponent.getProject()!=component.getProject())
+    		if (JavaEEProjectUtilities.isEARProject(component.getProject()) || targetComponent.getProject()!=component.getProject())
     			module = ServerUtil.getModule(JEEDeployableFactory.ID+":"+targetComponent.getName()); //$NON-NLS-1$
     	}
 		return module;
-    }
-    
+    }    
+
+    @Override
+	protected boolean shouldIncludeUtilityComponent(IVirtualComponent virtualComp,IVirtualReference[] references, ArtifactEdit edit) {
+    	if( edit instanceof IEARModelProvider) {
+    		if (JavaEEProjectUtilities.isEARProject(component.getProject()))
+    			return virtualComp != null && virtualComp.isBinary() && !isNestedJ2EEModule(virtualComp, references, (IEARModelProvider)edit);
+    	}
+    	return super.shouldIncludeUtilityComponent(virtualComp, references, edit);
+	}
+
     /**
-     * Determine if the component is nested J2EE module on the application.xml of this EAR
-     * @param aComponent
-     * @return boolean is passed in component a nested J2EE module on this EAR
+     * Determine if the component parameter is of a valid version and type
+	 * 
+     * @param aComponent a binary "utility" component
+     * @param references the list of references the parent ear component has
+     * @param model the artifact edit of the ear, if it exists 
+     * @return
      */
     private boolean isNestedJ2EEModule(IVirtualComponent aComponent, IVirtualReference[] references, IEARModelProvider model) {
     	if (model==null) 
 			return false;
-		Application app = (Application)model.getModelObject();
-		IVirtualReference reference = getReferenceNamed(references,aComponent.getName());
-		// Ensure module URI exists on EAR DD for binary archive
-		return app.getFirstModule(reference.getArchiveName()) != null;
+		int version = JavaEEBinaryComponentHelper.
+			getJavaEEQuickPeek(aComponent).getJavaEEVersion();
+		boolean knownType = 
+			JavaEEBinaryComponentHelper.getJavaEEQuickPeek(aComponent).getType() != JavaEEQuickPeek.UNKNOWN;
+		
+		return version >= J2EEVersionConstants.JEE_5_0_ID && knownType;
     }
 }
