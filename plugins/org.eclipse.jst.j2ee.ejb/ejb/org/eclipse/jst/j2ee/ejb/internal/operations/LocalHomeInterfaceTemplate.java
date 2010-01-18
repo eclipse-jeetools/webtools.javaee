b0c81ab8a058e99df8b4742e08fdc930e1b5f139
kraev <kraev>
2010-01-18 15:50:18 +0000

    [299799] Unnecessary import java.rmi.RemoteException in local home interface when adding a session bean

--- plugins/org.eclipse.jst.j2ee.ejb/ejb/org/eclipse/jst/j2ee/ejb/internal/operations/LocalHomeInterfaceTemplate.java
+++ plugins/org.eclipse.jst.j2ee.ejb/ejb/org/eclipse/jst/j2ee/ejb/internal/operations/LocalHomeInterfaceTemplate.java
@@ -14,7 +14,7 @@ public class LocalHomeInterfaceTemplate
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl; //$NON-NLS-1$
   protected final String TEXT_1 = "package "; //$NON-NLS-1$
   protected final String TEXT_2 = ";"; //$NON-NLS-1$
-  protected final String TEXT_3 = NL + "import java.rmi.RemoteException;" + NL + "" + NL + "import javax.ejb.CreateException;" + NL + "import javax.ejb.EJBLocalHome;"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+  protected final String TEXT_3 = NL + "" + NL + "import javax.ejb.CreateException;" + NL + "import javax.ejb.EJBLocalHome;"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
   protected final String TEXT_4 = NL + "import "; //$NON-NLS-1$
   protected final String TEXT_5 = "."; //$NON-NLS-1$
   protected final String TEXT_6 = ";"; //$NON-NLS-1$
