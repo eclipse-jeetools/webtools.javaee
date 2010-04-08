bc85afcdf17266313594047f2442715be8bc1eb3
canderson <canderson>
2010-04-08 05:22:33 +0000

    [308387] NPE thrown for "run on server" of binary ejb

--- plugins/org.eclipse.jst.jee.ui/src/org/eclipse/jst/jee/ui/internal/deployables/EJBDeployableArtifactAdapterUtil.java
+++ plugins/org.eclipse.jst.jee.ui/src/org/eclipse/jst/jee/ui/internal/deployables/EJBDeployableArtifactAdapterUtil.java
@@ -157,6 +157,8 @@ public class EJBDeployableArtifactAdapterUtil {
 
 	protected static IModule getModule(EObject refObject) {
 		IProject proj = ProjectUtilities.getProject(refObject);
+		if (proj == null)
+			return null;
 		Resource refResource = refObject.eResource();
 		IVirtualResource[] resources = null;
 		IVirtualComponent component = null;
