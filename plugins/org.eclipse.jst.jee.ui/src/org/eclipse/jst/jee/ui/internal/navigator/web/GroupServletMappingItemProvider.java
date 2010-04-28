db75e9339353cfbd6873b1ca320d0c960fa59ae6
dgiormov <dgiormov>
2010-04-28 07:38:26 +0000

    [310455] multiple mappings to single servlet are not shown in DD tree also valid for filter mappings.

--- plugins/org.eclipse.jst.jee.ui/src/org/eclipse/jst/jee/ui/internal/navigator/web/GroupServletMappingItemProvider.java
+++ plugins/org.eclipse.jst.jee.ui/src/org/eclipse/jst/jee/ui/internal/navigator/web/GroupServletMappingItemProvider.java
@@ -11,10 +11,13 @@
 package org.eclipse.jst.jee.ui.internal.navigator.web;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jst.j2ee.internal.plugin.J2EEPlugin;
+import org.eclipse.jst.javaee.core.UrlPatternType;
+import org.eclipse.jst.javaee.web.ServletMapping;
 import org.eclipse.jst.javaee.web.WebApp;
 import org.eclipse.jst.jee.ui.internal.Messages;
 import org.eclipse.jst.jee.ui.plugin.JEEUIPluginIcons;
@@ -38,7 +41,7 @@ public class GroupServletMappingItemProvider extends AbstractWebGroupProvider{
 	@Override
 	public List getChildren() {
 		if (javaee != null){
-			return ((WebApp) javaee).getServletMappings();
+			return flattenMapping(((WebApp) javaee).getServletMappings());
 		}
 		return null;
 	}
@@ -61,5 +64,43 @@ public class GroupServletMappingItemProvider extends AbstractWebGroupProvider{
 		}
 		return SERVLETMAPPING;
 	}
+	
+	private List<?> flattenMapping(List<ServletMapping> servletMappings) {
+		List<ServletMappingUIWrapper> result = new ArrayList<ServletMappingUIWrapper>();
+		if (servletMappings != null && servletMappings.size() > 0){
+			for (ServletMapping filterMapping : servletMappings) {
+				result.addAll(getServletMappingDisplay(filterMapping));
+			}
+		}
+		return result;
+	}
+
+	
+	private List<ServletMappingUIWrapper> getServletMappingDisplay(ServletMapping element) {
+		List<ServletMappingUIWrapper> result = new ArrayList<ServletMappingUIWrapper>();
+		String value = null;
+		if (element.getUrlPatterns().size() > 0){
+			for (UrlPatternType pattern : element.getUrlPatterns()) {
+				value = pattern.getValue();
+				result.add(new ServletMappingUIWrapper(value + " -> " + element.getServletName())); //$NON-NLS-1$
+			}
+			
+		}
+		return result;
+	}
+	
+	public class ServletMappingUIWrapper{
+		private String value;
+
+		public String getValue() {
+			return value;
+		}
+
+		public ServletMappingUIWrapper(String value) {
+			super();
+			this.value = value;
+		}
+	}
+	
 
 }
