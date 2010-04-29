54ab9c8b2aa3fc64cb1a93505ce62da25fb6a3a3
cbridgha <cbridgha>
2010-04-29 00:27:39 +0000

    [234412] uninstall fix

--- plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/project/facet/UtilityFacetUnInstallDelegate.java
+++ plugins/org.eclipse.jst.j2ee/earproject/org/eclipse/jst/j2ee/project/facet/UtilityFacetUnInstallDelegate.java
@@ -11,20 +11,13 @@
 
 package org.eclipse.jst.j2ee.project.facet;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jst.common.project.facet.WtpUtils;
 import org.eclipse.jst.common.project.facet.core.ClasspathHelper;
-import org.eclipse.jst.j2ee.internal.J2EEConstants;
-import org.eclipse.wst.common.componentcore.ComponentCore;
 import org.eclipse.wst.common.componentcore.internal.StructureEdit;
-import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
-import org.eclipse.wst.common.componentcore.resources.IVirtualFolder;
 import org.eclipse.wst.common.project.facet.core.IDelegate;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 
@@ -40,22 +33,6 @@ public final class UtilityFacetUnInstallDelegate implements IDelegate {
 			IFile file = project.getFile( StructureEdit.MODULE_META_FILE_NAME );
 			file.delete( true, monitor );
 			
-			//remove manifest file
-			final IVirtualComponent c = ComponentCore.createComponent(project);
-			final IVirtualFolder root = c.getRootFolder();
-			IContainer container = null;
-
-			if (root.getProjectRelativePath().segmentCount() == 0) {
-				container = project;
-			} else {
-				container = project.getFolder(root.getProjectRelativePath());
-			}
-			
-			IFile manifestFile = container.getFile(new Path(J2EEConstants.MANIFEST_URI));
-			manifestFile.delete( true, monitor );
-			
-			IFolder manifestFolder = container.getFolder( new Path(J2EEConstants.META_INF) );
-			manifestFolder.delete( true, monitor );
 		     
 			//remove  module core nature
 			WtpUtils.removeNatures( project );
