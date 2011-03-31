18d0e7e76ff16d45e5810e6008bd630c0ce7684e
canderson <canderson>
2011-03-31 01:21:06 +0000

    [340913] Renaming projects does not update java-output-path property in org.eclipse.wst.common.component

--- plugins/org.eclipse.jst.j2ee/refactor/org/eclipse/jst/j2ee/refactor/operations/ProjectRenameOperation.java
+++ plugins/org.eclipse.jst.j2ee/refactor/org/eclipse/jst/j2ee/refactor/operations/ProjectRenameOperation.java
@@ -17,7 +17,9 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jst.j2ee.common.CompatibilityDescriptionGroup;
 import org.eclipse.jst.j2ee.model.IModelProvider;
 import org.eclipse.jst.j2ee.model.ModelProviderManager;
@@ -29,9 +31,11 @@ import org.eclipse.jst.javaee.core.DisplayName;
 import org.eclipse.jst.javaee.core.JavaeeFactory;
 import org.eclipse.jst.javaee.ejb.EJBJar;
 import org.eclipse.jst.javaee.web.WebApp;
+import org.eclipse.wst.common.componentcore.internal.Property;
 import org.eclipse.wst.common.componentcore.internal.StructureEdit;
 import org.eclipse.wst.common.componentcore.internal.WorkbenchComponent;
 import org.eclipse.wst.common.componentcore.internal.util.ComponentUtilities;
+import org.eclipse.wst.common.componentcore.internal.util.IModuleConstants;
 import org.eclipse.wst.common.frameworks.datamodel.AbstractDataModelProvider;
 import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
@@ -71,15 +75,30 @@ public class ProjectRenameOperation extends ProjectRefactorOperation {
 		StructureEdit core = null;
 		WorkbenchComponent component = null;
 		try{
-		core = StructureEdit.getStructureEditForWrite(refactoredMetadata.getProject());
-		if(core != null){
-		component = core.getComponent();
-		}
+			core = StructureEdit.getStructureEditForWrite(refactoredMetadata.getProject());
+			if(core != null){
+				component = core.getComponent();
+			}
 	
-		// if the deploy-name equals the old project name, update it in the module-specific deployment descriptor
-		if (component != null && component.getName().equals(originalMetadata.getProjectName())) {
-			component.setName(refactoredMetadata.getProjectName());
-		}
+			// if the deploy-name equals the old project name, update it in the module-specific deployment descriptor
+			if (component != null && component.getName().equals(originalMetadata.getProjectName())) {
+				component.setName(refactoredMetadata.getProjectName());
+			}
+			
+			//update the java-output-path property
+			if (component != null){
+				List<Property> properties = component.getProperties();
+				for (Property property:properties){
+					if (property.getName().equals(IModuleConstants.PROJ_REL_JAVA_OUTPUT_PATH)){
+						IPath javaOutputPath = new Path(property.getValue());
+						IPath oldProjectNamePath = new Path(oldProjectName).makeAbsolute();					
+						if (oldProjectNamePath.isPrefixOf(javaOutputPath)){
+							IPath newJavaOutputPath = new Path(newProjectName).append(javaOutputPath.removeFirstSegments(1)).makeAbsolute();						
+							property.setValue(newJavaOutputPath.toString());
+						}
+					}
+				}			
+			}
 		}finally {
 			if(core != null) {
 				core.saveIfNecessary(null);
