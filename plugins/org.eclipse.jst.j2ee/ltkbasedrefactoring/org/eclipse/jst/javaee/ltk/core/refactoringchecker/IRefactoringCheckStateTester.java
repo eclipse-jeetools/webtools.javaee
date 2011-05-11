fc827eab41ebf43dcdcfa51263773bf92b044009
cbridgha <cbridgha>
2011-05-11 02:08:28 +0000

    This commit was manufactured by cvs2svn to create tag 'v201105110300'.

--- plugins/org.eclipse.jst.j2ee/ltkbasedrefactoring/org/eclipse/jst/javaee/ltk/core/refactoringchecker/IRefactoringCheckStateTester.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.jst.javaee.ltk.core.refactoringchecker;
-
-import org.eclipse.core.resources.IProject;
-
-public interface IRefactoringCheckStateTester {
-
-	boolean testUncheckState(IProject project);
-}
