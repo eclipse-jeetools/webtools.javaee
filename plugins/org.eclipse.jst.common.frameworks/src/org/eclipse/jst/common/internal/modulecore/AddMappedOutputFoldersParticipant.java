bed903758aa5064fa92419ba37eb6286a89923a1
Roberto Sanchez <rsanchez@mx1.ibm.com>
2012-08-15 11:56:47 -0500

    [386969] Exported archive includes entries with absolute paths

--- plugins/org.eclipse.jst.common.frameworks/src/org/eclipse/jst/common/internal/modulecore/AddMappedOutputFoldersParticipant.java
+++ plugins/org.eclipse.jst.common.frameworks/src/org/eclipse/jst/common/internal/modulecore/AddMappedOutputFoldersParticipant.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Red Hat and others.
+ * Copyright (c) 2009, 2012 Red Hat and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Red Hat - Initial API and implementation
+ *     Roberto Sanchez (IBM) - Use relative path for mapped output folders
  *******************************************************************************/
 package org.eclipse.jst.common.internal.modulecore;
 
@@ -78,7 +79,7 @@ public class AddMappedOutputFoldersParticipant extends AbstractFlattenParticipan
 		while(i.hasNext()) {
 			IContainer next = i.next();
 			try {
-				new VirtualComponentFlattenUtility(list, this).addContainer(next, mapped.get(next));
+				new VirtualComponentFlattenUtility(list, this).addContainer(next, mapped.get(next).makeRelative());
 			} catch( CoreException ce) {}
 		}
 	}
