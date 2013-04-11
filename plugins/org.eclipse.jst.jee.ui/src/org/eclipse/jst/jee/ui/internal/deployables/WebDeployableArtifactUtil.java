6f3a0768769c5cf05169dfd2fe5ed50cd3f1f0c2
Carl Anderson <ccc@us.ibm.com>
2013-04-11 09:46:41 -0400

    [404752] URL not calculated for servlets in web fragments

--- plugins/org.eclipse.jst.jee.ui/src/org/eclipse/jst/jee/ui/internal/deployables/WebDeployableArtifactUtil.java
+++ plugins/org.eclipse.jst.jee.ui/src/org/eclipse/jst/jee/ui/internal/deployables/WebDeployableArtifactUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2007 IBM Corporation and others.
+ * Copyright (c) 2003, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -348,8 +348,8 @@ public class WebDeployableArtifactUtil {
 		IModelProvider provider = ModelProviderManager.getModelProvider( resource.getProject() );
 		Object mObj = provider.getModelObject();
 		
-		if (mObj instanceof WebApp) {
-			WebApp webApp= (WebApp) mObj;
+		if (mObj instanceof IWebCommon) {
+			IWebCommon webApp= (IWebCommon) mObj;
 			List<Servlet> servlets = webApp.getServlets();
 			// Ensure the display does not already exist in the web application
 			if (servlets != null && !servlets.isEmpty()) {
