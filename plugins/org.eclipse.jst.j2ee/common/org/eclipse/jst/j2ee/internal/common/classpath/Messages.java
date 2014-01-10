3750c4ff46e81d05d3d402df007d2055e881ba0b
Carl Anderson <ccc@us.ibm.com>
2014-01-10 11:04:02 -0500

    [425051] Deadlock due to Resource Listener waiting for workspace lock

--- plugins/org.eclipse.jst.j2ee/common/org/eclipse/jst/j2ee/internal/common/classpath/Messages.java
+++ plugins/org.eclipse.jst.j2ee/common/org/eclipse/jst/j2ee/internal/common/classpath/Messages.java
@@ -16,6 +16,7 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 
 	public static final String BUNDLE_NAME = "org.eclipse.jst.j2ee.internal.common.classpath.messages"; //$NON-NLS-1$
+	public static String J2EEComponentClasspathUpdater_Find_Node;
 	public static String J2EEComponentClasspathUpdater_EAR_Libraries_Update_Jo_;
 	public static String J2EEComponentClasspathUpdater_Verify_EAR_Libraries;
 
