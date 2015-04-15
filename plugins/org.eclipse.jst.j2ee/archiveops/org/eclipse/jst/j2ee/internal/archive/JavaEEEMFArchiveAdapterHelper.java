a1697225fcc3111bbb216eda8f288fa9703a587c
Roberto Sanchez <rsanchez@mx1.ibm.com>
2015-04-15 09:39:00 -0500

    Revert "[459564] JUNIT failed to run during the WTP build (code for diagnostic)"
    This reverts commit eeeb25e2c79ed32157333347e8f07dd3406f6364.

--- plugins/org.eclipse.jst.j2ee/archiveops/org/eclipse/jst/j2ee/internal/archive/JavaEEEMFArchiveAdapterHelper.java
+++ plugins/org.eclipse.jst.j2ee/archiveops/org/eclipse/jst/j2ee/internal/archive/JavaEEEMFArchiveAdapterHelper.java
@@ -125,11 +125,9 @@ public class JavaEEEMFArchiveAdapterHelper {
 		try {
 			resource = getResourceSet().getResource(uri, true);
 		}
-		catch (RuntimeException e){
+		catch (org.eclipse.emf.common.util.WrappedException e){
 			J2EEPlugin.logError("Error getting resource using " //$NON-NLS-1$
 					+ "org.eclipse.emf.ecore.resource.ResourceSet.getResource(URI uri, boolean loadOnDemand). The URI is: " + uri, e); //$NON-NLS-1$
-			System.out.println("Error getting resource using " //$NON-NLS-1$
-					+ "org.eclipse.emf.ecore.resource.ResourceSet.getResource(URI uri, boolean loadOnDemand). The URI is: " + uri +". The exception is: " + e.getMessage()); //$NON-NLS-1$ //$NON-NLS-2$
 			throw e;
 		}
 		return resource;
